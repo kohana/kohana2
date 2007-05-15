dfb3ea977ee819cf33aa78565b8339ebac2dd1c1
Phoop <Phoop@6c8b873b-1030-0410-a378-995d7067e127>
2007-05-15 12:10:30 +0000

    Fixed according #6

--- system/helpers/cookie_helper.php
+++ system/helpers/cookie_helper.php
@@ -36,17 +36,18 @@
  * @access	public
  * @param	mixed
  * @param	string	the value of the cookie
- * @param	string	the number of seconds until expiration
+ * @param	int		the number of seconds until expiration
  * @param	string	the cookie domain.  Usually:  .yourdomain.com
  * @param	string	the cookie path
  * @param	string	the cookie prefix
+ * @param	bool	whether the cookie should be sent ONLY over SSL
  * @return	void
  */
-function set_cookie($name = '', $value = '', $expire = '', $domain = '', $path = '/', $prefix = '')
+function set_cookie($name, $value = '', $expire = 0, $domain = null, $path = null, $prefix = null, $secure = false)
 {
 	if (is_array($name))
-	{		
-		foreach (array('value', 'expire', 'domain', 'path', 'prefix', 'name') as $item)
+	{
+		foreach (array('value', 'expire', 'domain', 'path', 'prefix', 'secure', 'name') as $item)
 		{
 			if (isset($name[$item]))
 			{
@@ -54,42 +55,44 @@ function set_cookie($name = '', $value = '', $expire = '', $domain = '', $path =
 			}
 		}
 	}
-	
+
 	// Set the config file options
 	$CI =& get_instance();
-	
-	if ($prefix == '' AND $CI->config->item('cookie_prefix') != '')
-	{
-		$CI->config->item('cookie_prefix');
-	}
-	if ($domain == '' AND $CI->config->item('cookie_domain') != '')
-	{
-		$CI->config->item('cookie_domain');
-	}
-	if ($prefix == '/' AND $CI->config->item('cookie_path') != '/')
-	{
-		$CI->config->item('cookie_path');
-	}
-		
 	if ( ! is_numeric($expire))
 	{
-		$expire = time() - 86500;
+		$default_cookie_lifetime = $CI->config->item('cookie_lifetime');
+		$expire =
+			(! is_numeric($default_cookie_lifetime) || $default_cookie_lifetime<=0) ?
+				0 :
+				(time() + $default_cookie_lifetime);
 	}
 	else
 	{
-		if ($expire > 0)
-		{
-			$expire = time() + $expire;
-		}
-		else
-		{
-			$expire = 0;
-		}
+		$expire =
+			($expire==0) ?
+				$expire :
+				(time() + $expire);
 	}
-	
-	setcookie($prefix.$name, $value, $expire, $path, $domain, 0);
+	$default_domain = $CI->config->item('cookie_domain');
+	if (is_null($domain) AND !empty($default_domain))
+	{
+		$domain = $default_domain;
+	}
+	$default_path = $CI->config->item('cookie_path');
+	if (is_null($path) AND ! empty($default_path))
+	{
+		$path = $default_path;
+	}
+	$default_prefix = $CI->config->item('cookie_prefix');
+	if (is_null($prefix) AND ! empty($default_prefix))
+	{
+		$prefix = $default_prefix;
+	}
+	$secure = is_bool($secure) ? $secure : false;
+
+	setcookie($prefix.$name, $value, $expire, $path, $domain, $secure);
 }
-	
+
 // --------------------------------------------------------------------
 
 /**
@@ -97,13 +100,19 @@ function set_cookie($name = '', $value = '', $expire = '', $domain = '', $path =
  *
  * @access	public
  * @param	string
+ * @param	string
  * @param	bool
  * @return	mixed
  */
-function get_cookie($index = '', $xss_clean = FALSE)
+function get_cookie($name = '', $prefix=null, $xss_clean = FALSE)
 {
 	$CI =& get_instance();
-	return $CI->input->cookie($index, $xss_clean);
+	$default_prefix = $CI->config->item('cookie_prefix');
+	if (is_null($prefix) AND ! empty($default_prefix))
+	{
+		$prefix = $default_prefix;
+	}
+	return $CI->input->cookie($prefix.$name, $xss_clean);
 }
 
 // --------------------------------------------------------------------
@@ -111,15 +120,31 @@ function get_cookie($index = '', $xss_clean = FALSE)
 /**
  * Delete a COOKIE
  *
+ * @access	public
  * @param	mixed
  * @param	string	the cookie domain.  Usually:  .yourdomain.com
  * @param	string	the cookie path
  * @param	string	the cookie prefix
+ * @param 	bool
  * @return	void
  */
-function delete_cookie($name = '', $domain = '', $path = '/', $prefix = '')
+function delete_cookie($name = '', $domain = null, $path = null, $prefix = null, $remove_live = TRUE)
 {
-	set_cookie($name, '', '', $domain, $path, $prefix);
+	if($remove_live===TRUE)
+	{
+		$CI =& get_instance();
+		$default_prefix = $CI->config->item('cookie_prefix');
+		if (is_null($prefix) AND ! empty($default_prefix))
+		{
+			$prefix = $default_prefix;
+		}
+		$cIndex = is_array($name) ? $name['name'] : $name;
+		if(isset($_COOKIE[$prefix.$cIndex]))
+		{
+			unset($_COOKIE[$prefix.$cIndex]);
+		}
+	}
+	set_cookie($name, '', -86500, $domain, $path, $prefix);
 }
 
 
