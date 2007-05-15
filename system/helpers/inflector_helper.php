9b887e8f737515bb5e27e0f6738631a9543c605e
Tido <Tido@6c8b873b-1030-0410-a378-995d7067e127>
2007-05-15 09:37:42 +0000

    fixed singular and plural helpers in accordance with ticket 21.

--- system/helpers/inflector_helper.php
+++ system/helpers/inflector_helper.php
@@ -39,24 +39,28 @@
  */		
 function singular($str)
 {
-	$str = strtolower(trim($str));
-	$end = substr($str, -3);
-	
-	if ($end == 'ies')
-	{
-		$str = substr($str, 0, strlen($str)-3).'y';
-	}
-	else
-	{
-		$end = substr($str, -1);
-		
-		if ($end == 's')
-		{
-			$str = substr($str, 0, strlen($str)-1);
-		}
-	}
-	
-	return $str;
+    $str = strtolower(trim($str));
+    $end = substr($str, -3);
+    
+    if ($end == 'ies')
+    {
+        $str = substr($str, 0, strlen($str)-3).'y';
+    }
+    elseif ($end == 'ses')
+    {
+        $str = substr($str, 0, strlen($str)-2);
+    }
+    else
+    {
+        $end = substr($str, -1);
+        
+        if ($end == 's')
+        {
+            $str = substr($str, 0, strlen($str)-1);
+        }
+    }
+    
+    return $str;
 }
 
 // --------------------------------------------------------------------
@@ -70,22 +74,30 @@ function singular($str)
  * @param	string
  * @return	str
  */		
-function plural($str)
+function plural($str, $force = FALSE)
 {
-	$str = strtolower(trim($str));
-	$end = substr($str, -1);
+    $str = strtolower(trim($str));
+    $end = substr($str, -1);
 
-	if ($end == 'y')
-	{
-		$str = substr($str, 0, strlen($str)-1).'ies';
-	}
-	elseif ($end != 's')
-	{
-		$str .= 's';
-	}
+    if ($end == 'y')
+    {
+        $str = substr($str, 0, strlen($str)-1).'ies';
+    }
+    elseif ($end == 's')
+    {
+        if ($force == TRUE)
+        {
+            $str .= 'es';
+        }
+    }
+    else
+    {
+        $str .= 's';
+    }
 
-	return $str;	
+    return $str;
 }
+
 
 // --------------------------------------------------------------------
 
