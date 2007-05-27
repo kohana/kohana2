a6b498edd51aa7ce60c2defa91f36b6a93a4423e
Tido <Tido@6c8b873b-1030-0410-a378-995d7067e127>
2007-05-27 21:12:42 +0000

    merged to include various fixes and optimizations from branches/1.0/system/libraries/Validation.php

--- system/libraries/Validation.php
+++ system/libraries/Validation.php
@@ -504,7 +504,7 @@ class CI_Validation {
 	 */		
 	function alpha($str)
 	{
-		return ctype_alpha($str);
+		return ( ! preg_match("/^([-a-z])+$/i", $str)) ? FALSE : TRUE;
 	}
 	
 	// --------------------------------------------------------------------
@@ -518,7 +518,7 @@ class CI_Validation {
 	 */	
 	function alpha_numeric($str)
 	{
-		return ctype_alnum($str);
+		return ( ! preg_match("/^([-a-z0-9])+$/i", $str)) ? FALSE : TRUE;
 	}
 	
 	// --------------------------------------------------------------------
@@ -532,21 +532,7 @@ class CI_Validation {
 	 */	
 	function alpha_dash($str)
 	{
-		return (bool) preg_match('/^[-a-z0-9_]+$/i', $str); 
-	}
-	
-	// --------------------------------------------------------------------
-	
-	/**
-	 * Digits Only [0-9, no dots or dashes]
-	 *
-	 * @access	public
-	 * @param	int
-	 * @return	bool
-	 */
-	function digit($str)
-	{
-		return ctype_digit($str);
+		return ( ! preg_match("/^([-a-z0-9_-])+$/i", $str)) ? FALSE : TRUE;
 	}
 	
 	// --------------------------------------------------------------------
@@ -558,15 +544,11 @@ class CI_Validation {
 	 * @param	int
 	 * @return	bool
 	 */	
-	function numeric($str) {	
-		if (!is_numeric($str))
-		    return FALSE;
-	    
-		if (!preg_match('/^[-0-9.]+$/', $str))
-		    return FALSE;
-	    
-		return TRUE;
-	} 
+	function numeric($str)
+	{
+		return ( ! ereg("^[0-9\.]+$", $str)) ? FALSE : TRUE;
+	}
+
 	// --------------------------------------------------------------------
 	
 	/**
