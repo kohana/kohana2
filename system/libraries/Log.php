1a021bb64b16e230c6499128f53175c717ffb5ef
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-05-28 04:21:17 +0000

    Fixing #46. Changed files: application/config/config.php system/libraries/Log.php

--- system/libraries/Log.php
+++ system/libraries/Log.php
@@ -43,27 +43,27 @@ class CI_Log {
 	function CI_Log()
 	{
 		$config =& get_config();
-		
-		$this->log_path = ($config['log_path'] != '') ? $config['log_path'] : BASEPATH.'logs/';
-		
+
+		$this->log_path = ($config['log_path'] != '') ? $config['log_path'] : APPPATH.'logs/';
+
 		if ( ! is_dir($this->log_path) OR ! is_writable($this->log_path))
 		{
 			$this->_enabled = FALSE;
 		}
-		
+
 		if (is_numeric($config['log_threshold']))
 		{
 			$this->_threshold = $config['log_threshold'];
 		}
-			
+
 		if ($config['log_date_format'] != '')
 		{
 			$this->_date_fmt = $config['log_date_format'];
 		}
 	}
-	
+
 	// --------------------------------------------------------------------
-	
+
 	/**
 	 * Write Log File
 	 *
@@ -74,42 +74,42 @@ class CI_Log {
 	 * @param	string	the error message
 	 * @param	bool	whether the error is a native PHP error
 	 * @return	bool
-	 */		
+	 */
 	function write_log($level = 'error', $msg, $php_error = FALSE)
-	{		
+	{
 		if ($this->_enabled === FALSE)
 		{
 			return FALSE;
 		}
-	
+
 		$level = strtoupper($level);
-		
+
 		if ( ! isset($this->_levels[$level]) OR ($this->_levels[$level] > $this->_threshold))
 		{
 			return FALSE;
 		}
-	
+
 		$filepath = $this->log_path.'log-'.date('Y-m-d').EXT;
 		$message  = '';
-		
+
 		if ( ! file_exists($filepath))
 		{
 			$message .= "<"."?php  if (!defined('BASEPATH')) exit('No direct script access allowed'); ?".">\n\n";
 		}
-			
+
 		if ( ! $fp = @fopen($filepath, "a"))
 		{
 			return FALSE;
 		}
 
 		$message .= $level.' '.(($level == 'INFO') ? ' -' : '-').' '.date($this->_date_fmt). ' --> '.$msg."\n";
-		
-		flock($fp, LOCK_EX);	
+
+		flock($fp, LOCK_EX);
 		fwrite($fp, $message);
 		flock($fp, LOCK_UN);
 		fclose($fp);
-	
-		@chmod($filepath, 0666); 		
+
+		@chmod($filepath, 0666);
 		return TRUE;
 	}
 
