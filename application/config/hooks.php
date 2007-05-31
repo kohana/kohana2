97afade32dea6963ed73673f93ec429ebf94ffc1
Tido <Tido@6c8b873b-1030-0410-a378-995d7067e127>
2007-05-31 17:28:19 +0000

    branching to 1.0
    *crosses fingers* hope I don't break it! :x

---
+++ application/config/hooks.php
@@ -0,0 +1,14 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------------
+| Hooks
+| -------------------------------------------------------------------------
+| This file lets you define "hooks" to extend BlueFlame without hacking the core
+| files.  Please see the user guide for info:
+|
+|	http://www.codeigniter.com/user_guide/general/hooks.html
+|
+*/
+
+
+?>
