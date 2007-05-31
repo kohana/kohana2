eab5bfdabcc4483a21100b00efbc6e6c12d76ad0
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-05-31 22:22:09 +0000

    Added MY_Loader library extension for loading ORM modeling

---
+++ system/libraries/MY_Loader.php
@@ -0,0 +1,46 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class MY_Loader extends CI_Loader {
+
+	function MY_Loader()
+	{
+		parent::CI_Loader();
+	}
+
+	/**
+	 * ORM Loader
+	 *
+	 * This function lets users load ORM for a specific table
+	 *
+	 * @access	public
+	 * @param	string the name of the table
+	 * @param	mixed  WHERE clause
+	 * @return	void
+	 */
+	function orm($table, $where = FALSE)
+	{
+		if ($table == FALSE)
+			return FALSE;
+
+		if ( ! class_exists('CI_DB'))
+		{
+			$CI =& get_instance();
+			$CI->load->database();
+			unset($CI);
+		}
+
+		require_once($this->_ci_find_class('ORM'));
+
+		$model = new Core_ORM($table);
+		if ($where != FALSE)
+		{
+			$model->get($where);
+		}
+
+		return $model;
+	}
+
+}
+
+// END class MY_Loader
+?>
