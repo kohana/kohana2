fdf35b64f978939139324e4c5e6601eb04268625
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-05-31 20:07:04 +0000

    Fixing #62 and some whitespace issues
    Changed files:
    system/scaffolding/Scaffolding.php
    system/scaffolding/views/footer.php

--- system/scaffolding/views/footer.php
+++ system/scaffolding/views/footer.php
@@ -2,7 +2,7 @@
 </div>
 
 <div id="footer">
-<p><a href="http://blueflame.ciforge.com/">BlueFlame</a> -  Version <?php echo CI_VERSION ?></p>
+<p><a href="http://blueflame.ciforge.com/">BlueFlame</a> -  Version <?php echo BF_VERSION ?></p>
 <p>Page rendered in {elapsed_time}</p>
 </div>
 
