dfc87c754e694df8570d4b86355fc6e028551870
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2007-06-09 18:27:54 +0000

    human_to_unix() = strtotime() http://codeigniter.com/forums/viewthread/51809/

--- system/helpers/date_helper.php
+++ system/helpers/date_helper.php
@@ -413,57 +413,7 @@ function unix_to_human($time = '', $seconds = FALSE, $fmt = 'us')
  */	
 function human_to_unix($datestr = '')
 {
-	if ($datestr == '')
-	{
-		return FALSE;
-	}
-	
-	$datestr = trim($datestr);
-	$datestr = preg_replace("/\040+/", "\040", $datestr);
-
-	if ( ! ereg("^[0-9]{2,4}\-[0-9]{1,2}\-[0-9]{1,2}\040[0-9]{1,2}:[0-9]{1,2}.*$", $datestr))
-	{
-		return FALSE;
-	}
-
-	$split = preg_split("/\040/", $datestr);
-
-	$ex = explode("-", $split['0']);
-	
-	$year  = (strlen($ex['0']) == 2) ? '20'.$ex['0'] : $ex['0'];
-	$month = (strlen($ex['1']) == 1) ? '0'.$ex['1']  : $ex['1'];
-	$day   = (strlen($ex['2']) == 1) ? '0'.$ex['2']  : $ex['2'];
-
-	$ex = explode(":", $split['1']);
-	
-	$hour = (strlen($ex['0']) == 1) ? '0'.$ex['0'] : $ex['0'];
-	$min  = (strlen($ex['1']) == 1) ? '0'.$ex['1'] : $ex['1'];
-
-	if (isset($ex['2']) AND ereg("[0-9]{1,2}", $ex['2']))
-	{
-		$sec  = (strlen($ex['2']) == 1) ? '0'.$ex['2'] : $ex['2'];
-	}
-	else
-	{
-		// Unless specified, seconds get set to zero.
-		$sec = '00';
-	}
-	
-	if (isset($split['2']))
-	{
-		$ampm = strtolower($split['2']);
-		
-		if (substr($ampm, 0, 1) == 'p' AND $hour < 12)
-			$hour = $hour + 12;
-			
-		if (substr($ampm, 0, 1) == 'a' AND $hour == 12)
-			$hour =  '00';
-			
-		if (strlen($hour) == 1)
-			$hour = '0'.$hour;
-	}
-			
-	return mktime($hour, $min, $sec, $month, $day, $year);
+	return strtotime($datestr);
 }
 	
 // ------------------------------------------------------------------------
