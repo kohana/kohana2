e01fc5c3e80d274c833cc273767fd31323480a70
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-06-11 22:14:31 +0000

    Fixing #81 using Alan's patch

--- system/libraries/Router.php
+++ system/libraries/Router.php
@@ -412,6 +412,7 @@ class CI_Router {
 		{
 			if ( ! preg_match("|^[".preg_quote($this->config->item('permitted_uri_chars'))."]+$|i", $str))
 			{
+				header('HTTP/1.1 400 Bad Request');
 				exit('The URI you submitted has disallowed characters.');
 			}
 		}
