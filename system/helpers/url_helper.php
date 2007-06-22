4ea1e4b18277e616acdb1578f92e5731c6911919
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-06-22 23:32:26 +0000

    Correcting bad search/replace of ending newlines

--- system/helpers/url_helper.php
+++ system/helpers/url_helper.php
@@ -274,7 +274,9 @@ function safe_mailto($email, $title = '', $attributes = '')
 var l=new Array();
 <?php
 $i = 0;
-foreach ($x as $val){ ?>l[<?php echo $i++; ?>]='<?php echo $val; ?>';<?php } ?>for (var i = l.length-1; i >= 0; i=i-1){
+foreach ($x as $val){ ?>l[<?php echo $i++; ?>]='<?php echo $val; ?>';<?php } ?>
+
+for (var i = l.length-1; i >= 0; i=i-1){
 if (l[i].substring(0, 1) == '|') document.write("&#"+unescape(l[i].substring(1))+";");
 else document.write(unescape(l[i]));}
 //]]>
