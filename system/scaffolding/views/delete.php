c926531d6d36c50071d69fb3a6222a485787ad2b
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-06-22 23:33:00 +0000

    Correcting bad search/replace of ending newlines

--- system/scaffolding/views/delete.php
+++ system/scaffolding/views/delete.php
@@ -1,3 +1,7 @@
-<?php  $this->load->view('header');  ?><p><?php echo $message; ?></p>
+<?php  $this->load->view('header');  ?>
 
-<p><?php echo $no; ?>&nbsp;&nbsp;|&nbsp;&nbsp;<?php echo $yes; ?><?php $this->load->view('footer'); ?>
+<p><?php echo $message; ?></p>
+
+<p><?php echo $no; ?>&nbsp;&nbsp;|&nbsp;&nbsp;<?php echo $yes; ?>
+
+<?php $this->load->view('footer'); ?>
