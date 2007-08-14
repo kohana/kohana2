765132094cee64236ee80bfb9397581af6393b2f
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-08-14 02:56:04 +0000

    There we go, proper user_guide location

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -4,11 +4,19 @@ class Welcome extends Controller {
 
 	function index()
 	{
+		$this->load->view('welcome')->set('message', 'testing')->render(TRUE);
+		print "hi";
+	}
+
+	function _index()
+	{
 		$this->load->helper('url');
 		print "<p>Welcome to ".url::site_url('welcome')."!</p>\n";
 		
 		$this->load->model('users');
 		print "<p>Model test: ".$this->users->hi()."!</p>\n";
+
+		print "<p>Rendered in {execution_time} seconds</p>\n";
 	}
 
 }
