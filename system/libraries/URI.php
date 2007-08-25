ae965910f19eb997d92c969b9908ec3d0a1fe952
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2007-08-25 19:36:08 +0000

    URI->segment_array() and URI->uri_string()

--- system/libraries/URI.php
+++ system/libraries/URI.php
@@ -8,5 +8,23 @@ class URI_Core extends Router {
 		
 		return isset(self::$segments[$index]) ? self::$segments[$index] : FALSE;
 	}
+
+	public function segment_array()
+	{
+		$i = 1;
+		$segment_array = array();
+
+		foreach (self::$segments as $segment)
+		{
+			$segment_array[$i++] = $segment;
+		}
+		
+		return $segment_array;
+	}
+	
+	public function uri_string()
+	{
+		return implode('/', self::$segments);
+	}
 	
 } // End URI Class
