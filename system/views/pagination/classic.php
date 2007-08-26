3d0e76dbff2c9e18df3b922d6f250588851bb708
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2007-08-26 18:41:34 +0000

    Fixed pagination->url() access in views New pagination style: punbb

--- system/views/pagination/classic.php
+++ system/views/pagination/classic.php
@@ -9,11 +9,11 @@
 <p>
 	
 	<?php if ($first_page) { ?>
-		<a href="<?php echo __Pagination__->url(1) ?>">&lsaquo;&nbsp;First</a>
+		<a href="<?php echo $this->pagination->url(1) ?>">&lsaquo;&nbsp;First</a>
 	<?php } ?>
 
 	<?php if ($previous_page) { ?>
-		<a href="<?php echo __Pagination__->url($previous_page) ?>">&lt;</a>
+		<a href="<?php echo $this->pagination->url($previous_page) ?>">&lt;</a>
 	<?php } ?>
 	
 
@@ -22,18 +22,18 @@
 		<?php if ($i == $current_page) { ?>
 			<strong><?php echo $i ?></strong>
 		<?php } else { ?>
-			<a href="<?php echo __Pagination__->url($i) ?>"><?php echo $i ?></a>
+			<a href="<?php echo $this->pagination->url($i) ?>"><?php echo $i ?></a>
 		<?php } ?>
 		
 	<?php } ?>
 
 
 	<?php if ($next_page) { ?>
-		<a href="<?php echo __Pagination__->url($next_page) ?>">&gt;</a>
+		<a href="<?php echo $this->pagination->url($next_page) ?>">&gt;</a>
 	<?php } ?>
 
 	<?php if ($last_page) { ?>
-		<a href="<?php echo __Pagination__->url($last_page) ?>">Last&nbsp;&rsaquo;</a>
+		<a href="<?php echo $this->pagination->url($last_page) ?>">Last&nbsp;&rsaquo;</a>
 	<?php } ?>
 
 </p>
