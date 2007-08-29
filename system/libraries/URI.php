126ad812dd47c562f3a43c30d415c1e3fd7d26d1
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2007-08-29 19:38:12 +0000

    * Added parameter for alternative default return value to URI->segment()  * Added URI->total_segments()

--- system/libraries/URI.php
+++ system/libraries/URI.php
@@ -2,13 +2,27 @@
 
 class URI_Core extends Router {
 
-	public function segment($index = 1)
+	/**
+	 * Retrieve a specific URI segment
+	 *
+	 * @access	public
+	 * @param	int
+	 * @param	mixed
+	 * @return	mixed
+	 */
+	public function segment($index = 1, $default = FALSE)
 	{
 		$index = (int) $index - 1;
 
-		return isset(self::$segments[$index]) ? self::$segments[$index] : FALSE;
+		return isset(self::$segments[$index]) ? self::$segments[$index] : $default;
 	}
 
+	/**
+	 * Returns an array containing all the URI segments
+	 *
+	 * @access	public
+	 * @return	array
+	 */
 	public function segment_array()
 	{
 		$i = 1;
@@ -22,9 +36,26 @@ class URI_Core extends Router {
 		return $segment_array;
 	}
 
+	/**
+	 * Returns the complete URI as a string
+	 *
+	 * @access	public
+	 * @return	string
+	 */
 	public function string()
 	{
 		return implode('/', self::$segments);
+	}
+	
+	/**
+	 * Returns the total number of URI segments
+	 *
+	 * @access	public
+	 * @return	integer
+	 */
+	public function total_segments()
+	{
+		return count(self::$segments);
 	}
 
 } // End URI Class
