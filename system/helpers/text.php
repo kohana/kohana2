2baba5707fad22d8a6dedc8214678c1e0d1067e2
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2007-08-29 09:26:20 +0000

    Merged text::soft_limit_chars() functionality into 4th parameter of the text::limit_chars() helper

--- system/helpers/text.php
+++ system/helpers/text.php
@@ -38,37 +38,23 @@ class text {
 	 * @param	string
 	 * @param	integer
 	 * @param	string
+	 * @param   boolean
 	 * @return	string
 	 */
-	public static function limit_chars($str, $limit = 100, $end_char = '&#8230;')
+	public static function limit_chars($str, $limit = 100, $end_char = '&#8230;', $preserve_words = FALSE)
 	{
 		$limit = (int) $limit;
+		$end_char = ($end_char === NULL) ? '&#8230;' : $end_char;
 
 		if (trim($str) == '' OR utf8::strlen($str) <= $limit)
 			return $str;
-
-		return rtrim(utf8::substr($str, 0, $limit)).$end_char;
-	}
-	
-	/**
-	 * Character limiter that preserves words
-	 *
-	 * @access	public
-	 * @param	string
-	 * @param	integer
-	 * @param	string
-	 * @return	string
-	 */
-	public static function soft_limit_chars($str, $limit = 100, $end_char = '&#8230;')
-	{
-		$limit = (int) $limit;
-
-		if (trim($str) == '' OR utf8::strlen($str) <= $limit)
-			return $str;
-
+		
 		if ($limit <= 0)
 			return $end_char;
-
+		
+		if ( ! $preserve_words)
+			return rtrim(utf8::substr($str, 0, $limit)).$end_char;
+		
 		preg_match('/^.{'.($limit - 1).'}\S*/us', $str, $matches);
 
 		if (strlen($matches[0]) == strlen($str))
