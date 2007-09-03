2d883797005dfd985a2d9c7acd961605abfed696
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-09-03 05:44:36 +0000

    Setting up the User Guide for i18n

---
+++ modules/user_guide/views/user_guide/en/template.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<title>Kohana User Guide</title>
+
+<link type="text/css" rel="stylesheet" href="<?php echo url::base(TRUE) ?>user_guide/css/layout.css" />
+<link type="text/css" rel="stylesheet" href="<?php echo url::base(TRUE) ?>user_guide/css/prettify.css" />
+
+<script type="text/javascript" src="<?php echo url::base(TRUE) ?>user_guide/js/jquery.js"></script>
+<script type="text/javascript" src="<?php echo url::base(TRUE) ?>user_guide/js/plugins.js"></script>
+<script type="text/javascript" src="<?php echo url::base(TRUE) ?>user_guide/js/prettify.js"></script>
+<script type="text/javascript" src="<?php echo url::base(TRUE) ?>user_guide/js/effects.js"></script>
+
+</head>
+<body>
+<div id="container">
+
+<div id="menu">
+<?php echo $menu ?>
+</div>
+
+<!-- @start Body -->
+<div id="body">
+<?php echo $content ?>
+</div>
+<!-- @end Body -->
+
+<!-- @start Footer -->
+<div id="footer">
+<p id="copyright">copyright (c) <?php echo date('Y') ?> Kohana Team :: All rights reserved :: Rendered in {execution_time} seconds using {memory_usage}MB of memory</p>
+</div>
+<!-- @end Footer -->
+
+</div>
+</body>
+</html>
