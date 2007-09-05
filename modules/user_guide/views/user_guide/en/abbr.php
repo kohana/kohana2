e600422f588898d4bfc157c3932ec3ecd2d2b8a9
OscarB <OscarB@6c8b873b-1030-0410-a378-995d7067e127>
2007-09-05 18:14:25 +0000

    UG updates: add hr rule for css, add some abbrev

--- modules/user_guide/views/user_guide/en/abbr.php
+++ modules/user_guide/views/user_guide/en/abbr.php
@@ -1,4 +1,7 @@
+*[COOKIE]: Data exchanged by server and browser to provide persistent state
+*[HTML]: Hyper Text Markup Language
 *[HTTP]: Hyper Text Transfer Protocol
+*[HTTPS]: HyperText Transfer Protocol Secure
 *[OS]: Operating System
 *[OSX]: Apple OS X
 *[PDO]: PHP Database Objects
