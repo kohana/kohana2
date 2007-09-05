ae641a71ea87cf6e700aa629b3d21e33f416b619
OscarB <OscarB@6c8b873b-1030-0410-a378-995d7067e127>
2007-09-05 18:19:24 +0000

    UG updates: Adding libraries content, encryption, input

--- modules/user_guide/views/user_guide/en/libraries/encryption.php
+++ modules/user_guide/views/user_guide/en/libraries/encryption.php
@@ -1,3 +1,33 @@
-Article status [Stub] requires [Writing] Describe encryption class and usage
+Article status [First draft] requires [Editing] Describe class
 # Encryption Class
-This is a stub (please write me)
+Todo Describe the class
+
+## Methods
+
+### Encode
+<code>$this->encrypt->encode()</code> accepts multiple parameters. The input **string** is required.
+Returns an encoded string.
+
+    $encoded_message = $this->encrypt->encode($str, $key = '');
+
+****
+
+### Decode
+<code>$this->encrypt->decode()</code> accepts multiple parameters. The input **string** is required.
+Returns a decoded string.
+
+    $decoded_message = $this->encrypt->decode($str, $key = '');
+
+
+****
+
+### Hash
+<code>$this->encrypt->hash()</code> accepts one mandatory parameter. The input **string** is required.
+Returns a message digest.
+
+    $sha_digest = $this->encrypt->hash($str);
+
+
+
+<?php echo $this->load->view('user_guide/en/abbr') ?>
+<?php /* $Id$ */ ?>
