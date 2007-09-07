757b25f8ced2ce5766cc8ed3805aa4c6db8c1458
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-09-07 06:20:28 +0000

    stupid f... case... insens..i give up

---
+++ system/i18n/en/validation.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+$lang = array
+(
+	'error_format'  => 'Your error message string must contain the string {message} .',
+	'invalid_rule'  => 'Invalid validation rule used: %s',
+
+	'unknown_error' => 'Unknown validation error while validating the %s field.',
+
+	'required'      => 'The %s field is required.',
+	'min_length'    => 'The %s field must be at least %s characters long.',
+	'max_length'    => 'The %s field must be %s characters or less.'
+);
