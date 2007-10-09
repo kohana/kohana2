18ec0ac456866690dd6a23cda7f9fb44cc9f707a
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2007-10-09 11:05:39 +0000

    no_default_route error fix

--- system/i18n/en/core.php
+++ system/i18n/en/core.php
@@ -3,7 +3,7 @@
 $lang = array
 (
 	'there_can_be_only_one' => 'There can be only one instance of Kohana per page request.',
-	'no_default_route'      => 'Please set a default route in <tt>config.php</tt>.',
+	'no_default_route'      => 'Please set a default route in <tt>config/routes.php</tt>.',
 	'page_not_found'        => 'The page you requested, <tt>%s</tt>, could not be found.',
 	'resource_not_found'    => 'The requested %s, <tt>%s</tt>, could not be found.',
 	'no_controller'         => 'Kohana was not able to determine a controller to process this request: %s',
