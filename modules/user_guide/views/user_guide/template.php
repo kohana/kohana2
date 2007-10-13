e99d6681476b73c1e8aded9b66f784d04f12308d
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2007-10-13 18:02:31 +0000

    Fixing doctype language in user guide's template. It may not be a superclean solution, but at least it works.

--- modules/user_guide/views/user_guide/template.php
+++ modules/user_guide/views/user_guide/template.php
@@ -1,5 +1,10 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//<?php echo strtoupper(Config::item('core.locale')) ?>" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo Config::item('core.locale') ?>" lang="<?php echo Config::item('core.locale') ?>">
+<?php
+
+$lang = substr(Config::item('core.locale'), 0, 2);
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//<?php echo strtoupper($lang) ?>" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang ?>" lang="<?php echo $lang ?>">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
