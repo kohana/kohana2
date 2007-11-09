7467c51d8ca5a8c86e1d93926d2dc413d091144e
zombor <zombor@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-09 17:56:19 +0000

    Add SQL to create the initial index page.

--- application/views/pages/tutorials/page.php
+++ application/views/pages/tutorials/page.php
@@ -154,7 +154,10 @@ echo geshi_highlight("CREATE TABLE `pages` (
 `child_of` mediumint( 9 ) NOT NULL default '0',
 PRIMARY KEY ( `id` ) ,
 UNIQUE KEY `filename` ( `filename` )
-) ENGINE = MYISAM DEFAULT CHARSET = utf8 PACK_KEYS =0;", 'sql', NULL, TRUE)
+) ENGINE = MYISAM DEFAULT CHARSET = utf8 PACK_KEYS =0;
+
+INSERT INTO `pages` ( `id` , `page_name` , `title` , `content` , `menu` , `filename` , `order` , `date` , `child_of` ) 
+VALUES (  NULL , 'Home', 'Home', 'index', '1', '', '1', '0', '0' );", 'sql', NULL, TRUE)
 
 ?>
 <p>Next we will set up a model to grab some pages out of our database and feed them into our controller.</p>
