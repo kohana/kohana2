64308141843c3354111b2237bd706973593593a2
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-14 22:22:00 +0000

    Updated the sync date.

--- application/controllers/download.php
+++ application/controllers/download.php
@@ -19,7 +19,7 @@ class Download_Controller extends Controller {
 		$content->counter = file_get_contents('application/cache/counter.txt');
 
 		// Pull date
-		$content->sync_date = strtotime('2007/11/12');
+		$content->sync_date = strtotime('2007/11/14');
 
 		// Set up groups array
 		$groups = array
