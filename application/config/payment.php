7a7c76322097e6732dfca594cd6aa70e090c8f3e
zombor <zombor@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-16 05:26:43 +0000

    Add preliminary donate controller and empty view files to come later.

---
+++ application/config/payment.php
@@ -0,0 +1,33 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * File: Credit Card
+ *  Settings related to the CreditCard library.
+ *
+ * Options:
+ *  driver - default driver to use
+ *  test_mode - Turn TEST MODE on or off
+ */
+$config['default'] = array
+(
+	'driver'        => 'Paypal',
+	'test_mode'     => TRUE
+);
+
+/*
+ * PayPal Options:
+ *  API_UserName - the username to use
+ *  API_Password - the password to use
+ *  API_Signature - the api signature to use
+ *  ReturnUrl - the URL to sent the user to after they login with paypal
+ *  CANCELURL - the URL to sent the user to if they cancel the paypal transaction
+ *  CURRENCYCODE - the Currency Code to to the transactions in (What do you want to get paid in?)
+ */
+$config['Paypal'] = array
+(
+	'API_UserName' => '',
+	'API_Password' => '',
+	'API_Signature' => '',
+	'ReturnUrl' => 'http://www.kohanaphp.com/donate/paypal.html',
+	'CANCELURL' => 'http://www.kohanaphp.com/donate/index.html',
+	'CURRENCYCODE' => 'USD'
+);
---
+++ application/controllers/donate.php
@@ -0,0 +1,72 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Donate_Controller extends Controller {
+
+	protected $auto_render = TRUE;
+
+	public function index()
+	{
+		$this->template->set(array
+		(
+			'title'   => 'Donate',
+			'content' => new View('pages/donate/index')
+		));
+	}
+
+	public function paypal()
+	{
+		$payment = new Payment();
+		$session = new Session();
+		if ($amount = $this->input->post('amount')) // They are coming from index()
+		{
+			// Set the payment amount in session for when they return from paypal
+			$session->set(array('donate_amount' => $amount));
+
+			// Set the amount and send em to PayPal
+			$payment->amount = $amount;
+			$payment->process();
+		}
+		else if ($amount = $session->get('amount'))
+		{
+			// Display the final 'order' page
+			$this->template->set(array
+			(
+				'title'   => 'Donate',
+				'content' => new View('pages/donate/paypal', array('payerid' => $this->input->get('payerid')))
+			));
+		}
+		else
+		{
+			// They shouldn't be here
+			$this->index();
+		}
+	}
+
+	public function process_paypal()
+	{
+		$payment->amount = $session->get('donate_amount');
+		$payment->payerid = $this->input->post('payerid');
+
+		// Try and process the payment
+		if ($payment->process())
+		{
+			$this->template->set(array
+			(
+				'title'   => 'Donate',
+				'content' => new View('pages/donate/paypal_success')
+			));
+		}
+		else
+		{
+			$this->template->set(array
+			(
+				'title'   => 'Donate',
+				'content' => new View('pages/donate/paypal_error')
+			));
+		}
+	}
+	public function credit_card()
+	{
+		
+	}
+}
