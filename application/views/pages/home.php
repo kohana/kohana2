5318cbe12e306af7edbf997ea2fae6f5a2e0b917
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-16 21:59:09 +0000

    Added a section to the home back detailing the differences between CI and Kohana.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -27,3 +27,20 @@
 	<li>Originally based on <?php echo html::anchor('http://www.codeigniter.com', 'CodeIgniter') ?></li>
 </ul>
 </div>
+
+<h3 style="clear:both;padding-top:1em;">How is Kohana Different?</h3>
+
+<p>The most commonly asked question about Kohana is "What's the difference between Kohana and CodeIgniter?". Although this is a large topic, we have attempted to outline some of the primary differences and similarities:</p>
+
+<ol>
+<li><strong>Strict PHP5 <abbr title="Object Oriented Programming">OOP</abbr>.</strong> Offers many benefits: visibility protection, automatic class loading, overloading, interfaces, abstracts, and singletons.</li>
+<li><strong>Continues CodeIgniter design patterns.</strong> Anyone who has used CodeIgniter will quickly understand Kohana's structure and design patterns.</li>
+<li><strong>Community, not company, driven.</strong> Kohana is driven by community discussion, ideas, and code. Kohana developers are from all around the world, each with their own talents. This allows a rapid and flexible development cycle that can respond to bugs and requests within hours, instead of days or months.</li>
+<li><strong>GET, POST, COOKIE, <em>and</em> SESSION arrays all work as expected.</strong> Kohana does not limit your access to global data, but offers the same filtering and <abbr title="Cross Site Scripting">XSS</abbr> protection that CodeIgniter does.</li>
+<li><strong>Cascading resources, modules, and inheritance.</strong> Controllers, models, libraries, helpers, and views can be loaded from any location within your <tt>system</tt>, <tt>application</tt>, or <strong>module paths</strong>. Configuration options are inherited and can by dynamically overwritten by each application.</li>
+<li><strong>No namespace conflicts.</strong> Class suffixes, like <tt>_Controller</tt>, are used to prevent namespace conflicts. This allows a Users controller and Users model to both be loaded at the same time.</li>
+<li><strong>True auto-loading of classes.</strong> This includes libraries, controllers, models, and helpers. This is <strong>not</strong> pre-loading, but true dynamic loading of classes, as they are requested.</li>
+<li><strong>Helpers are static classes, not functions.</strong> For exmaple, instead of using <tt>form_open()</tt>, you would use <tt>form::open()</tt>.</li>
+<li><strong>Library drivers and API consistency.</strong> Libraries can use different "drivers" to handle different external <abbr title="Application Programming Interface">API</abbr>s transparently. For example, multiple session storage options are available (database, cookie, and native), but the same interface is used for all of them. This allows new drivers to be developed for existing libraries, which keeps the API consistent and transparent.</li>
+<li><strong>Powerful event handler.</strong> Kohana events can by dynamically added to, replaced, or even removed completely. This allows many changes to Kohana execution process, without modification to existing system code.</li>
+</ol>
