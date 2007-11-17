2ec649ae03a01d295fe02513a36d66c61e9ccf67
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-17 06:09:24 +0000

    Updated the Auth library with some fancy password salting.

---
+++ system/config/auth.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/*
+ * List of 8 random offsets, lower than 30
+ */
+$config['salt_pattern'] = '1, 5, 7, 8, 11, 14, 15, 17, 22, 25, 30';
--- system/libraries/Auth.php
+++ system/libraries/Auth.php
@@ -12,13 +12,100 @@ class Auth_Core {
 	protected $db;
 	protected $users_table = 'users';
 
+	protected $salt_pattern;
+
 	public function __construct()
 	{
 		// Load libraries
 		$this->db = new Database();
 		$this->session = new Session();
 
+		// Get the salt pattern
+		$this->salt_pattern = array_map('trim', explode(',', Config::item('auth.salt_pattern')));
+
+		$plain = 'breakfast';
+
+		$pass = $this->hash_password($plain);
+		$salt = $this->find_salt($pass);
+		$test = $this->hash_password($plain, $salt);
+
+		print 'Hashed: '.Kohana::debug($pass);
+		print 'Rebuilt: '.Kohana::debug($test);
+		print 'Matches: '.Kohana::debug($pass === $test);
+		exit;
+
 		Log::add('debug', 'Auth Library loaded');
+	}
+
+	/*
+	 * Creates a hashed password from a plaintext password, inserting salt
+	 * based on the configured salt pattern.
+	 *
+	 * Parameters:
+	 *  password - plaintext password
+	 *
+	 * Returns:
+	 *  Hashed password string
+	 */
+	protected function hash_password($password, $salt = FALSE)
+	{
+		if ($salt == FALSE)
+		{
+			// Create a salt string, same length as the number of offsets in the pattern
+			$salt = substr(sha1(uniqid(NULL, TRUE)), 0, count($this->salt_pattern));
+		}
+
+		// Password hash that the salt will be inserted into
+		$hash = sha1($salt.$password);
+
+		// Change salt to an array
+		$salt = str_split($salt, 1);
+
+		// Returned password
+		$password = '';
+
+		// Used to calculate the length of splits
+		$last_offset = 0;
+
+		foreach($this->salt_pattern as $offset)
+		{
+			// Split a new part of the hash off
+			$part = substr($hash, 0, $offset - $last_offset);
+
+			// Cut the current part out of the hash
+			$hash = substr($hash, $offset - $last_offset);
+
+			// Add the part to the password, appending the salt character
+			$password .= $part.array_shift($salt);
+
+			// Set the last offset to the current offset
+			$last_offset = $offset;
+		}
+
+		// Return the password, with the remaining hash appended
+		return $password.$hash;
+	}
+
+	/*
+	 * Finds the salt from a password, based on the figured salt pattern.
+	 *
+	 * Parameters:
+	 *  password - hashed password
+	 *
+	 * Returns:
+	 *  Salt string
+	 */
+	protected function find_salt($password)
+	{
+		$salt = '';
+
+		foreach($this->salt_pattern as $i => $offset)
+		{
+			// Find salt characters... take a good long look
+			$salt .= substr($password, $offset + $i, 1);
+		}
+
+		return $salt;
 	}
 
 	/*
