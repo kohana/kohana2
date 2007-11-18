4e5328e61bb850c5f8b638995c9d521d37812bd4
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-18 09:33:41 +0000

    Updated Auth install SQL with role inserts, necessary for login.

--- modules/auth/views/auth/install.php
+++ modules/auth/views/auth/install.php
@@ -1,29 +1,32 @@
 <p>The following tables must be installed in your database: users, roles, and users_roles. If you have no already installed these tables, please run the following query:</p>
 
 <pre>
-CREATE TABLE IF NOT EXISTS roles (
-  id int(11) unsigned NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) unsigned NOT NULL auto_increment,
   `name` varchar(32) NOT NULL,
-  description varchar(255) NOT NULL,
-  PRIMARY KEY  (id),
-  UNIQUE KEY uniq_name (`name`)
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `uniq_name` (`name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS users (
-  id int(11) unsigned NOT NULL auto_increment,
-  email varchar(127) NOT NULL,
-  username varchar(32) NOT NULL default '',
+INSERT INTO `roles` (`id`, `name`, `description`) VALUES(1, 'login', 'Login privileges, granted after account confirmation');
+INSERT INTO `roles` (`id`, `name`, `description`) VALUES(2, 'admin', 'Administrative user, has access to everything.');
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `email` varchar(127) NOT NULL,
+  `username` varchar(32) NOT NULL default '',
   `password` char(50) NOT NULL,
-  logins int(10) unsigned NOT NULL default '0',
-  PRIMARY KEY  (id),
-  UNIQUE KEY uniq_username (username),
-  UNIQUE KEY uniq_email (email)
+  `logins` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `uniq_username` (`username`),
+  UNIQUE KEY `uniq_email` (`email`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS users_roles (
-  user_id int(10) unsigned NOT NULL,
-  role_id int(10) unsigned NOT NULL,
-  PRIMARY KEY  (user_id,role_id)
+CREATE TABLE IF NOT EXISTS `users_roles` (
+  `user_id` int(10) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`user_id`,`role_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 </pre>
 
