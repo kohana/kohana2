da65541c128b201e23322ce7455f2477443573c3
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-19 17:12:32 +0000

    Added Chinese translation.

---
+++ system/i18n/zh_CN/session.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+$lang = array
+(
+	'no_table'                        => 'Session数据表 %s, 不存在.',
+	'driver_not_supported'            => '请求的session驱动, %s, 不存在.',
+	'driver_must_implement_interface' => 'Session必须实现Session_Driver接口.'
+);
