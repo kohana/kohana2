6defd361ecae93a27e832f85b2c63d850538d25d
PugFish <PugFish@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-20 16:07:48 +0000

    Fixed profiler configuration comments

--- system/config/profiler.php
+++ system/config/profiler.php
@@ -1,16 +1,13 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-
 /*
- * Profiler library configuration options.
+ * File: Profiler
+ *  Profiler library configuration options.
  *
  * Options:
- *  driver     - Session driver name
- *  storage    - Session storage parameter, used by drivers
- *  name       - Default session name
- *  validate   - Session parameters to validate
- *  encryption - Encryption key, set to FALSE to disable session encryption
- *  expiration - Number of seconds that each session will last
- *  regenerate - Number of page loads before the session is regenerated
+ *  benchmarks - Show benchmarks
+ *  database   - Show database queries
+ *  post       - Show POST data
+ *  session    - Show session data
  */
 $config = array
 (
