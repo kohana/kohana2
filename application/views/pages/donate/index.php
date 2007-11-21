d1a21d78e9797735003adf0f10609bdeaa2df55c
zombor <zombor@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-21 03:51:10 +0000

    Whoops!

--- application/views/pages/donate/index.php
+++ application/views/pages/donate/index.php
@@ -1,7 +1,10 @@
 <h2>Donating</h2>
 <p>To donate any amount to the Kohana project, enter the amount in the box below and click Submit. You will be sent to PayPal to login, then back to our site to complete the donation.</p>
 <?php echo form::open('donate/paypal') ?>
-<?php echo form::hidden('payerid', 'KohanaUser') ?>
+<h4>Name</h4>
+<p>$<?php echo form::input('name')?></p>
+<h4>Email</h4>
+<p>$<?php echo form::input('email')?></p>
 <h4>Amount</h4>
 <p>$<?php echo form::input('amount') ?> </p>
 <p><?php echo form::submit('submit', 'Donate') ?></p>
--- application/views/pages/donate/paypal.php
+++ application/views/pages/donate/paypal.php
@@ -1,11 +1,6 @@
 <h2>Donate via PayPal</h2>
 <p>Click the confirm button below to finalize your donation.</p>
 <?php echo form::open('donate/process_paypal', array(), array('payerid' => $payerid, 'donate_amount' => $donate_amount))?>
-<h4>Name</h4>
-<p>$<?php echo form::input('name')?></p>
-<h4>Email</h4>
-<p>$<?php echo form::input('email')?></p>
-<h4>Amount</h4>
-<p>$<?php echo form::input('amount')?></p>
+<p>You are donating <strong>$<?php echo $donate_amount; ?></strong></p>
 <p><?php echo form::submit('Confirm')?></p>
 <?php echo form::close()?>
