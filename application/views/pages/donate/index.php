30bb22bb04b383aa0bfb2b2a05941370aaf3e60e
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-21 03:29:01 +0000

    And a bug with the form.

--- application/views/pages/donate/index.php
+++ application/views/pages/donate/index.php
@@ -1,7 +1,8 @@
 <h2>Donating</h2>
 <p>To donate any amount to the Kohana project, enter the amount in the box below and click Submit. You will be sent to PayPal to login, then back to our site to complete the donation.</p>
-<?php echo form::open('donate/paypal')?>
+<?php echo form::open('donate/paypal') ?>
+<?php echo form::hidden('payerid', 'KohanaUser') ?>
 <h4>Amount</h4>
-<p>$<?php echo form::input('amount')?></p>
-<p><?php echo form::submit('submit', 'Donate')?></p>
+<p>$<?php echo form::input('amount') ?> </p>
+<p><?php echo form::submit('submit', 'Donate') ?></p>
 <?php echo form::close()?>
