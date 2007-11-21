0cde4e77ab0adf73a2680819382cd9c850147fb1
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-21 02:31:31 +0000

    Forgot to add these files to the website.

---
+++ application/config/auth.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$config = array
+(
+	'user_table'   => 'users',
+	'role_table'   => 'roles',
+	'hash_method'  => 'sha1',
+	'salt_pattern' => ''
+);
---
+++ application/config/database.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$config['website'] = array
+(
+	'show_errors'   => TRUE,
+	'benchmark'     => FALSE,
+	'persistent'    => FALSE,
+	'connection'    => '',
+	'character_set' => 'utf8',
+	'table_prefix'  => 'kohana_',
+	'object'        =>  TRUE
+);
