f29d44504305b1c144b2eb072368ee8c5e0deaa0
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-27 21:07:25 +0000

    Translation alert!
    Added the i18n files for the Cache library.

---
+++ system/i18n/en_US/cache.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$lang = array
+(
+	'driver_not_supported' => 'The %s cache driver does not exist.',
+	'unwritable'           => 'The configured storage location, <tt>%s</tt>, is not writable.',
+	'resources'            => 'Caching of resources is impossible, because resources cannot be serialized.',
+);
