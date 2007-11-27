4f93d53eb0acd1835b38f3b1a59877864db90b88
Kodeart <Kodeart@6c8b873b-1030-0410-a378-995d7067e127>
2007-11-27 21:37:54 +0000

    Added cache.php translation file

---
+++ system/i18n/mk_MK/cache.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$lang = array
+(
+	'driver_not_supported' => 'Cache драјверот %s не постои.',
+	'unwritable'           => 'Во конфигурираната локација за кеширање, <tt>%s</tt>, не може да се запишува.',
+	'resources'            => 'Кеширање на ресурсите е невозможно, бидејќи ресурсите не можат да се серијализират.',
+);
