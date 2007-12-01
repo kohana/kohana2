9378ec48c1317d94d65cedd7ecb1f23eaf4547ed
zombor <zombor@6c8b873b-1030-0410-a378-995d7067e127>
2007-12-01 01:19:52 +0000

    Add a message to the payment button.

--- application/views/pages/donate/paypal.php
+++ application/views/pages/donate/paypal.php
@@ -2,5 +2,5 @@
 <p>Click the confirm button below to finalize your donation.</p>
 <?php echo form::open('donate/process_paypal', array(), array('payerid' => $payerid, 'donate_amount' => $donate_amount))?>
 <p>You are donating <strong>$<?php echo $donate_amount; ?></strong></p>
-<p><?php echo form::submit('Confirm')?></p>
+<p><?php echo form::submit('Confirm', 'Confirm Your Payment')?></p>
 <?php echo form::close()?>
