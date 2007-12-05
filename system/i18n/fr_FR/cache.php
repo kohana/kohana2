e2455ff15f5cdcf55473b3886d66aa76b27d4cb1
elitemedia <elitemedia@6c8b873b-1030-0410-a378-995d7067e127>
2007-12-05 19:37:34 +0000

    Added missing translation for driver_error key

--- system/i18n/fr_FR/cache.php
+++ system/i18n/fr_FR/cache.php
@@ -5,4 +5,5 @@ $lang = array
 	'driver_not_supported' => 'Le driver du cache %s n\'existe pas.',
 	'unwritable'           => 'Le chemin <tt>%s</tt> configuré pour le cache n\'est pas accessible en écriture.',
 	'resources'            => 'La mise en cache des ressources est impossible car elles n\'ont pas pu être sérialisées.',
+    'driver_error'         => '%s'
 );
