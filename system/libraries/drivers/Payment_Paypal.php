bf757e5c25e71718bf61fb6647cb7d934697b10f
armen <armen@6c8b873b-1030-0410-a378-995d7067e127>
2007-12-12 12:12:26 +0000

    Modified Payment_Paypal driver to abide coding standards.

--- system/libraries/drivers/Payment_Paypal.php
+++ system/libraries/drivers/Payment_Paypal.php
@@ -1,4 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
+
 /**
  * Class: Payment_Paypal_Driver
  *  Provides payment processing with Paypal.
@@ -10,33 +11,37 @@
  *
  * Notes:
  *  You have to set payerid after authorizing with paypal:
- *  >$this->paypment->payerid = $this->input->get('payerid');
+ *  $this->paypment->payerid = $this->input->get('payerid');
  */
-class Payment_Paypal_Driver
-{
-	private $required_fields = array('API_UserName' => FALSE,
-	                                'API_Password'  => FALSE,
-	                                'API_Signature' => FALSE,
-	                                'API_Endpoint'  => TRUE,
-	                                'version'       => TRUE,
-	                                'Amt'           => FALSE,
-	                                'PAYMENTACTION' => TRUE,
-	                                'ReturnUrl'     => FALSE,
-	                                'CANCELURL'     => FALSE,
-	                                'CURRENCYCODE'  => TRUE);
+class Payment_Paypal_Driver {
 
-	private $paypal_values = array('API_UserName'  => '',
-	                               'API_Password'  => '',
-	                               'API_Signature' => '',
-	                               'API_Endpoint'  => 'https://api-3t.paypal.com/nvp',
-	                               'version'       => '3.0',
-	                               'Amt'           => 0,
-	                               'PAYMENTACTION' => 'Sale',
-	                               'ReturnUrl'     => '',
-	                               'CANCELURL'     => '',
-	                               'error_url'     => '',
-	                               'CURRENCYCODE'  => 'USD',
-	                               'payerid'       => '');
+	private $required_fields = array( 
+									  'API_UserName'  => FALSE,
+									  'API_Password'  => FALSE,
+									  'API_Signature' => FALSE,
+									  'API_Endpoint'  => TRUE,
+									  'version'       => TRUE,
+									  'Amt'           => FALSE,
+									  'PAYMENTACTION' => TRUE,
+									  'ReturnUrl'     => FALSE,
+									  'CANCELURL'     => FALSE,
+									  'CURRENCYCODE'  => TRUE
+									);
+
+	private $paypal_values = array( 
+									'API_UserName'  => '',
+									'API_Password'  => '',
+									'API_Signature' => '',
+									'API_Endpoint'  => 'https://api-3t.paypal.com/nvp',
+									'version'       => '3.0',
+									'Amt'           => 0,
+									'PAYMENTACTION' => 'Sale',
+									'ReturnUrl'     => '',
+									'CANCELURL'     => '',
+									'error_url'     => '',
+									'CURRENCYCODE'  => 'USD',
+									'payerid'       => ''
+								  );
 
 	private $paypal_url = '';
 
@@ -49,26 +54,29 @@ class Payment_Paypal_Driver
 	 */
 	function __construct($config)
 	{
-		$this->paypal_values['API_UserName'] = $config['API_UserName'];
-		$this->paypal_values['API_Password'] = $config['API_Password'];
+		$this->paypal_values['API_UserName']  = $config['API_UserName'];
+		$this->paypal_values['API_Password']  = $config['API_Password'];
 		$this->paypal_values['API_Signature'] = $config['API_Signature'];
-		$this->paypal_values['ReturnUrl'] = $config['ReturnUrl'];
-		$this->paypal_values['CANCELURL'] = $config['CANCELURL'];
-		$this->paypal_values['CURRENCYCODE'] = $config['CURRENCYCODE'];
-		$this->paypal_values['API_Endpoint'] = ($config['test_mode']) ? 'https://api.sandbox.paypal.com/nvp' : 'https://api-3t.paypal.com/nvp';
-		$this->paypal_url = ($config['test_mode']) ? 'https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=' : 'https://www.paypal.com/webscr&cmd=_express-checkout&token=';
+		$this->paypal_values['ReturnUrl']     = $config['ReturnUrl'];
+		$this->paypal_values['CANCELURL']     = $config['CANCELURL'];
+		$this->paypal_values['CURRENCYCODE']  = $config['CURRENCYCODE'];
+		$this->paypal_values['API_Endpoint']  = ($config['test_mode']) ? 'https://api.sandbox.paypal.com/nvp' : 'https://api-3t.paypal.com/nvp';
 
-		$this->required_fields['API_UserName'] = !empty($config['API_UserName']);
-		$this->required_fields['API_Password'] = !empty($config['API_Password']);
+		$this->paypal_url = ($config['test_mode']) 
+						  ? 'https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=' 
+						  : 'https://www.paypal.com/webscr&cmd=_express-checkout&token=';
+
+		$this->required_fields['API_UserName']  = !empty($config['API_UserName']);
+		$this->required_fields['API_Password']  = !empty($config['API_Password']);
 		$this->required_fields['API_Signature'] = !empty($config['API_Signature']);
-		$this->required_fields['ReturnUrl'] = !empty($config['ReturnUrl']);
-		$this->required_fields['CANCELURL'] = !empty($config['CANCELURL']);
-		$this->required_fields['CURRENCYCODE'] = !empty($config['CURRENCYCODE']);
+		$this->required_fields['ReturnUrl']     = !empty($config['ReturnUrl']);
+		$this->required_fields['CANCELURL']     = !empty($config['CANCELURL']);
+		$this->required_fields['CURRENCYCODE']  = !empty($config['CURRENCYCODE']);
 
 		$this->curl_config = $config['curl_config'];
 
 		$this->session = new Session();
-		$this->input = new Input();
+		$this->input   = new Input();
 
 		Log::add('debug', 'PayPal Payment Driver Initialized');
 	}
@@ -89,13 +97,17 @@ class Payment_Paypal_Driver
 			{
 				case 'amount':
 					$key = 'Amt';
-					break;
+				break;
 				default:
-					break;
+				break;
 			}
 
 			$this->paypal_values[$key] = $value;
-			if (array_key_exists($key, $this->required_fields) and !empty($value)) $this->required_fields[$key] = TRUE;
+			
+			if (array_key_exists($key, $this->required_fields) AND !empty($value))
+			{
+				$this->required_fields[$key] = TRUE;
+			}
 		}
 	}
 
@@ -111,12 +123,12 @@ class Payment_Paypal_Driver
 	 */
 	function process()
 	{
-		//Make sure the payer ID is set. We do it here because it's not required the first time around.
+		// Make sure the payer ID is set. We do it here because it's not required the first time around.
 		if ($this->session->get('paypal_token') AND isset($this->paypal_values['payerid']))
 		{
 			$this->required_fields['payerid'] = TRUE;
 		}
-		else if ($this->session->get('paypal_token'))
+		elseif ($this->session->get('paypal_token'))
 		{
 			$this->required_fields['payerid'] = FALSE;
 		}
@@ -127,28 +139,32 @@ class Payment_Paypal_Driver
 			$fields = array();
 			foreach ($this->required_fields as $key => $field)
 			{
-				if (!$field) $fields[] = $key;
+				if ( ! $field)
+				{
+					$fields[] = $key;
+				}
 			}
+
 			throw new Kohana_Exception('payment.required', implode(', ', $fields));
 		}
 
-		if (!$this->session->get('paypal_token'))
+		if ( ! $this->session->get('paypal_token'))
 		{
 			$this->paypal_login();
 			return FALSE;
 		}
 
-		//post data for submitting to server
-		$data="&TOKEN=".$this->session->get('paypal_token').
-		      "&PAYERID=".$this->paypal_values['payerid'].
-		      "&IPADDRESS=".urlencode($_SERVER['SERVER_NAME']).
-		      "&Amt=".$this->paypal_values['Amt'].
-		      "&PAYMENTACTION=".$this->paypal_values['PAYMENTACTION'].
-		      "&ReturnUrl=".$this->paypal_values['ReturnUrl'].
-		      "&CANCELURL=".$this->paypal_values['CANCELURL'] .
-		      "&CURRENCYCODE=".$this->paypal_values['CURRENCYCODE']."&COUNTRYCODE=US";
+		// Post data for submitting to server
+		$data = '&TOKEN='.$this->session->get('paypal_token').
+		        '&PAYERID='.$this->paypal_values['payerid'].
+		        '&IPADDRESS='.urlencode($_SERVER['SERVER_NAME']).
+ 		        '&Amt='.$this->paypal_values['Amt'].
+		        '&PAYMENTACTION='.$this->paypal_values['PAYMENTACTION'].
+		        '&ReturnUrl='.$this->paypal_values['ReturnUrl'].
+		        '&CANCELURL='.$this->paypal_values['CANCELURL'] .
+		        '&CURRENCYCODE='.$this->paypal_values['CURRENCYCODE'].'&COUNTRYCODE=US';
 
-		$response = $this->contact_paypal('DoExpressCheckoutPayment', $data);
+		$response    = $this->contact_paypal('DoExpressCheckoutPayment', $data);
 		$nvpResArray = $this->deformatNVP($response);
 
 		return ($nvpResArray['ACK'] == TRUE);
@@ -160,20 +176,22 @@ class Payment_Paypal_Driver
 	 */
 	function paypal_login()
 	{
-		$data = "&Amt=".$this->paypal_values['Amt'].
-		        "&PAYMENTACTION=".$this->paypal_values['PAYMENTACTION'].
-		        "&ReturnURL=".$this->paypal_values['ReturnUrl'].
-		        "&CancelURL=".$this->paypal_values['CANCELURL'];
+		$data = '&Amt='.$this->paypal_values['Amt'].
+		        '&PAYMENTACTION='.$this->paypal_values['PAYMENTACTION'].
+		        '&ReturnURL='.$this->paypal_values['ReturnUrl'].
+		        '&CancelURL='.$this->paypal_values['CANCELURL'];
 
-		$reply = $this->contact_paypal("SetExpressCheckout",$data);
+		$reply = $this->contact_paypal('SetExpressCheckout', $data);
 		$this->session->set(array('reshash' => $reply));
-		$reply = $this->deformatNVP($reply);
-		$ack = strtoupper($reply["ACK"]);
 
-		if($ack=="SUCCESS")
+		$reply = $this->deformatNVP($reply);
+		$ack   = strtoupper($reply['ACK']);
+
+		if ($ack == 'SUCCESS')
 		{
-			$paypal_token = urldecode($reply["TOKEN"]);
-			//Redirect to paypal.com here
+			$paypal_token = urldecode($reply['TOKEN']);
+
+			// Redirect to paypal.com here
 			$this->session->set(array('paypal_token' => $paypal_token));
 
 			// We are off to paypal to login!
@@ -184,6 +202,7 @@ class Payment_Paypal_Driver
 			Log::add('debug', Kohana::debug($reply));
 			url::redirect($this->paypal_values['error_url']);
 		}
+
 		die('<h3>You should not be here. Please email jeremy.bush@kohanaphp.com and include what you did right before seeing this.</h3>');
 	}
 
@@ -200,26 +219,27 @@ class Payment_Paypal_Driver
 	 */
 	function contact_paypal($method, $data)
 	{
-		$final_data   ="METHOD=".urlencode($method).
-		        "&VERSION=".urlencode($this->paypal_values['version']).
-		        "&PWD=".urlencode($this->paypal_values['API_Password']).
-		        "&USER=".urlencode($this->paypal_values['API_UserName']).
-		        "&SIGNATURE=".urlencode($this->paypal_values['API_Signature']).
-		        $data;
+		$final_data = 'METHOD='.urlencode($method).
+		        	  '&VERSION='.urlencode($this->paypal_values['version']).
+		        	  '&PWD='.urlencode($this->paypal_values['API_Password']).
+		        	  '&USER='.urlencode($this->paypal_values['API_UserName']).
+		        	  'SIGNATURE='.urlencode($this->paypal_values['API_Signature']).$data;
+
 		$ch = curl_init($this->paypal_values['API_Endpoint']);
 
 		// Set custom curl options
 		curl_setopt_array($ch, $this->curl_config);
 		curl_setopt($ch, CURLOPT_POST, 1);
 
-		//setting the nvpreq as POST FIELD to curl
-		curl_setopt($ch,CURLOPT_POSTFIELDS,$final_data);
-		//getting response from server
-		$response =  curl_exec($ch);
+		// Setting the nvpreq as POST FIELD to curl
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $final_data);
+
+		// Getting response from server
+		$response = curl_exec($ch);
 
 		if (curl_errno($ch))
 		{
-			// moving to error page to display curl errors
+			// Moving to error page to display curl errors
 			$this->session->set_flash(array('curl_error_no' => curl_errno($ch), 'curl_error_msg' => curl_error($ch)));
 			url::redirect($this->error_url);
 		}
@@ -244,23 +264,27 @@ class Payment_Paypal_Driver
 	 */
 	function deformatNVP($nvpstr)
 	{
-		$intial=0;
+		$intial   = 0;
 		$nvpArray = array();
 
-		while(strlen($nvpstr))
+		while (strlen($nvpstr))
 		{
-			//postion of Key
-			$keypos= strpos($nvpstr,'=');
-			//position of value
-			$valuepos = strpos($nvpstr,'&') ? strpos($nvpstr,'&'): strlen($nvpstr);
+			// Postion of Key
+			$keypos = strpos($nvpstr, '=');
 
-			/*getting the Key and Value values and storing in a Associative Array*/
-			$keyval=substr($nvpstr,$intial,$keypos);
-			$valval=substr($nvpstr,$keypos+1,$valuepos-$keypos-1);
-			//decoding the respose
-			$nvpArray[urldecode($keyval)] =urldecode( $valval);
-			$nvpstr=substr($nvpstr,$valuepos+1,strlen($nvpstr));
+			// Position of value
+			$valuepos = strpos($nvpstr, '&') ? strpos($nvpstr, '&') : strlen($nvpstr);
+
+			// Getting the Key and Value values and storing in a Associative Array
+			$keyval = substr($nvpstr, $intial, $keypos);
+			$valval = substr($nvpstr, $keypos + 1, $valuepos - $keypos - 1);
+
+			// Decoding the respose
+			$nvpArray[urldecode($keyval)] = urldecode( $valval);
+			
+			$nvpstr = substr($nvpstr, $valuepos + 1, strlen($nvpstr));
 		}
+
 		return $nvpArray;
 	}
 }
