8161fa499ee6446ad704386d76630045b9c03591
zombor <zombor@6c8b873b-1030-0410-a378-995d7067e127>
2007-12-12 20:38:49 +0000

    Change db caching off by default in the library too.

--- system/libraries/Database.php
+++ system/libraries/Database.php
@@ -24,7 +24,7 @@ class Database_Core {
 		'character_set' => 'utf8',
 		'table_prefix'  => '',
 		'object'        => TRUE,
-		'cache'         => TRUE
+		'cache'         => FALSE
 	);
 
 	// Database driver object
