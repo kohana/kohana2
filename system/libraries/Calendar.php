e8e0477efa0664324ba1ce87bfe8679802af8ecc
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2007-12-26 14:43:14 +0000

    Updates to Calendar

--- system/libraries/Calendar.php
+++ system/libraries/Calendar.php
@@ -15,6 +15,12 @@ class Calendar_Core {
 	protected $month;
 	protected $year;
 
+	// Start the calendar on Sunday by default
+	public $week_start = 3;
+
+	// Event data
+	public $events;
+
 	/**
 	 * Create a new Calendar instance. A month and year can be specified.
 	 * By default, the current month and year are used.
@@ -23,7 +29,7 @@ class Calendar_Core {
 	 * @param   integer  year number
 	 * @return  void
 	 */
-	public function __construct($month = NULL, $year = NULL)
+	public function __construct($month = NULL, $year = NULL, $start_monday = NULL)
 	{
 		empty($month) and $month = date('n'); // Current month
 		empty($year)  and $year  = date('Y'); // Current year
@@ -31,6 +37,17 @@ class Calendar_Core {
 		// Set the month and year
 		$this->month = (int) $month;
 		$this->year  = (int) $year;
+
+		// Some locales start the week on Monday, not Sunday.
+		($start_monday === TRUE) and $this->week_start = 1;
+	}
+
+	public function events()
+	{
+		if (func_num_args() === 0)
+			return;
+
+		$this->events = func_get_args();
 	}
 
 	/**
@@ -57,13 +74,13 @@ class Calendar_Core {
 		// Number of days added. When this reaches 7, start a new month
 		$days = 0;
 
-		if (($w = (int) date('w', $first)) > 0)
+		if (($w = (int) date('w', $first)) > $this->week_start)
 		{
 			// Number of days in the previous month
 			$n = (int) date('t', mktime(1, 0, $this->month - 1, 1, $this->year));
 
 			// i = number of day, t = number of days to pad
-			for($i = $n - $w, $t = $w; $t > 0; $t--, $i++)
+			for($i = $n - $w, $t = $w - $this->week_start; $t > 0; $t--, $i++)
 			{
 				// Add previous month padding days
 				$week[] = array($i, FALSE);
@@ -86,7 +103,7 @@ class Calendar_Core {
 			$days++;
 		}
 
-		if (($w = (int) date('w', $last)) < 6)
+		if (($w = (int) date('w', $last) - $this->week_start) < 6 )
 		{
 			// i = number of day, t = number of days to pad
 			for ($i = 1, $t = 6 - $w; $t > 0; $t--, $i++)
@@ -110,9 +127,10 @@ class Calendar_Core {
 	{
 		$view =  new View('kohana_calendar', array
 		(
-			'month' => $this->month,
-			'year'  => $this->year,
-			'weeks' => $this->weeks(),
+			'month'  => $this->month,
+			'year'   => $this->year,
+			'weeks'  => $this->weeks(),
+			'events' => $this->events,
 		));
 
 		return $view->render();
--- system/views/kohana_calendar.php
+++ system/views/kohana_calendar.php
@@ -1,4 +1,8 @@
 <?php
+
+$first_day = mktime(1, 0, 0, $month, 1, $year);
+$today = (date('Y/m') === date('Y/m', $first_day)) ? date('j') : FALSE;
+
 /**
  * @todo This needs to be moved to an i18n file, or be configured in the library.
  */
@@ -10,13 +14,14 @@ $headings = array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
 ?><style type="text/css">
 table.calendar { text-align: right; }
 table.calendar caption { font-size: 1.5em; padding: 0.2em; }
-table.calendar th, table.calendar td { padding: 0.2em; background: #fff; }
+table.calendar th, table.calendar td { padding: 0.2em; background: #fff; border: 0; }
 table.calendar td:hover { background: #ddf; }
-table.calendar td.prev-next { background: #fff; color: #666; }
+table.calendar td.prev-next { background: #ccc; color: #999; }
+table.calendar td.today { color: #800; }
 </style>
 
 <table class="calendar">
-<caption><?php echo strftime('%B %Y', mktime(1, 0, 0, $month, 1, $year)) ?></caption>
+<caption><?php echo strftime('%B %Y', $first_day) ?></caption>
 <tr>
 <?php foreach ($headings as $day): ?>
 <th><?php echo $day ?></th>
@@ -29,12 +34,21 @@ table.calendar td.prev-next { background: #fff; color: #666; }
 <td class="prev-next"><?php echo $day[0] ?></td>
 <?php else: ?>
 <?php
+
+if ($today > 0 AND $day[0] == $today)
+{
+	$class = ' class="today"';
+}
+else
+{
+	$class = '';
+}
 /**
  * @todo Need to add assignable stuff to this. For example, making certain dates into links.
  */
 ?>
-<td><?php echo $day[0] ?></td>
-<?php endif ?>
+<td<?php echo $class ?>><?php echo $day[0] ?></td>
+<?php endif; ?>
 <?php endforeach ?>
 </tr>
 <?php endforeach ?>
