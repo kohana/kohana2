c6c8b5f7a4e65f87008cbacf09546901048f0752
gregmac <gregmac@6c8b873b-1030-0410-a378-995d7067e127>
2007-12-27 17:54:38 +0000

    Initial verison of PdoSqlite driver, based on http://forum.kohanaphp.com/index.php/topic,338.0.html

---
+++ system/libraries/drivers/Database_Pdosqlite.php
@@ -0,0 +1,746 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * Class: Database_PdoSqlite_Driver
+ *  Provides specific database items for Sqlite.
+ *
+ * Connection string should be, eg: "pdosqlite://path/to/database.db"
+ *
+ * Version 1.0 alpha
+ *  author    - Doutu, updated by gregmac
+ *  copyright - (c) BSD
+ *  license   - <no>
+ */
+
+class Database_Pdosqlite_Driver extends Database_Driver {
+
+	// Database connection link
+	protected $link;
+	protected $db_config;
+
+	/*
+	 * Constructor: __construct
+	 *  Sets up the config for the class.
+	 *
+	 * Parameters:
+	 *  config - database configuration
+	 *
+	 */
+	public function __construct($config)
+	{
+		$this->db_config = $config;
+
+		Log::add('debug', 'PDO:Sqlite Database Driver Initialized');
+	}
+
+	public function connect()
+	{
+		// Import the connect variables
+		extract($this->db_config['connection']);
+
+		try {
+			$this->link = ($this->db_config['persistent'] == TRUE)
+						? new PDO ('sqlite:'.$socket.$database, $user, $pass, array(PDO::ATTR_PERSISTENT => true))
+						: new PDO ('sqlite:'.$socket.$database, $user, $pass);
+			$this->link->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
+			$this->link->query('PRAGMA count_changes=1;');
+
+			if ($charset = $this->db_config['character_set'])
+			{
+				$this->set_charset($charset);
+			}
+		} catch (PDOException $e) {
+			throw new Kohana_Database_Exception('database.error', $e->getMessage());
+		}
+		if ( $this->link ) {
+			Log::add('debug','connection ok');
+		}
+
+		return $this->link;
+	}
+
+	public function query($sql)
+	{
+		//Log::add('debug','query:'.$sql);
+		try {
+			$sth = $this->link->query($sql);
+		} catch (PDOException $e) {
+			throw new Kohana_Database_Exception('database.error', $e->getMessage());
+			return FALSE;
+		}
+		return new Pdosqlite_Result($sth, $this->link, $this->db_config['object'], $sql);
+	}
+
+	public function delete($table, $where)
+	{
+		return 'DELETE FROM '.$this->escape_table($table).' WHERE '.implode(' ', $where);
+	}
+
+	public function update($table, $values, $where)
+	{
+		foreach($values as $key => $val)
+		{
+			$valstr[] = $this->escape_column($key)." = ".$val;
+		}
+		return 'UPDATE '.$this->escape_table($table).' SET '.implode(', ', $valstr).' WHERE '.implode(' ',$where);
+	}
+
+	public function set_charset($charset)
+	{
+		$this->link->query('PRAGMA encoding = '.$this->escape_str($charset));
+	}
+
+	public function escape_table($table)
+	{
+		return '`'.str_replace('.', '`.`', $table).'`';
+	}
+
+	public function escape_column($column)
+	{
+		if (strtolower($column) == 'count(*)' OR $column == '*')
+			return $column;
+
+		// This matches any modifiers we support to SELECT.
+		if ( ! preg_match('/\b(?:rand|all|distinct(?:row)?|high_priority|sql_(?:small_result|b(?:ig_result|uffer_result)|no_cache|ca(?:che|lc_found_rows)))\s/i', $column))
+		{
+			if (stripos($column, ' AS ') !== FALSE)
+			{
+				// Force 'AS' to uppercase
+				$column = str_ireplace(' AS ', ' AS ', $column);
+
+				// Runs escape_column on both sides of an AS statement
+				$column = array_map(array($this, __FUNCTION__), explode(' AS ', $column));
+
+				// Re-create the AS statement
+				return implode(' AS ', $column);
+			}
+
+			return preg_replace('/[^.*]+/', '`$0`', $column);
+		}
+
+		$parts = explode(' ', $column);
+		$column = '';
+
+		for ($i = 0, $c = count($parts); $i < $c; $i++)
+		{
+			// The column is always last
+			if ($i == ($c - 1))
+			{
+				$column .= preg_replace('/[^.*]+/', '`$0`', $parts[$i]);
+			}
+			else // otherwise, it's a modifier
+			{
+				$column .= $parts[$i].' ';
+			}
+		}
+		return $column;
+	}
+
+	public function where($key, $value, $type, $num_wheres, $quote)
+	{
+		$prefix = ($num_wheres == 0) ? '' : $type;
+
+		if ($quote === -1)
+		{
+			$value = '';
+		}
+		else
+		{
+			if ($value === NULL)
+			{
+				if ( ! $this->has_operator($key))
+				{
+					$key .= ' IS';
+				}
+
+				$value = ' NULL';
+			}
+			elseif (is_bool($value))
+			{
+				if ( ! $this->has_operator($key))
+				{
+					$key .= ' =';
+				}
+
+				$value = ($value == TRUE) ? ' 1' : ' 0';
+			}
+			else
+			{
+				if ( ! $this->has_operator($key))
+				{
+					$key = $this->escape_column($key).' =';
+				}
+				else
+				{
+					preg_match('/^(.+?)([<>!=]+|\bIS(?:\s+NULL))\s*$/i', $key, $matches);
+					$key = $this->escape_column(trim($matches[1])).' '.trim($matches[2]);
+				}
+
+				$value = ' '.(($quote == TRUE) ? $this->escape($value) : $value);
+			}
+		}
+
+		return $prefix.$key.$value;
+	}
+
+	public function like($field, $match = '', $type = 'AND ', $num_likes)
+	{
+		$prefix = $num_likes == 0 ? '' : $type;
+
+		$match = (substr($match, 0, 1) == '%' OR substr($match, (strlen($match)-1), 1) == '%')
+			   ? $this->escape_str($match)
+			   : '%'.$this->escape_str($match).'%';
+
+		return $prefix." ".$this->escape_column($field)." LIKE '".$match . "'";
+	}
+	
+	public function regex($field, $match = '', $type = 'AND ', $num_regexs)
+	{
+		throw new Kohana_Database_Exception('database.not_implemented', __FUNCTION__);
+	}
+
+	public function notregex($field, $match = '', $type = 'AND ', $num_regexs)
+	{
+		throw new Kohana_Database_Exception('database.not_implemented', __FUNCTION__);
+	}
+
+	public function merge($table, $keys, $values)
+	{
+		throw new Kohana_Database_Exception('database.not_implemented', __FUNCTION__);
+	}
+	
+	public function insert($table, $keys, $values)
+	{
+		// Escape the column names
+		foreach ($keys as $key => $value)
+		{
+			$keys[$key] = $this->escape_column($value);
+		}
+		return 'INSERT INTO '.$this->escape_table($table).' ('.implode(', ', $keys).') VALUES ('.implode(', ', $values).')';
+	}
+
+	public function limit($limit, $offset = 0)
+	{
+		return 'LIMIT '.$offset.', '.$limit;
+	}
+
+	public function compile_select($database)
+	{
+		$sql = ($database['distinct'] == TRUE) ? 'SELECT DISTINCT ' : 'SELECT ';
+		$sql .= (count($database['select']) > 0) ? implode(', ', $database['select']) : '*';
+
+		if (count($database['from']) > 0)
+		{
+			$sql .= "\nFROM ";
+			$sql .= implode(', ', $database['from']);
+		}
+
+		if (count($database['join']) > 0)
+		{
+			$sql .= ' '.implode("\n", $database['join']);
+		}
+
+		if (count($database['where']) > 0)
+		{
+			$sql .= "\nWHERE ";
+		}
+
+		$sql .= implode("\n", $database['where']);
+
+		if (count($database['groupby']) > 0)
+		{
+			$sql .= "\nGROUP BY ";
+			$sql .= implode(', ', $database['groupby']);
+		}
+
+		if (count($database['having']) > 0)
+		{
+			$sql .= "\nHAVING ";
+			$sql .= implode("\n", $database['having']);
+		}
+
+		if (count($database['orderby']) > 0)
+		{
+			$sql .= "\nORDER BY ";
+			$sql .= implode(', ', $database['orderby']);
+		}
+
+		if (is_numeric($database['limit']))
+		{
+			$sql .= "\n";
+			$sql .= $this->limit($database['limit'], $database['offset']);
+		}
+
+		return $sql;
+	}
+
+	public function has_operator($str)
+	{
+		return (bool) preg_match('/[<>!=]|\sIS\s+(?:NOT\s+)?NULL\b/i', trim($str));
+	}
+
+	public function escape($value)
+	{
+		switch (gettype($value))
+		{
+			case 'string':
+				$value = "'".$this->escape_str($value)."'";
+				break;
+			case 'boolean':
+				$value = (int) $value;
+			break;
+			default:
+				$value = ($value === NULL) ? 'NULL' : $value;
+			break;
+		}
+
+		return (string) $value;
+	}
+
+	public function escape_str($str)
+	{
+		if(function_exists('sqlite_escape_string')) {
+			$res = sqlite_escape_string($str);
+		} else {
+			$res = str_replace("'", "''", $str);
+		}
+		return $res;
+	}
+
+	public function list_tables()
+	{
+		$sql = "SELECT `name` FROM `sqlite_master` WHERE `type`='table' ORDER BY `name`;";
+		try{
+			$result = $this->query($sql)->result(FALSE, PDO::FETCH_ASSOC);
+			$retval = array();
+			foreach($result as $row)
+			{
+				$retval[] = current($row);
+			}
+		} catch (PDOException $e) {
+			throw new Kohana_Database_Exception('database.error', $e->getMessage());
+			return FALSE;
+		}
+		return $retval;
+	}
+
+	public function show_error()
+	{
+		$err = $this->link->errorInfo();
+		return isset($err[2]) ? $err[2] : 'Unknow error!';
+	}
+
+	public function list_fields($table, $query = FALSE)
+	{
+		static $tables;
+		if (is_object($query))
+		{
+			if (empty($tables[$table]))
+			{
+				$tables[$table] = array();
+
+				foreach($query->result() as $row)
+				{
+					$tables[$table][] = $row->name;
+				}
+			}
+
+			return $tables[$table];
+		}
+		else
+		{
+			return 'PRAGMA table_info('.$this->escape_table($table).')';
+		}
+	}
+
+	public function field_data($table)
+	{
+		Log::add('error', 'This method is under developing');
+	}
+	/**
+	 * Version number query string
+	 *
+	 * @access	public
+	 * @return	string
+	 */
+	function version()
+	{
+		return $this->link->getAttribute(constant("PDO::ATTR_SERVER_VERSION"));
+	}
+
+} // End Database_PdoSqlite_Driver Class
+
+/*
+ * Class: PdoSqlite_Result
+ *  The result class for Sqlite queries.
+ *
+ * Version 1.0 alpha
+ *  author    - Doutu, updated by gregmac
+ *  copyright - (c) BSD
+ *  license   - <no>
+ */
+class Pdosqlite_Result implements Database_Result, ArrayAccess, Iterator, Countable {
+
+	// Result resource
+	protected $result = NULL;
+
+	// Total rows and current row
+	protected $total_rows  = FALSE;
+	protected $current_row = FALSE;
+
+	// Insert id
+	protected $insert_id = FALSE;
+
+	// Data fetching types
+	protected $fetch_type  = PDO::FETCH_OBJ;
+	protected $return_type = PDO::FETCH_ASSOC;
+
+	/*
+	 * Constructor: __construct
+	 *  Sets up the class.
+	 *
+	 * Parameters:
+	 *  result - result resource
+	 *  link   - database resource link
+	 *  object - return objects or arrays
+	 *  sql    - sql query that was run
+	 *
+	 */
+	public function __construct($result, $link, $object = TRUE, $sql)
+	{
+		if (is_object($result) OR $result = $link->query($sql))
+		{
+			if (preg_match('/^SELECT|PRAGMA|EXPLAIN/i', $sql))
+			{
+				//Log::add('debug','it was a SELECT, SHOW, DESCRIBE, EXPLAIN query');
+				$this->result = $result;
+				$this->current_row = 0;
+				$this->total_rows = $result->rowCount(); // Do NOT work for sqlite now!!!!!!!!!!
+				$this->fetch_type = ($object === TRUE) ? PDO::FETCH_OBJ : PDO::FETCH_ASSOC;
+			}
+			elseif (preg_match('/^DELETE|INSERT|UPDATE/i', $sql))
+			{
+				//Log::add('debug','Its an DELETE, INSERT, REPLACE, or UPDATE query');
+				$this->insert_id  = $link->lastInsertId();
+			}
+		}
+		else
+		{
+			// SQL error
+			throw new Kohana_Database_Exception('database.error', $link->errorInfo().' - '.$sql);
+		}
+		// Set result type
+		//$this->result($object);
+	}
+
+	/*
+	 * Destructor: __destruct
+	 *  Magic __destruct function, frees the result.
+	 */
+	public function __destruct()
+	{
+		if (is_object($this->result))
+		{
+			$this->result->closeCursor();
+			$this->result = NULL;
+		}
+	}
+
+	public function result($object = TRUE, $type = PDO::FETCH_BOTH)
+	{
+		$rows = array();
+
+		$this->fetch_type = (bool) $object ? PDO::FETCH_OBJ : PDO::FETCH_BOTH;
+
+		if ($this->fetch_type == PDO::FETCH_OBJ)
+		{
+			$this->return_type = class_exists($type, FALSE) ? $type : 'stdClass';
+		}
+		else
+		{
+			$this->return_type = $type;
+		}
+		return $this;
+		try {
+			//Log::add('debug', 'fetch_type->'.$this->fetch_type);
+			$rows = $this->result->fetchAll($this->fetch_type);
+		} catch(PDOException $e) {
+			throw new Kohana_Database_Exception('database.error', $e->getMessage());
+			return FALSE;
+    	}
+		/**
+		*PHP manual for PDO nom_rows :
+		* "For most databases, PDOStatement::rowCount() does not return the number of rows affected by
+		*a SELECT statement. Instead, use PDO::query() to issue a SELECT COUNT(*) statement with the
+		*same predicates as your intended SELECT statement, then use PDOStatement::fetchColumn() to
+		*retrieve the number of rows that will be returned.
+		*
+		*/
+		Log::add('debug','rowCount:'.$this->result->rowCount());
+		if($this->fetch_type==PDO::FETCH_BOTH)
+		{
+			$this->total_rows = sizeof($rows)/2;
+		}
+		else
+		{
+			$this->total_rows = sizeof($rows);
+		}
+		return $rows;
+	}
+
+	public function result_array($object = NULL, $type = PDO::FETCH_ASSOC)
+	{
+		$rows = array();
+
+		if (is_string($object))
+		{
+			$fetch = $object;
+		}
+		elseif (is_bool($object))
+		{
+			if ($object === TRUE)
+			{
+				$fetch = PDO::FETCH_OBJ;
+
+				// NOTE - The class set by $type must be defined before fetching the result,
+				// autoloading is disabled to save a lot of stupid overhead.
+				$type = class_exists($type, FALSE) ? $type : 'stdClass';
+			}
+			else
+			{
+				$fetch = PDO::FETCH_OBJ;
+			}
+		}
+		else
+		{
+			// Use the default config values
+			$fetch = $this->fetch_type;
+
+			if ($fetch == PDO::FETCH_OBJ)
+			{
+				$type = class_exists($type, FALSE) ? $type : 'stdClass';
+			}
+		}
+		try {
+			while ($row = $this->result->fetch($fetch))
+			{
+				$rows[] = $row;
+			}
+		} catch(PDOException $e) {
+			throw new Kohana_Database_Exception('database.error', $e->getMessage());
+			return FALSE;
+    	}
+		return $rows;
+	}
+
+	public function insert_id()
+	{
+		return $this->insert_id;
+	}
+
+	public function list_fields()
+	{
+		//~ This function only work correctly after you execute a query,
+		//~ AND BEFORE you fetch the query result!!
+		//~ You should really use Database_PdoSqlite::list_fields instead of PdoSqlite_Result::list_fields()
+		Log::add('debug','If Sqlite_Result::list_fields() do NOT work as what you expect,read the method\'s comment plz');
+
+		$field_names = array();
+		for ($i = 0; $i<$this->result->columnCount(); $i++)
+		{
+			$colInfo = $this->result->getColumnMeta($i);
+			$field_names[] = $colInfo['name'];
+		}
+		return $field_names;
+	}
+	// End Interface
+
+	// Interface: Countable
+	/*
+	 * Method: count
+	 *  Counts the number of rows in the result set.
+	 *
+	 * Returns:
+	 *  The number of rows in the result set
+	 *
+	 */
+	public function count()
+	{
+		//~ Now only work after calling result() or result_array();
+		Log::add('debug', 'Now only work after calling result() or result_array()');
+		return $this->total_rows;
+	}
+
+	public function num_rows()
+	{
+		Log::add('error', 'You should really be using "count($result)" instead of "$result->num_rows()". Fix your code!');
+
+		return $this->total_rows;
+	}
+	// End Interface
+
+	// Interface: ArrayAccess
+	/*
+	 * Method: offsetExists
+	 *  Determines if the requested offset of the result set exists.
+	 *
+	 * Parameters:
+	 *  offset - offset id
+	 *
+	 * Returns:
+	 *  TRUE if the offset exists, FALSE otherwise
+	 *
+	 */
+	public function offsetExists($offset)
+	{
+		if ($this->total_rows > 0)
+		{
+			$min = 0;
+			$max = $this->total_rows - 1;
+
+			return ($offset < $min OR $offset > $max) ? FALSE : TRUE;
+		}
+
+		return FALSE;
+	}
+
+	/*
+	 * Method: offsetGet
+	 *  Retreives the requested query result offset.
+	 *
+	 * Parameters:
+	 *  offset - offset id
+	 *
+	 * Returns:
+	 *  The query row
+	 *
+	 */
+	public function offsetGet($offset)
+	{
+		$row = array();
+		try {
+			$row = $this->result->fetch(PDO::FETCH_OBJ, PDO::FETCH_ORI_NEXT, $offset);
+		} catch(PDOException $e) {
+			throw new Kohana_Database_Exception('database.error', $e->getMessage());
+			return FALSE;
+    	}
+		return $row;
+	}
+
+	/*
+	 * Method: offsetSet
+	 *  Sets the offset with the provided value. Since you can't modify query result sets, this function just throws an exception.
+	 *
+	 * Parameters:
+	 *  offset - offset id
+	 *  value  - value to set
+	 *
+	 * Returns:
+	 *  <Kohana_Database_Exception> object
+	 *
+	 */
+	public function offsetSet($offset, $value)
+	{
+		throw new Kohana_Database_Exception('database.result_read_only');
+	}
+
+	/*
+	 * Method: offsetUnset
+	 *  Unsets the offset. Since you can't modify query result sets, this function just throws an exception.
+	 *
+	 * Parameters:
+	 *  offset - offset id
+	 *
+	 * Returns:
+	 *  <Kohana_Database_Exception> object
+	 *
+	 */
+	public function offsetUnset($offset)
+	{
+		throw new Kohana_Database_Exception('database.result_read_only');
+	}
+	// End Interface
+
+	// Interface: Iterator
+	/*
+	 * Method: current
+	 *  Retreives the current result set row.
+	 *
+	 * Returns:
+	 *  The current result row (type based on <PdoSqlite_result.result>)
+	 *
+	 */
+	public function current()
+	{
+		return $this->offsetGet($this->current_row);
+	}
+
+	/*
+	 * Method: key
+	 *  Retreives the current row id.
+	 *
+	 * Returns:
+	 *  The current result row id
+	 *
+	 */
+	public function key()
+	{
+		return $this->current_row;
+	}
+
+	/*
+	 * Method: next
+	 *  Moves the result pointer ahead one.
+	 *
+	 * Returns:
+	 *  The next row id
+	 *
+	 */
+	public function next()
+	{
+		return ++$this->current_row;
+	}
+
+	/*
+	 * Method: next
+	 *  Moves the result pointer back one.
+	 *
+	 * Returns:
+	 *  The previous row id
+	 *
+	 */
+	public function prev()
+	{
+		return --$this->current_row;
+	}
+
+	/*
+	 * Method: rewind
+	 *  Moves the result pointer to the beginning of the result set.
+	 *
+	 * Returns:
+	 *  0
+	 *
+	 */
+	public function rewind()
+	{
+		//~ To request a scrollable cursor for your PDOStatement object,
+		//~ you must set the PDO::ATTR_CURSOR attribute to PDO::CURSOR_SCROLL
+		//~ when you prepare the SQL statement with PDO->prepare().
+		Log::add('error','this method do not work now,please read the comment of that.');
+		//return $this->current_row = 0;
+	}
+
+	/*
+	 * Method: valid
+	 *  Determines if the current result pointer is valid.
+	 *
+	 * Returns:
+	 *  TRUE if the pointer is valid, FALSE otherwise
+	 *
+	 */
+	public function valid()
+	{
+		return $this->offsetExists($this->current_row);
+	}
+	// End Interface
+} // End PdoSqlite_Result Class
