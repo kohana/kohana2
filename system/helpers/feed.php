da2a896a020e005fb18c29a3cf26bfb9a7767d21
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-01-02 03:06:25 +0000

    Updated feed::parse() to use SimpleXML instead of DOMDocument.

--- system/helpers/feed.php
+++ system/helpers/feed.php
@@ -26,51 +26,34 @@ class feed_Core {
 		// Make limit an integer
 		$limit = (int) $limit;
 
-		// Create a DOM parser
-		$parser = new DOMDocument;
+		// Disable error reporting while opening the feed
+		$ER = error_reporting(0);
 
-		if (is_file($feed))
+		// Allow loading by filename or raw XML string.
+		$feed = is_file($feed) ? simplexml_load_file($feed) : simplexml_load_string($feed);
+
+		// Restore error reporting
+		error_reporting($ER);
+
+		// Feed could not be loaded
+		if ($feed === FALSE)
+			return array();
+
+		// Detect the feed type. RSS 1.0/2.0 and Atom 1.0 are supported.
+		$feed = isset($feed->channel) ? $feed->xpath('//item') : $feed->entry;
+
+		$i = 0;
+		$items = array();
+
+		foreach ($feed as $item)
 		{
-			// Load by file
-			$parser->load($feed);
-		}
-		else
-		{
-			// Load XML in the string
-			$parser->loadXML($feed);
-		}
-
-		// Reset the feed to an empty array
-		$feed = array();
-
-		// Choose the name of the entry (RSS or Atom). Atom feeds use <feed>
-		// as the XML container, RSS uses <rdf>.
-		$entry = ($parser->getElementsByTagName('feed')->length > 0) ? 'entry' : 'item';
-
-		// Parse each of the RSS items
-		foreach($parser->getElementsByTagName($entry) as $index => $node)
-		{
-			if ($limit > 0 AND $index >= $limit)
+			if ($i++ === $limit)
 				break;
 
-			// Create a new data set
-			$item = array();
-
-			// Get all the of fields in the feed item
-			foreach($node->childNodes as $node)
-			{
-				// Only add XML element nodes
-				if ($node->nodeType === XML_ELEMENT_NODE)
-				{
-					$item[$node->nodeName] = $node->nodeValue;
-				}
-			}
-
-			// Add the item data to the feed
-			$feed[] = $item;
+			$items[] = (array) $item;
 		}
 
-		return $feed;
+		return $items;
 	}
 
 } // End rss
