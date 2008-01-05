a8f78333e951290c0a488df2bef1499d8a89caac
Edy <Edy@6c8b873b-1030-0410-a378-995d7067e127>
2008-01-05 15:38:28 +0000

    updated de_DE due to recent changes

--- system/i18n/de_DE/core.php
+++ system/i18n/de_DE/core.php
@@ -7,9 +7,11 @@ $lang = array
 	'invalid_method'        => 'Ungültige Methode <tt>%s</tt> aufgerufen in <tt>%s</tt>.',
 	'cannot_write_log'      => 'Ihre Einstellung log.directory in der Konfiguration verweist nicht auf ein beschreibbares Verzeichnis.',
 	'resource_not_found'    => 'Aufgerufenes %s, <tt>%s</tt>, konnte nicht gefunden werden.',
+	'invalid_filetype'      => 'Die Dateiendung <tt>.%s</tt>, ist in Ihrer View-Konfigurationsdatei nicht erlaubt.',
 	'no_default_route'      => 'Erstellen Sie bitte eine Standardroute <tt>config/routes.php</tt>.',
 	'no_controller'         => 'Kohana gelang es nicht einen Controller zu finden, um diesen Aufruf zu verarbeiten: %s',
 	'page_not_found'        => 'Die aufgerufene Seite, <tt>%s</tt>, konnte nicht gefunden werden.',
 	'stats_footer'          => 'Seite geladen in {execution_time} Sekunden bei {memory_usage} Speichernutzung. Generiert von Kohana v{kohana_version}.',
-	'error_message'         => 'Fehler aufgetreten in <strong>Zeile %s</strong> in <strong>%s</strong>.'
+	'error_message'         => 'Fehler aufgetreten in <strong>Zeile %s</strong> in <strong>%s</strong>.',
+	'stack_trace'           => 'Stack Trace'
 );
