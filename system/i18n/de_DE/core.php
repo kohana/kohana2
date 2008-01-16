1dd466be02a006e3a3fb435e95fdf2ecd1b0adea
Edy <Edy@6c8b873b-1030-0410-a378-995d7067e127>
2008-01-16 20:46:36 +0000

    core language file update in de_DE according to r1720

--- system/i18n/de_DE/core.php
+++ system/i18n/de_DE/core.php
@@ -6,12 +6,20 @@ $lang = array
 	'uncaught_exception'    => 'Unerwarteter Fehler vom Typ %s: %s in %s in Zeile %s',
 	'invalid_method'        => 'Ungültige Methode <tt>%s</tt> aufgerufen in <tt>%s</tt>.',
 	'cannot_write_log'      => 'Ihre Einstellung log.directory in der Konfiguration verweist nicht auf ein beschreibbares Verzeichnis.',
-	'resource_not_found'    => 'Aufgerufenes %s, <tt>%s</tt>, konnte nicht gefunden werden.',
+	'resource_not_found'    => '%s <tt>%s</tt> konnte nicht gefunden werden.',
 	'invalid_filetype'      => 'Die Dateiendung <tt>.%s</tt> ist in Ihrer View-Konfiguration nicht vorhanden.',
 	'no_default_route'      => 'Erstellen Sie bitte eine Standardroute <tt>config/routes.php</tt>.',
 	'no_controller'         => 'Kohana gelang es nicht einen Controller zu finden, um diesen Aufruf zu verarbeiten: %s',
 	'page_not_found'        => 'Die aufgerufene Seite, <tt>%s</tt>, konnte nicht gefunden werden.',
 	'stats_footer'          => 'Seite geladen in {execution_time} Sekunden bei {memory_usage} Speichernutzung. Generiert von Kohana v{kohana_version}.',
 	'error_message'         => 'Fehler aufgetreten in <strong>Zeile %s</strong> in <strong>%s</strong>.',
-	'stack_trace'           => 'Stack Trace'
+	'stack_trace'           => 'Stack Trace',
+	
+	// Resource names
+	'controller'            => 'Der Controller',
+	'helper'                => 'Der Helfer',
+	'library'               => 'Die Bibliothek',
+	'driver'                => 'Der Treiber',
+	'model'                 => 'Das Modell',
+	'view'                  => 'Die Ansicht',
 );
