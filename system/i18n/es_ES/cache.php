602da8d57ba59694da26a27cbd10fbeeb61ee046
alexsancho <alexsancho@6c8b873b-1030-0410-a378-995d7067e127>
2008-01-20 19:48:29 +0000

    core language file update in es_ES according to r1748

--- system/i18n/es_ES/cache.php
+++ system/i18n/es_ES/cache.php
@@ -3,6 +3,7 @@
 $lang = array
 (
 	'driver_not_supported' => 'El driver %s, no existe.',
+	'extension_not_loaded' => 'La extensión PHP %s tiene que estar cargada para poder utilizar este driver.',
 	'unwritable'           => 'El directorio seleccionado, <tt>%s</tt>, no tiene permisos de escritura.',
 	'resources'            => 'No es posible guardar el contenido en la cache, el contenido no es serializable.',
 	'driver_error'         => '%s',
