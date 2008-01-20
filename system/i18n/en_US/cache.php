366c8ba317c33a51b2d6efaf1220134f1df95f9e
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-01-20 19:09:32 +0000

    Added extension_not_loaded to i18n/en_US/cache.php.

--- system/i18n/en_US/cache.php
+++ system/i18n/en_US/cache.php
@@ -3,6 +3,7 @@
 $lang = array
 (
 	'driver_not_supported' => 'The %s cache driver does not exist.',
+	'extension_not_loaded' => 'The %s PHP extension must be loaded to use this driver.',
 	'unwritable'           => 'The configured storage location, <tt>%s</tt>, is not writable.',
 	'resources'            => 'Caching of resources is impossible, because resources cannot be serialized.',
 	'driver_error'         => '%s',
