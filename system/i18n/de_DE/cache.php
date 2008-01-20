62305dfdd8425e12019f82912af2078bca562b0c
Edy <Edy@6c8b873b-1030-0410-a378-995d7067e127>
2008-01-20 19:49:31 +0000

    cache language file update in de_DE according to r1748

--- system/i18n/de_DE/cache.php
+++ system/i18n/de_DE/cache.php
@@ -3,6 +3,7 @@
 $lang = array
 (
 	'driver_not_supported' => 'Der %s-Cachetreiber existiert nicht.',
+	'extension_not_loaded' => 'Die PHP-Erweiterung %s muss geladen sein, um diesen Treiber benutzen zu können.',
 	'unwritable'           => 'Der eingestellte Speicherort, <tt>%s</tt>, ist nicht beschreibbar.',
 	'resources'            => 'Das Cachen von Ressourcen ist nicht möglich, da diese nicht serialisiert werden können.',
 	'driver_error'         => '%s'
