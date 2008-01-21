72a950ae88caca847e7f757101d931ddba57c108
Edy <Edy@6c8b873b-1030-0410-a378-995d7067e127>
2008-01-21 20:53:31 +0000

    we love i18n updates :-)

--- system/i18n/de_DE/session.php
+++ system/i18n/de_DE/session.php
@@ -4,5 +4,6 @@ $lang = array
 (
 	'no_table'                        => 'Die eingestellte Sessiontabelle, %s, wurde nicht gefunden.',
 	'driver_not_supported'            => 'Der geforderte Sessiontreiber, %s, wurde nicht gefunden.',
-	'driver_must_implement_interface' => 'Sessiontreiber müssen das Session_Driver-Interface implementieren.'
+	'driver_must_implement_interface' => 'Sessiontreiber müssen das Session_Driver-Interface implementieren.',
+	'invalid_session_name'            => 'Der Sessionname %s ist ungültig. Dieser sollte nur aus alphanumerischen Zeichen und mindestens einem Buchstaben bestehen.',
 );
