c58498e73c9ab269d564b697d4dd645a70c9eac9
alexsancho <alexsancho@6c8b873b-1030-0410-a378-995d7067e127>
2008-01-21 21:04:33 +0000

    lang update

--- system/i18n/es_ES/session.php
+++ system/i18n/es_ES/session.php
@@ -4,5 +4,6 @@ $lang = array
 (
     'no_table'                        => 'No se encuentra la tabla, %s, configurada para guardar las sesiones.',
     'driver_not_supported'            => 'No se encuentra el driver, %s, requerido por la librería Session.',
-    'driver_must_implement_interface' => 'Los driver de sesiones deben implementar la interface Session_Driver.'
+    'driver_must_implement_interface' => 'Los driver de sesiones deben implementar la interface Session_Driver.',
+    'invalid_session_name'            => 'El parametro session_name, %s, no es valido. Solo debe contener caracteres alfanumericos y al menos uno debe de ser una letra.'
 );
