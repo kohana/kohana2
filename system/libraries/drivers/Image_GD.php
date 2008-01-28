a43e03bb090a198f0c690d18476982d84018f867
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-01-28 00:42:13 +0000

    Updated the Image_GD driver:
     * Crop fully functional
     * Resize fully functional
     * Rotate started, but not functional
     * Processing on transparent PNGs has been implemented, part of #156

--- system/libraries/drivers/Image_GD.php
+++ system/libraries/drivers/Image_GD.php
@@ -2,17 +2,32 @@
 
 class Image_GD_Driver extends Image_Driver {
 
+
 	// GD image create function name
 	protected $imagecreate;
 
 	// GD image save function name
 	protected $imagesave;
 
+	// A transparent PNG as a string
+	protected static $blank_png;
+
 	public function __construct()
 	{
 		// Make sure that GD2 is available
 		if ( ! function_exists('imageconvolution'))
 			throw new Kohana_Exception('image.gd.requires_v2');
+
+		// Decode the blank PNG if it has not been done already
+		(self::$blank_png === NULL) and self::$blank_png = base64_decode
+		(
+			'iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29'.
+			'mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAADqSURBVHjaYvz//z/DYAYAAcTEMMgBQAANegcCBN'.
+			'CgdyBAAA16BwIE0KB3IEAADXoHAgTQoHcgQAANegcCBNCgdyBAAA16BwIE0KB3IEAADXoHAgTQoHcgQ'.
+			'AANegcCBNCgdyBAAA16BwIE0KB3IEAADXoHAgTQoHcgQAANegcCBNCgdyBAAA16BwIE0KB3IEAADXoH'.
+			'AgTQoHcgQAANegcCBNCgdyBAAA16BwIE0KB3IEAADXoHAgTQoHcgQAANegcCBNCgdyBAAA16BwIE0KB'.
+			'3IEAADXoHAgTQoHcgQAANegcCBNCgdyBAgAEAMpcDTTQWJVEAAAAASUVORK5CYII='
+		);
 	}
 
 	protected function set_functions($type)
@@ -50,7 +65,7 @@ class Image_GD_Driver extends Image_Driver {
 		return ! (empty($this->imagecreate) OR empty($this->imagesave));
 	}
 
-	public public function process($image, $actions, $dir, $file)
+	public function process($image, $actions, $dir, $file)
 	{
 		// Make sure the image type is supported
 		if ( ! $this->set_functions($image['type']))
@@ -94,10 +109,10 @@ class Image_GD_Driver extends Image_Driver {
 		list($width, $height) = $this->properties();
 
 		// Create the temporary image to copy to
-		$tmp = imagecreatetruecolor($properties['width'], $properties['height']);
+		$tmp = $this->imagecreatetransparent($properties['width'], $properties['height']);
 
 		// Execute the crop
-		imagecopyresampled($tmp, $this->tmp_image, 0, 0, $properties['left'], $properties['top'], $properties['width'], $properties['height'], $width, $height);
+		imagecopyresampled($tmp, $this->tmp_image, 0, 0, $properties['left'], $properties['top'], $width, $height, $width, $height);
 
 		// Destroy the temporary image
 		imagedestroy($this->tmp_image);
@@ -108,17 +123,48 @@ class Image_GD_Driver extends Image_Driver {
 		return TRUE;
 	}
 
-	public public function resize($properties)
+	public function resize($properties)
 	{
-		echo Kohana::debug($properties);
+		// Sanitize the resize settings
+		$this->sanitize_geometry($properties);
+
+		// Get the current width and height
+		list($width, $height) = $this->properties();
+
+		// Create the temporary image to copy to
+		$img = $this->imagecreatetransparent($properties['width'], $properties['height']);
+
+		// Execute the resize
+		imagecopyresampled($img, $this->tmp_image, 0, 0, 0, 0, $properties['width'], $properties['height'], $width, $height);
+
+		// Destroy the temporary image
+		imagedestroy($this->tmp_image);
+
+		// Set the temporary image to this image
+		$this->tmp_image = $img;
+
+		return TRUE;
 	}
 
-	public public function rotate($amount)
+	public function rotate($amount)
 	{
-		echo Kohana::debug($amount);
+		// Prevent the alpha from being lost
+		imagealphablending($this->tmp_image, TRUE);
+		imagesavealpha($this->tmp_image, TRUE);
+
+		// Rotate, using -1 as the color to preserve transparency
+		$img = imagerotate($this->tmp_image, $amount, -1);
+
+		// Destory the current image
+		imagedestroy($this->tmp_image);
+
+		// Swap the new image for the old one
+		$this->tmp_image = $img;
+
+		return TRUE;
 	}
 
-	public public function sharpen($amount)
+	public function sharpen($amount)
 	{
 		throw new Kohana_Exception('image.unsupported_driver_method', 'sharpen');
 	}
@@ -126,6 +172,29 @@ class Image_GD_Driver extends Image_Driver {
 	protected function properties()
 	{
 		return array(imagesx($this->tmp_image), imagesy($this->tmp_image));
+	}
+
+	/**
+	 * Returns an image with a transparent background. Used for rotating to
+	 * prevent unfilled backgrounds.
+	 *
+	 * @param   integer  image width
+	 * @param   integer  image height
+	 * @return  GD resource
+	 */
+	protected function imagecreatetransparent($width, $height)
+	{
+		$img = imagecreatetruecolor($width, $height);
+		$tmp = imagecreatefromstring(self::$blank_png);
+
+		// Prevent the alpha from being lost
+		imagealphablending($img, FALSE);
+		imagesavealpha($img, TRUE);
+
+		// Resize the blank image
+		imagecopyresized($img, $tmp, 0, 0, 0, 0, $width, $height, imagesx($tmp), imagesy($tmp));
+
+		return $img;
 	}
 
 } // End Image GD Driver
