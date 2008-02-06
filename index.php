580984e0aef77cf8125cb5e84603197480db4255
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-02-06 06:53:43 +0000

    Updated index page.

--- index.php
+++ index.php
@@ -1,89 +1,84 @@
 <?php
-/*
- * File: index
- *  This file acts as the "front controller" to your application. You can
- *  configure your application and system directories here, as well as error
- *  reporting and error display.
+/**
+ * This file acts as the "front controller" to your application. You can
+ * configure your application and system directories here, as well as error
+ * reporting and error display.
  *
- * Kohana Source Code:
- *  author    - Kohana Team
- *  copyright - (c) 2007 Kohana Team
- *  license   - <http://kohanaphp.com/license.html>
+ * @package    Core
+ * @author     Kohana Team
+ * @copyright  (c) 2007 Kohana Team
+ * @license    http://kohanaphp.com/license.html
+ */
+
+error_reporting(0);
+
+/**
+ * Toggle for production status.
+ */
+define('IN_PRODUCTION', stripos($_SERVER['SERVER_NAME'], 'kohanaphp.com') !== FALSE);
+
+/**
+ * Kohana website application directory. This directory should contain your
+ * application configuration, controllers, models, views, and other resources.
  *
- * Credits:
- *  - Kohana was originally a fork of CodeIgniter <http://codeigniter.com> (c) 2006 Ellis Labs
- *  - XSS cleaning from popoon <http://www.popoon.org> (c) 2001-2006 Bitflux GmbH
- *  - HTML cleaning from <http://htmlpurifier.org> (c) 2006-2007 Edward Z. Yang
- */
-
-/*
- * About: Error Reporting
- *  Set the error reporting level. By default, Kohana will ignore E_NOTICE
- *  messages. Unless you have a special need, E_ALL is a good level for
- *  error reporting.
- */
-error_reporting(E_ALL);
-
-/*
- * About: Display Errors
- *  Enable or disable error display. During development, it is very helpful
- *  to display errors. However, errors can give away information about your
- *  application. For greater security, it is recommended that you disable
- *  the displaying of errors in production.
- */
-ini_set('display_errors', TRUE);
-
-/*
- * About: Application Directory
- *  Kohana website application directory. Most of your controllers, models, and
- *  views will by placed in this directory. This directory must contain the
- *  <config/config.php> file.
+ * This path can be absolute or relative to this file.
  */
 $kohana_application = 'application';
 
-/*
- * About: System Directory
- *  Kohana core resources, includes libraries, drivers, language files, helpers,
- *  and library-related views.
+/**
+ * Kohana package files. This directory should contain the core/ directory, and
+ * the resources you included in your download of Kohana.
+ *
+ * This path can be absolute or relative to this file.
  */
 $kohana_system = 'system';
 
-/*
- * About: Filename Extension
- *  If you have to rename all of the .php files distributed with Kohana to a
- *  different filename, you set the new extension here. Most people will never
- *  use this option.
+/**
+ * Set the error reporting level. Unless you have a special need, E_ALL is a
+ * good level for error reporting.
+ */
+error_reporting(E_ALL & ~E_STRICT);
+
+/**
+ * Turning off display_errors will effectively disable Kohana error display
+ * and logging. You can turn off Kohana errors in application/config/config.php
+ */
+ini_set('display_errors', TRUE);
+
+/**
+ * If you rename all of your .php files to a different extension, set the new
+ * extension here. This option can left to .php, even if this file is has a
+ * different extension.
  */
 define('EXT', '.php');
 
-/*
- * PLEASE DO NOT EDIT BELOW THIS LINE, unless you understand the repercussions!
- * ----------------------------------------------------------------------------
- * $Id: index.php 893 2007-10-26 01:53:51Z Shadowhand $
- */
-$docroot = pathinfo(str_replace('\\', '/', realpath(__FILE__)));
+//
+// DO NOT EDIT BELOW THIS LINE, UNLESS YOU FULLY UNDERSTAND THE IMPLICATIONS.
+// ----------------------------------------------------------------------------
+// $Id: index.php 1631 2007-12-28 00:11:38Z Shadowhand $
+//
 
-// Define the docroot
-define('KOHANA',  $docroot['basename']);
-define('DOCROOT', $docroot['dirname'].'/');
+// Define the front controller name and docroot
+define('DOCROOT', getcwd().DIRECTORY_SEPARATOR);
+define('KOHANA',  substr(__FILE__, strlen(DOCROOT)));
 
 // Define application and system paths
 define('APPPATH', str_replace('\\', '/', realpath($kohana_application)).'/');
 define('SYSPATH', str_replace('\\', '/', realpath($kohana_system)).'/');
 
 // Clean up
-unset($docroot, $kohana_application, $kohana_system);
+unset($kohana_application, $kohana_system);
 
 (is_dir(APPPATH) AND is_dir(APPPATH.'/config')) or die
 (
 	'Your <code>$kohana_application</code> directory does not exist. '.
-	'Set a valid <code>$kohana_application</code> in <tt>index.php</tt> and refresh the page.'
+	'Set a valid <code>$kohana_application</code> in <tt>'.KOHANA.'</tt> and refresh the page.'
 );
 
 (is_dir(SYSPATH) AND file_exists(SYSPATH.'/core/'.'Bootstrap'.EXT)) or die
 (
 	'Your <code>$kohana_system</code> directory does not exist. '.
-	'Set a valid <code>$kohana_system</code> in <tt>index.php</tt> and refresh the page.'
+	'Set a valid <code>$kohana_system</code> in <tt>'.KOHANA.'</tt> and refresh the page.'
 );
 
 require SYSPATH.'core/Bootstrap'.EXT;
