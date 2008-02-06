e60c6515e5bda602e8cb6c00573d66ec7b1d46aa
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-02-06 14:43:28 +0000

    Added en_GB translation, thanks edam!

---
+++ system/i18n/en_GB/ftp.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+$lang = array
+(
+	'no_connection'       => 'Unable to locate a valid connection ID.  Please make sure you are connected before peforming any file routines.',
+	'unable_to_connect'   => 'Unable to connect to your FTP server using the supplied hostname.',
+	'unable_to_login'     => 'Unable to login to your FTP server.  Please check your username and password.',
+	'unable_to_makdir'    => 'Unable to create the directory you have specified.',
+	'unable_to_changedir' => 'Unable to change directories.',
+	'unable_to_chmod'     => 'Unable to set file permissions.  Please check your path.  Note: This feature is only available in PHP 5 or higher.',
+	'unable_to_upload'    => 'Unable to upload the specified file.  Please check your path.',
+	'no_source_file'      => 'Unable to locate the source file.  Please check your path.',
+	'unable_to_remame'    => 'Unable to rename the file.',
+	'unable_to_delete'    => 'Unable to delete the file.',
+	'unable_to_move'      => 'Unable to move the file.  Please make sure the destination directory exists.'	
+);
