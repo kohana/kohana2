78df56dd84bb12274a5ac75f30d084de4c188cea
Kodeart <Kodeart@6c8b873b-1030-0410-a378-995d7067e127>
2008-02-06 18:33:36 +0000

    Macedonian translation updated. Small grammar errors fixed and 'payment_Trident' added.

--- system/i18n/mk_MK/core.php
+++ system/i18n/mk_MK/core.php
@@ -8,12 +8,14 @@ $lang = array
 	'cannot_write_log'      => 'Твојот log.directory config елемент не поинтира во директориум во кој може да се пишува.',
 	'resource_not_found'    => 'Побараниот %s, <tt>%s</tt>, не е пронајден.',
 	'invalid_filetype'      => 'Побараниот тип на датотека, <tt>.%s</tt>, не е дозволен во view конфигурационата датотека.',
-	'no_default_route'      => 'Подеси ја default рутата во <tt>config/routes.php</tt>.',
-	'no_controller'         => 'Kohana не пронајде контролер да го процесира ова барање: %s',
+	'no_default_route'      => 'Подете ја default рутата во <tt>config/routes.php</tt>.',
+	'no_controller'         => 'Kohana не пронајде контролер за да го процесира ова барање: %s',
 	'page_not_found'        => 'Страната која ја побаравте, <tt>%s</tt>, не е пронајдена.',
-	'stats_footer'          => 'Вчитано за {execution_time} секунди, употребено {memory_usage} меморија. Креирано со Kohana v{kohana_version}.',
+	'stats_footer'          => 'Вчитано за {execution_time} секунди, употребено {memory_usage} меморија. Креирано со Kohana в{kohana_version}.',
 	'error_message'         => 'Грешка во <strong>линија %s</strong> во <strong>%s</strong>.',
 	'stack_trace'           => 'Stack Trace',
+	'generic_error'         => 'Барањето Не Може Да Биде Извршено',
+	'errors_disabled'       => 'Можете да отидете на <a href="%s">home page</a> или да <a href="%s">пробате повторно</a>.',
 
 	// Resource names
 	'controller'            => 'controller',
--- system/i18n/mk_MK/session.php
+++ system/i18n/mk_MK/session.php
@@ -2,6 +2,7 @@
 
 $lang = array
 (
-	'driver_not_supported' => 'Потребниот session драјвер, %s, не е пронајден.',
-	'driver_implements'    => 'Session драјверите мора да го имплементират интерфејсот Session_Driver.'
+	'driver_not_supported' => 'Задолжителниот Session драјвер, %s, не е пронајден.',
+	'driver_implements'    => 'Задолжителниот Session драјвер не го имплементира Session_Driver.',
+	'invalid_session_name' => 'session_name, %s, е грешка. Треба да содржи само алфанумерички карактери и мора да е напишана најмалку една буква.',
 );
