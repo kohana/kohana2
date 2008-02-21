575ef9eed4236be82137a45ef4d96518400a963b
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-02-21 19:52:57 +0000

    Well, nevermind about WP. It's quite literally impossible to embed using Kohana, due to WP's totally fucked up global usage.

--- modules/wordpress/config/wordpress.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-// Relative or absolute path to your Wordpress install directory
-$config['directory'] = '/Volumes/Media/Sites/Checkout/FetchDelivers/trunk/wordpress';
--- modules/wordpress/i18n/en_US/wordpress.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-$lang = array
-(
-	'invalid_directory' => 'You Wordpress directory does not exist. Please set a valid directory in application/config/wordpress.php.';
-);
