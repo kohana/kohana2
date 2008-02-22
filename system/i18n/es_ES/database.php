57fd27627c53aa20c84216bf8cd6cb071316b1d2
alexsancho <alexsancho@6c8b873b-1030-0410-a378-995d7067e127>
2008-02-22 22:13:43 +0000

    es_ES update

--- system/i18n/es_ES/database.php
+++ system/i18n/es_ES/database.php
@@ -10,5 +10,6 @@ $lang = array
 	'must_use_set'          => 'Necesitas una clausula SET para tu consulta.',
 	'must_use_where'        => 'Necesitas una clausula WHERE para tu consulta.',
 	'must_use_table'        => 'Necesitas especificar la tabla para tu consulta.',
+	'table_not_found'       => 'La tabla %s no existe en tu base de datos.',
 	'not_implemented'       => 'El método requerido, %s, no esta soportado por este driver.',
 );
