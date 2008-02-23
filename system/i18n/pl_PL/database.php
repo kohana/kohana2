afb0fd53f9a6607aa5c1943dcbe10ffc0e66d155
kamilko <kamilko@6c8b873b-1030-0410-a378-995d7067e127>
2008-02-23 16:35:49 +0000

    Update for 'table_not_found' message

--- system/i18n/pl_PL/database.php
+++ system/i18n/pl_PL/database.php
@@ -10,5 +10,6 @@ $lang = array
 	'must_use_set'          => 'Proszę ustawić sekcję SET dla zapytania.',
 	'must_use_where'        => 'Proszę ustawić sekcję WHERE dla zapytania.',
 	'must_use_table'        => 'Proszę wybrać tabelę dla zapytania.',
+	'table_not_found'       => 'Tabeli %s nie ma w bazie danych.',
 	'not_implemented'       => 'Wywołana metoda, %s, nie jest obsługiwana przez sterownik.'
 );
