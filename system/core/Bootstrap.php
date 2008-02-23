ec7b6ed5d94f682f2c04d61c1b0d3756833dbe96
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-02-23 01:45:01 +0000

    Merging r2146 and r2148 into releases/2.1.2

--- system/core/Bootstrap.php
+++ system/core/Bootstrap.php
@@ -13,6 +13,9 @@
 define('KOHANA_VERSION',  '2.1.1');
 define('KOHANA_CODENAME', 'Schneefeier');
 
+// Test of Kohana is running in Windows
+define('KOHANA_IS_WIN', PHP_SHLIB_SUFFIX === 'dll');
+
 // Kohana benchmarks are prefixed by a random string to prevent collisions
 define('SYSTEM_BENCHMARK', uniqid());
 
