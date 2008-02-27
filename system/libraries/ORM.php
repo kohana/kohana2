e9aee49c31843e2876a9122bc52e881e53b6bb8e
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-02-27 21:00:32 +0000

    Working around a PHP bug when setting a returned key.

--- system/libraries/ORM.php
+++ system/libraries/ORM.php
@@ -137,16 +137,14 @@ class ORM_Core {
 			// Set the child id name
 			$child_id = $key.'_id';
 
-			if (isset($this->object->$child_id))
-			{
+			$this->object->$key = new $model(isset($this->object->$child_id)
 				// Get the foreign object using the key defined in this object
-				return $this->object->$key = new $model($this->object->$child_id);
-			}
-			else
-			{
+				? $this->object->$child_id
 				// Get the foreign object using the primary key of this object
-				return $this->object->$key = new $model(array($this->class.'_id', $this->object->id));
-			}
+				: array($this->class.'_id', $this->object->id));
+
+			// Return the model
+			return $this->object->$key;
 		}
 		else
 		{
