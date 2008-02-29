383838a96abba05dfa02e2b44399378a2e293428
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2008-02-29 22:41:00 +0000

    Replaced PHP's email filter with a faster and more accurate regex. For more information about my regex, check out: http://fightingforalostcause.net/misc/2006/compare-email-regex.php

--- system/helpers/valid.php
+++ system/helpers/valid.php
@@ -19,7 +19,7 @@ class valid_Core {
 	 */
 	public static function email($email)
 	{
-		return (bool) filter_var($email, FILTER_VALIDATE_EMAIL);
+		return (bool) preg_match('/^[-_a-z0-9\'+*$^&%=~!?{}]++(?:\.[-_a-z0-9\'+*$^&%=~!?{}]+)*+@(?:(?![-.])[-a-z0-9.]+(?<![-.])\.[a-z]{2,6}|\d{1,3}(?:\.\d{1,3}){3})(?::\d++)?$/iD', $email);
 	}
 
 	/**
