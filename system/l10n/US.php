69d7a5365ead220298e351ac678fa807a9d8f12d
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-03-03 18:10:00 +0000

    Updated US locale file

--- system/l10n/US.php
+++ system/l10n/US.php
@@ -1,10 +1,14 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+// Phone prefix and format
+$locale['phone_prefix'] = '1';
+$locale['phone_format'] = '3-3-4';
+
+// State names
 $locale['states'] = array
 (
 	'AL' => 'Alabama',
 	'AK' => 'Alaska',
-	// 'AS' => 'American Samoa',
 	'AZ' => 'Arizona',
 	'AR' => 'Arkansas',
 	'CA' => 'California',
@@ -12,7 +16,6 @@ $locale['states'] = array
 	'CT' => 'Connecticut',
 	'DE' => 'Delaware',
 	'DC' => 'District of Columbia',
-	// 'FM' => 'Federated States of Micronesia',
 	'FL' => 'Florida',
 	'GA' => 'Georgia',
 	'GU' => 'Guam',
@@ -25,7 +28,6 @@ $locale['states'] = array
 	'KY' => 'Kentucky',
 	'LA' => 'Louisiana',
 	'ME' => 'Maine',
-	// 'MH' => 'Marshall Islands',
 	'MD' => 'Maryland',
 	'MA' => 'Massachusetts',
 	'MI' => 'Michigan',
@@ -41,13 +43,10 @@ $locale['states'] = array
 	'NY' => 'New York',
 	'NC' => 'North Carolina',
 	'ND' => 'North Dakota',
-	// 'MP' => 'Northern Mariana Islands',
 	'OH' => 'Ohio',
 	'OK' => 'Oklahoma',
 	'OR' => 'Oregon',
-	// 'PW' => 'Palau',
 	'PA' => 'Pennsylvania',
-	// 'PR' => 'Puerto Rico',
 	'RI' => 'Rhode Island',
 	'SC' => 'South Carolina',
 	'SD' => 'South Dakota',
@@ -55,10 +54,52 @@ $locale['states'] = array
 	'TX' => 'Texas',
 	'UT' => 'Utah',
 	'VT' => 'Vermont',
-	// 'VI' => 'Virgin Islands',
 	'VA' => 'Virginia',
 	'WA' => 'Washington',
 	'WV' => 'West Virginia',
 	'WI' => 'Wisconsin',
 	'WY' => 'Wyoming',
+);
+
+// States with territories
+$locale['all_states'] = array_merge($locale['states'], array
+(
+	'AS' => 'American Samoa',
+	'FM' => 'Federated States of Micronesia',
+	'MH' => 'Marshall Islands',
+	'MP' => 'Northern Mariana Islands',
+	'PW' => 'Palau',
+	'PR' => 'Puerto Rico',
+	'VI' => 'Virgin Islands',
+));
+// Re-sort the list
+ksort($locale['all_states']);
+
+// Month names
+$locale['months'] = array
+(
+	1  => array('short' => 'Jan', 'long' => 'January'),
+	2  => array('short' => 'Feb', 'long' => 'February'),
+	3  => array('short' => 'Mar', 'long' => 'March'),
+	4  => array('short' => 'Apr', 'long' => 'April'),
+	5  => array('short' => 'May', 'long' => 'May'),
+	6  => array('short' => 'Jun', 'long' => 'June'),
+	7  => array('short' => 'Jul', 'long' => 'July'),
+	8  => array('short' => 'Aug', 'long' => 'August'),
+	9  => array('short' => 'Sep', 'long' => 'September'),
+	10 => array('short' => 'Oct', 'long' => 'October'),
+	11 => array('short' => 'Nov', 'long' => 'November'),
+	12 => array('short' => 'Dec', 'long' => 'December'),
+);
+
+// Day names
+$locale['days'] = array
+(
+	0 => array('short' => 'Sun', 'long' => 'Sunday'),
+	1 => array('short' => 'Mon', 'long' => 'Monday'),
+	2 => array('short' => 'Tue', 'long' => 'Tuesday'),
+	3 => array('short' => 'Wed', 'long' => 'Wednesday'),
+	4 => array('short' => 'Thu', 'long' => 'Thursday'),
+	5 => array('short' => 'Fri', 'long' => 'Friday'),
+	6 => array('short' => 'Sat', 'long' => 'Saturday'),
 );
