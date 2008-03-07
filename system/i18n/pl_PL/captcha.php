2bf1052ba75cca6e840d234e91be9995e25c2e3e
kamilko <kamilko@6c8b873b-1030-0410-a378-995d7067e127>
2008-03-07 13:11:14 +0000

    Added captcha.php

---
+++ system/i18n/pl_PL/captcha.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+$lang = array
+(
+	'file_not_found' => 'Podany plik, %s, nie został znaleziony. Sprawdź plik czy istnieje używając <tt>file_exists</tt> przed wywołaniem.',
+	'requires_GD2'   => 'Biblioteka Captcha wymaga biblioteki graficznej GD2 z obsługa FreeType. Więcej informacji pod: http://php.net/gd_info.',
+);
