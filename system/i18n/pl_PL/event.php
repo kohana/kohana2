b0d6d21bdd310e19a370ff15bee1eb0d27546abb
kamilko <kamilko@6c8b873b-1030-0410-a378-995d7067e127>
2008-03-27 23:14:07 +0000

    added event.php

---
+++ system/i18n/pl_PL/event.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$lang = array
+(
+	'invalid_subject' => 'Attempt to attach invalid subject <tt>%s</tt> to <tt>%s</tt> failed. Subjects must extend the <tt>Event_Subject</tt> class.',
+	'invalid_observer' => 'Attempt to attach invalid observer <tt>%s</tt> to <tt>%s</tt> failed. Subjects must extend the <tt>Event_Observer</tt> class.',
+);
