be4cb617b1a74484af6ddf8ac31865ae33bd26cc
Edy <Edy@6c8b873b-1030-0410-a378-995d7067e127>
2008-03-29 21:16:56 +0000

    de_DE update

---
+++ system/i18n/de_DE/event.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$lang = array
+(
+	'invalid_subject' => 'Der Versuch das ungültige Subjekt <tt>%s</tt> an <tt>%s</tt> anzuhängen ist fehlgeschlagen. Subjekte müssen die Klasse <tt>Event_Subject</tt> erweitern.',
+	'invalid_observer' => 'Der Versuch den ungültigen Beobachter <tt>%s</tt> an <tt>%s</tt> anzuhängen ist fehlgeschlagen. Beobachter müssen die Klasse <tt>Event_Observer</tt> erweitern.',
+);
