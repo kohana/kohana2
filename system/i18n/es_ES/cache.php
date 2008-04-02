bee6834637a7db6eb83e19fad482bc980f3884a1
alexsancho <alexsancho@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-02 09:02:44 +0000

    es_ES update

--- system/i18n/es_ES/cache.php
+++ system/i18n/es_ES/cache.php
@@ -4,6 +4,7 @@ $lang = array
 (
 	'undefined_group'      => 'El grupo %s no esta definido en la configuracion.',
 	'driver_not_supported' => 'No se encuentra el driver, %s, requerido por la librería Cache.',
+	'driver_implements'    => 'El driver, %s, requerido por la libreria Cache no implementa la interface Cache_Driver.',
 	'extension_not_loaded' => 'La extensión PHP %s tiene que estar cargada para poder utilizar este driver.',
 	'unwritable'           => 'El directorio seleccionado, <tt>%s</tt>, no tiene permisos de escritura.',
 	'resources'            => 'No es posible guardar el contenido en la cache, el contenido no es serializable.',
