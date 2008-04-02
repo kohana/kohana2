b5e9c7a21b856b5b546a09c432910579f8d09946
alexsancho <alexsancho@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-02 09:03:24 +0000

    adding event lang file to es_ES

---
+++ system/i18n/es_ES/event.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$lang = array
+(
+	'invalid_subject' => 'Fallo el intento de añadir el sujeto <tt>%s</tt> a <tt>%s</tt>. Los sujetos deben extender la clase <tt>Event_Subject</tt>.',
+	'invalid_observer' => 'Fallo el intento de añadir el observador <tt>%s</tt> a <tt>%s</tt>. Los observadores deben extender la clase <tt>Event_Observer</tt>.',
+);
