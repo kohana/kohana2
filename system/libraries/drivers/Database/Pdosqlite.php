30575c381b4121d1dd0545315d98ccac61086ca9
PugFish <PugFish@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-03 21:54:10 +0000

    Merging r2236 into releases/2.1.2

--- system/libraries/drivers/Database/Pdosqlite.php
+++ system/libraries/drivers/Database/Pdosqlite.php
@@ -56,7 +56,7 @@ class Database_Pdosqlite_Driver extends Database_Driver {
 		}
 
 		// Clear password after successful connect
-		$this->config['connection']['pass'] = NULL;
+		$this->db_config['connection']['pass'] = NULL;
 
 		return $this->link;
 	}
@@ -81,11 +81,17 @@ class Database_Pdosqlite_Driver extends Database_Driver {
 
 	public function escape_table($table)
 	{
+		if ( ! $this->db_config['escape'])
+			return $table;
+
 		return '`'.str_replace('.', '`.`', $table).'`';
 	}
 
 	public function escape_column($column)
 	{
+		if ( ! $this->db_config['escape'])
+			return $column;
+
 		if (strtolower($column) == 'count(*)' OR $column == '*')
 			return $column;
 
@@ -202,6 +208,9 @@ class Database_Pdosqlite_Driver extends Database_Driver {
 
 	public function escape_str($str)
 	{
+		if ( ! $this->db_config['escape'])
+			return $str;
+
 		if(function_exists('sqlite_escape_string'))
 		{
 			$res = sqlite_escape_string($str);
