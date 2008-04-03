2cb8d364f5b17e6fc709e80bf48ea3a3ecc62636
PugFish <PugFish@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-03 22:21:15 +0000

    Merging r2413 into releases/2.1.2

--- system/i18n/en_US/database.php
+++ system/i18n/en_US/database.php
@@ -10,5 +10,6 @@ $lang = array
 	'must_use_set'          => 'You must set a SET clause for your query.',
 	'must_use_where'        => 'You must set a WHERE clause for your query.',
 	'must_use_table'        => 'You must set a database table for your query.',
-	'not_implemented'       => 'The method you called, %s, is not supported by this driver.'
+	'not_implemented'       => 'The method you called, %s, is not supported by this driver.',
+	'result_read_only'      => 'Query results are read only.'
 );
