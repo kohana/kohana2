168382719cb994d3835a2352c128f697dd967295
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-05 16:11:47 +0000

    Fixed a typo in i18n/event. Nice catch Edy!

--- system/i18n/en_US/event.php
+++ system/i18n/en_US/event.php
@@ -3,5 +3,5 @@
 $lang = array
 (
 	'invalid_subject'  => 'Attempt to attach invalid subject <tt>%s</tt> to <tt>%s</tt> failed. Subjects must extend the <tt>Event_Subject</tt> class.',
-	'invalid_observer' => 'Attempt to attach invalid observer <tt>%s</tt> to <tt>%s</tt> failed. Subjects must extend the <tt>Event_Observer</tt> class.',
+	'invalid_observer' => 'Attempt to attach invalid observer <tt>%s</tt> to <tt>%s</tt> failed. Observers must extend the <tt>Event_Observer</tt> class.',
 );
