49342229efbda24897a1211f6bb1c9e974cdf2d4
alf <alf@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-05 18:47:21 +0000

    i18n it_IT update 2

---
+++ system/i18n/it_IT/captcha.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.'); 
+
+$lang = array
+(
+	'file_not_found' => 'Il file specificato, %s, non è stato trovato. Verificarne l\'esistenza con <tt>file_exists</tt> prima di usarlo.',
+	'requires_GD2'	 => 'La libreria Captcha richiede GD2 con supporto FreeType. Leggere http://php.net/gd_info per maggiori informazioni.',
+);
