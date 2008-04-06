cfe1349289785c73d1f777376cf839c168d7eb18
spirit <spirit@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-06 13:22:05 +0000

    i18n fr_FR update 2

---
+++ system/i18n/fr_FR/event.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$lang = array
+(
+	'invalid_subject'  => 'Le sujet <tt>%s</tt> n\'a pas pu peut être attaché à <tt>%s</tt>. Les sujets doivent étendre la classe <tt>Event_Subject</tt>.',
+	'invalid_observer' => 'L\'observeur <tt>%s</tt> n\'a pas pu peut être attaché à <tt>%s</tt>. Les observeurs doivent étendre la classe <tt>Event_Observer</tt>.',
+);
