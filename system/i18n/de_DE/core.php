b48d4c5a62dd0b933e581132e00c64250a0e088f
Edy <Edy@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-15 10:25:09 +0000

    de_DE core update (see r2492)

--- system/i18n/de_DE/core.php
+++ system/i18n/de_DE/core.php
@@ -2,19 +2,20 @@
 
 $lang = array
 (
-	'there_can_be_only_one' => 'Pro Seitenaufruf kann es nur eine Instanz von Kohana geben.',
+	'there_can_be_only_one' => 'Pro Seitenaufruf kann es nur eine Instanz von Kohana geben',
 	'uncaught_exception'    => 'Unerwarteter Fehler vom Typ %s: %s in %s in Zeile %s',
-	'invalid_method'        => 'Ungültige Methode <tt>%s</tt> aufgerufen in <tt>%s</tt>.',
-	'cannot_write_log'      => 'Ihre Einstellung log.directory in der Konfiguration verweist nicht auf ein beschreibbares Verzeichnis.',
-	'resource_not_found'    => '%s <tt>%s</tt> konnte nicht gefunden werden.',
-	'invalid_filetype'      => 'Die Dateiendung <tt>.%s</tt> ist in Ihrer View-Konfiguration nicht vorhanden.',
-	'no_default_route'      => 'Erstellen Sie bitte eine Standardroute <tt>config/routes.php</tt>.',
+	'invalid_method'        => 'Ungültige Methode <tt>%s</tt> aufgerufen in <tt>%s</tt>',
+	'cannot_write_log'      => 'Ihre Einstellung log.directory in der Konfiguration verweist nicht auf ein beschreibbares Verzeichnis',
+	'resource_not_found'    => '%s <tt>%s</tt> konnte nicht gefunden werden',
+	'invalid_filetype'      => 'Die Dateiendung <tt>.%s</tt> ist in Ihrer View-Konfiguration nicht vorhanden',
+	'view_set_filename'     => 'Sie müssen den Dateinamen der Ansicht festlegen, bevor render aufgerufen wird',
+	'no_default_route'      => 'Erstellen Sie bitte eine Standardroute <tt>config/routes.php</tt>',
 	'no_controller'         => 'Kohana gelang es nicht einen Controller zu finden, um diesen Aufruf zu verarbeiten: %s',
 	'page_not_found'        => 'Die Seite <tt>%s</tt> konnte nicht gefunden werden.',
 	'stats_footer'          => 'Seite geladen in {execution_time} Sekunden bei {memory_usage} Speichernutzung. Generiert von Kohana v{kohana_version}.',
 	'error_file_line'       => '<tt>%s <strong>[%s]:</strong></tt>',
 	'stack_trace'           => 'Stack Trace',
-	'generic_error'         => 'Die Abfrage konnte nicht abgeschlossen werden.',
+	'generic_error'         => 'Die Abfrage konnte nicht abgeschlossen werden',
 	'errors_disabled'       => 'Sie können zur <a href="%s">Startseite</a> zurück kehren oder es <a href="%s">erneut versuchen</a>.',
 
 	// Resource names
