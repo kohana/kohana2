8c12563216b71c1502c7cdc386ff3df27d034f61
armen <armen@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-18 07:39:41 +0000

    Fixed a bug to make smarty.integration work.

--- modules/smarty/libraries/MY_View.php
+++ modules/smarty/libraries/MY_View.php
@@ -4,11 +4,11 @@ class View extends View_Core {
 
 	public function __construct($name, $data = NULL, $type = NULL)
 	{
-		$type = empty($type) ? Config::item('smarty.templates_ext') : $type;
+		$type = NULL;
 
-		if ( ! Kohana::find_file('views', $name, FALSE, $type)) 
+		if (Config::item('smarty.integration') == TRUE AND Kohana::find_file('views', $name, FALSE, $type))
 		{
-			$type = NULL;
+			$type = empty($type) ? Config::item('smarty.templates_ext') : $type;
 		}
 	
 		parent::__construct($name, $data, $type);
