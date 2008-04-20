2779aa5e21b5c36eaa4a78474e9fa75f00ddfc10
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-20 09:04:33 +0000

    Cleaning up Captcha library

--- system/config/captcha.php
+++ system/config/captcha.php
@@ -5,40 +5,44 @@
  * Configure the Captcha
  * Custom styles can be added by extending the Captcha class
  */
+
 /**
- * Width of captcha image, ignored if using a background
+ * Width and height of the Captcha image.
+ * These settings are ignored if using a background image.
  */
-$config['width'] = 150;
-/**
- * Height of captcha image, ignored if using a background
- */
+$config['width']  = 150;
 $config['height'] = 40;
+
 /**
  * Captcha style to use. Default is 'basic' and is only for testing as it
- * does not require any TrueType fonts installed.
+ * does not require any truetype fonts installed.
  * 'standard' is the recommended style. A font must be supplied. A background
  * image is optional.
  * 'math' is a 'solve the question' style.
  * A font must be supplied. A background image is optional.
- * Custom styles can be added easily by extending the library
+ * Custom styles can be added easily by extending the library.
  */
 $config['style'] = 'basic';
+
 /**
- * Number of characters to use for the captcha, ignored if using style 'math'
- * Four or five seems optimal.
+ * Number of characters to use for the Captcha (4 or 5 recommended).
+ * This setting is ignored if using style 'math'.
  */
 $config['num_chars'] = 4;
+
 /**
- * Path to font files. Default is none, ''. Example 'application/fonts/'
+ * Path to font files. Example: 'application/fonts/'.
  * If using 'standard' style, you must supply a valid truetype font file.
  */
 $config['font_path'] = '';
+
 /**
- * Name of the font, Case sensitive, with the file extension, default is ''
+ * Name of the font, with the file extension. Case sensitive.
  */
 $config['font_name'] = '';
+
 /**
- * Background image. Default ''. Example 'application/images/pattern.jpg'
+ * Background image. Example: 'application/images/pattern.jpg'.
  * The dimensions of this image will be used.
  */
 $config['background_image'] = '';
--- system/libraries/Captcha.php
+++ system/libraries/Captcha.php
@@ -10,16 +10,17 @@
  * @license    http://kohanaphp.com/license.html
  */
 class Captcha_Core {
-	//config
-	protected $font_path = '';
-	protected $font_name = '';
-	protected $width = 150;
-	protected $height = 50;
-	protected $background_image = '';
-	protected $style = 'basic';
-	protected $num_chars = 4;
 
-	// class internal variables
+	// Config
+	protected $font_path        = '';
+	protected $font_name        = '';
+	protected $width            = 150;
+	protected $height           = 50;
+	protected $background_image = '';
+	protected $style            = 'basic';
+	protected $num_chars        = 4;
+
+	// Class internal variables
 	protected $image;
 	protected $color_black;
 	protected $color_white;
@@ -27,9 +28,9 @@ class Captcha_Core {
 	protected $captcha_code;
 	protected $numerals = array('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine');
 
-
 	/**
 	 * Creates a new Captcha instance.
+	 *
 	 * @throws  Kohana_Exception
 	 * @param   array  configuration
 	 * @return  void
@@ -49,13 +50,14 @@ class Captcha_Core {
 
 		$this->initialize($config);
 
-		// If using a background image, check it exists.
+		// If using a background image, check if it exists.
 		if ($this->background_image)
 		{
 			if ( ! file_exists($this->background_image))
 				throw new Kohana_Exception('captcha.file_not_found', $this->background_image);
 		}
-		// If using a font, check it exists.
+
+		// If using a font, check if it exists.
 		if ($this->font_name)
 		{
 			if ( ! file_exists($this->font_path.$this->font_name))
@@ -84,67 +86,67 @@ class Captcha_Core {
 	}
 
 	/**
-	 * Sets the captcha code to use
+	 * Sets the Captcha code to use.
 	 *
-	 * @param   string captcha code generated in captcha controller
+	 * @param   string  captcha code generated in captcha controller
 	 * @return  void
 	 */
 	public function set_code($str)
 	{
-		$this->captcha_code = $str;
+		$this->captcha_code = (string) $str;
 	}
 
 	/**
-	 * Generates the Captcha Image
+	 * Generates the Captcha image.
 	 *
-	 * @param   none
 	 * @return  void
 	 */
 	public function render()
 	{
-		// if extending the class with a custom captcha function, name it 'xyz_captcha'
-		// style 'xyz' must be added to config
-		// Call the method that implements the captcha
+		// If extending the class with a custom Captcha function, name it 'xyz_captcha'.
+		// Style 'xyz' must be added to config. Now call the method that implements the Captcha.
 		$this->{$this->style.'_captcha'}();
 
-		//Tell browser what to expect
-		// todo make this automatic
-		//header("Content-Type: image/jpeg");
-		header("Content-Type: image/png");
+		// Tell browser what to expect
+		// TODO: make this automatic
+		// header('Content-Type: image/jpeg');
+		header('Content-Type: image/png');
 
-		//Output the captcha image
-		//imagejpeg($this->image);
+		// Output the captcha image
+		// imagejpeg($this->image);
 		imagepng($this->image);
 
-		//Free up resources
+		// Free up resources
 		imagedestroy($this->image);
 	}
+
 	/**
-	 * Validates the captcha code against session captcha code
+	 * Validates the Captcha code against session Captcha code
 	 *
-	 * @param   string captcha code text
+	 * @param   string   captcha code text
 	 * @return  boolean
 	 */
 	public static function valid_captcha($str)
 	{
-		return ($str == $_SESSION['captcha_code']) ? TRUE : FALSE;
+		return ((string) $str === Session::instance()->get('captcha_code'));
 	}
 
 	/**
-	 * Creates image resource and allocates some basic colors
+	 * Creates image resource and allocates some basic colors.
 	 * If a background image is supplied, the image dimensions are used.
-	 * @param   none
+	 *
 	 * @return  void
 	 */
 	protected function img_create()
 	{
 		if ($this->background_image)
 		{
-			// todo create from any valid image
+			// TODO: create from any valid image
 			$this->image = imagecreatefromjpeg($this->background_image);
 			$this->color_white = imagecolorallocate($this->image, 255, 255, 255);
-			// get the background image dimensions
-			$this->width = imagesx($this->image);
+
+			// Get the background image dimensions
+			$this->width  = imagesx($this->image);
 			$this->height = imagesy($this->image);
 		}
 		else
@@ -152,18 +154,17 @@ class Captcha_Core {
 			$this->image = imagecreatetruecolor($this->width, $this->height);
 			$this->color_white = imagecolorallocate($this->image, 255, 255, 255);
 
-			// Fill the image with a colored gradient
-			// Use random colors, but try not to obscure the text
-			$left_color = array(mt_rand(100,255), 0, 255);
+			// Fill the image with a colored gradient (use random colors, but try not to obscure text)
+			$left_color  = array(mt_rand(100,255), 0, 255);
 			$right_color = array(100, 100, mt_rand(100,0));
 			$this->img_color_gradient($this->image, 0, 0, $this->height, $this->width, $left_color, $right_color);
 		}
 	}
 
 	/**
-	 * Allocates a background color to image
+	 * Allocates a background color to image.
 	 *
-	 * @param   array GD image color identifier
+	 * @param   array  GD image color identifier
 	 * @return  void
 	 */
 	protected function img_background($color)
@@ -172,15 +173,14 @@ class Captcha_Core {
 	}
 
 	/**
-	 * Draws a very basic captcha image:
+	 * Draws a very basic Captcha image.
 	 * Requires only GD. Useful for testing or if you can't use truetype fonts.
 	 *
-	 * @param   none
 	 * @return  void
 	 */
 	protected function basic_captcha()
 	{
-		$this->image = imagecreate($this->width, $this->height);
+		$this->image       = imagecreate($this->width, $this->height);
 		$this->color_white = imagecolorallocate($this->image, 255, 255, 255);
 		$this->color_black = imagecolorallocate($this->image, 0, 0, 0);
 
@@ -188,8 +188,8 @@ class Captcha_Core {
 	}
 
 	/**
-	 * Draws the standard captcha image:
-	 * Requires GD with freetype and available true type compatible font files.
+	 * Draws the standard Captcha image:
+	 * Requires GD with freetype and available truetype compatible font files.
 	 *
 	 * @param   none
 	 * @return  void
@@ -201,85 +201,88 @@ class Captcha_Core {
 		$font = $this->font_path.$this->font_name;
 		$this->calculate_spacing();
 
-		// draw each captcha character with varying attributes
-		for ($i = 0; $i < strlen($this->captcha_code); $i++)
+		// Draw each Captcha character with varying attributes
+		for ($i = 0, $strlen = strlen($this->captcha_code); $i < $strlen; $i++)
 		{
-			// allocate random color, size, rotation attributes to text.
+			// Allocate random color, size and rotation attributes to text
 			$text_color = imagecolorallocate($this->image, mt_rand(0, 100), mt_rand(0, 100), mt_rand(0, 100));
 			$angle = mt_rand(-40, 40);
-			// make first char angle inward
-			if ($i == 0)
+
+			// Make first char angle inward
+			if ($i === 0)
 			{
 				$angle = -abs($angle);
 			}
-			// make last char angle inward
-			if ($i == ($this->num_chars -1))
+			// Make last char angle inward
+			if ($i === ($this->num_chars - 1))
 			{
 				$angle = abs($angle);
 			}
-			// Scale the charcter size on image height
-			$font_size = mt_rand(($this->height -20), ($this->height -12));
 
+			// Scale the character size on image height
+			$font_size = mt_rand($this->height - 20, $this->height - 12);
 			$char_details = imageftbbox($font_size, $angle, $font, $this->captcha_code[$i], array());
 
-            // calculate character starting coordinates
-            $iX = $this->spacing / 4 + $i * $this->spacing;
-            $char_height = $char_details[2] - $char_details[5];
-            $iY = $this->height / 2 + $char_height / 4;
+			// Calculate character starting coordinates
+			$iX = $this->spacing / 4 + $i * $this->spacing;
+			$char_height = $char_details[2] - $char_details[5];
+			$iY = $this->height / 2 + $char_height / 4;
 
-            // write text character to image
-            imagefttext($this->image, $font_size, $angle, $iX, $iY, $text_color, $font, $this->captcha_code[$i], array());
+			// Write text character to image
+			imagefttext($this->image, $font_size, $angle, $iX, $iY, $text_color, $font, $this->captcha_code[$i], array());
 		}
 	}
 
 	/**
-	 * Draws the math riddle captcha image:
-	 * Requires GD with freetype and available true type compatible font files.
+	 * Draws the math riddle Captcha image.
+	 * Requires GD with freetype and available truetype compatible font files.
 	 *
-	 * @param   none
 	 * @return  void
 	 */
 	protected function math_captcha()
 	{
-		$answer = $_SESSION['captcha_code'];
-		//get the last digit
-		$last_digit = substr($answer, -1);
-		// convert to numeral
-		$numeral = $this->numerals[$last_digit];
-		// subtract last digit from answer
+		$answer = Session::instance()->get('captcha_code');
+
+		// Convert to numeral
+		$numeral = $this->numerals[substr($answer, -1)];
+
+		// Subtract last digit from answer
 		$number = substr($answer, 0, 2).'0';
+
 		// $number plus $numeral equals $answer
 		$text = $number.' + '.$numeral.' = ';
 		$this->img_create();
 		$font = $this->font_path.$this->font_name;
-		// scale the font size to image height
+
+		// Scale the font size to image height
 		$font_size = $this->height / 3;
 		$text_details = imageftbbox($font_size, 0, $font, $text, array());
 		$iX = 5;
 		$iY = ($this->height / 2) + 5;
+
 		imagefttext($this->image, $font_size, 0, $iX, $iY, $this->color_white, $font, $text, array());
 	}
+
 	/**
-	 * Calculates letter spacing for true type font characters
+	 * Calculates letter spacing for truetype font characters.
 	 *
-	 * @param   none
 	 * @return  integer
 	 */
 	protected function calculate_spacing()
 	{
-		$this->spacing = (int)($this->width / $this->num_chars);
+		return $this->spacing = (int) $this->width / $this->num_chars;
 	}
 
 	/**
 	 * Fills the image with a colored gradient.
 	 *
-	 * @param   resource gd image resource identifier
-	 * @param   integer  start X position
-	 * @param   integer  start Y position
-	 * @param   integer  height of fill in pixels
-	 * @param   integer  width of fill in pixels
-	 * @param   resource gd image color identifier for left of image
-	 * @param   resource gd image color identifier for right of image
+	 * @param   resource  gd image resource identifier
+	 * @param   integer   start X position
+	 * @param   integer   start Y position
+	 * @param   integer   height of fill in pixels
+	 * @param   integer   width of fill in pixels
+	 * @param   resource  gd image color identifier for left of image
+	 * @param   resource  gd image color identifier for right of image
 	 * @return  void
 	 */
 	protected function img_color_gradient($image, $x1, $y1, $height, $width, $left_color, $right_color)
@@ -287,12 +290,14 @@ class Captcha_Core {
 		$color0 = ($left_color[0] - $right_color[0]) / $width;
 		$color1 = ($left_color[1] - $right_color[1]) / $width;
 		$color2 = ($left_color[2] - $right_color[2]) / $width;
-		for ($i=0; $i <= $width; $i++)
+
+		for ($i = 0; $i <= $width; $i++)
 		{
-			$red = $left_color[0] -floor($i*$color0);
+			$red   = $left_color[0] -floor($i*$color0);
 			$green = $left_color[1] -floor($i*$color1);
-			$blue = $left_color[2] -floor($i*$color2);
-			$col = imagecolorallocate($this->image, $red, $green, $blue);
+			$blue  = $left_color[2] -floor($i*$color2);
+			$col   = imagecolorallocate($this->image, $red, $green, $blue);
+
 			imageline($this->image, $x1 + $i, $y1, $x1 + $i, $y1 + $height, $col);
 		}
 	}
