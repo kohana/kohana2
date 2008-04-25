620f6c0cce6daca4d1cebff64042b99bdb1f8d0b
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-25 03:35:56 +0000

    Follow up to r2567, removed routes._allowed config setting.

--- system/config/routes.php
+++ system/config/routes.php
@@ -2,12 +2,6 @@
 /**
  * @package  Core
  *
- * Permitted URI characters. Note that "?", "#", and "=" are URL characters, and
- * should not be added here.
- */
-$config['_allowed'] = '-a-z 0-9~%.,:+_/';
-
-/**
  * Default route to use when no URI segments are available.
  */
 $config['_default'] = 'welcome';
