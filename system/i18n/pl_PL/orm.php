8492ea7ffa0bc5f193fe2c6b01ba937165fb1d70
kamilko <kamilko@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-25 23:41:58 +0000

    pl_PL update

--- system/i18n/pl_PL/orm.php
+++ system/i18n/pl_PL/orm.php
@@ -1,3 +1,4 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 $lang['method_not_implemented'] = 'Metoda %s nie jest zaimplementowana w modelu ORM %s.';
+$lang['validate_post_not_object'] = '$_POST musi być instancją obiektu Validation przed wywołaniem metody %s::validate()';
