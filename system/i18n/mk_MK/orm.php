84e6228964971b13f62c855370ea623bcef95708
Kodeart <Kodeart@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-29 13:19:33 +0000

    i18n mk_MK update

--- system/i18n/mk_MK/orm.php
+++ system/i18n/mk_MK/orm.php
@@ -1,3 +1,4 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 $lang['method_not_implemented'] = 'Методата %s не е имплементирана во %s ORM моделот.';
+$lang['validate_post_not_object'] = '$_POST мора да биде инстанца на Validation пред методата %s::validate() да биде повикана';
