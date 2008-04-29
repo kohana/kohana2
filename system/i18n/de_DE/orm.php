3dc51b2ce0f6c9e9e2e00ebc698e1006347551f2
Edy <Edy@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-29 20:25:11 +0000

    de_DE update (see r2605)

--- system/i18n/de_DE/orm.php
+++ system/i18n/de_DE/orm.php
@@ -1,4 +1,4 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 $lang['method_not_implemented'] = 'Die Methode %s ist im ORM-Modell %s nicht implementiert';
-$lang['validate_post_not_object'] = '$_POST muss eine Instanz von Validate sein, bevor %::validate() aufgerufen wird';
+$lang['array_not_validation_object'] = 'Die Daten müssen eine Instanz von Validate sein, bevor %::validate() aufgerufen wird';
