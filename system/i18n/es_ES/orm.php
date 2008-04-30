401c88b53c4a59b5502821a802905170c8df0dcf
alexsancho <alexsancho@6c8b873b-1030-0410-a378-995d7067e127>
2008-04-30 11:10:32 +0000

    es_ES update (r2608 and r2609)

--- system/i18n/es_ES/orm.php
+++ system/i18n/es_ES/orm.php
@@ -1,4 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 $lang['method_not_implemented'] = 'El metodo %s no esta implementado en el modelo ORM %s.';
-$lang['validate_post_not_object'] = '$_POST debe de ser una instacia de a clase Validation antes de llamar al metodo %s::validate()';
