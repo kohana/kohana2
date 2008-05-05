228336caaf1fb1c4dcbdfc25ddde8ad5b42a6efb
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2008-05-05 17:56:02 +0000

    Optimized text::auto_p() by automatically skipping regexes that are only needed if the string contains existing html. Since you'll often pass strings that have gone through html::specialchars(), the potential of saving time in text::auto_p() is rather high.

--- system/helpers/text.php
+++ system/helpers/text.php
@@ -298,20 +298,28 @@ class text_Core {
 		$str = preg_replace('~^[ \t]+~m', '', $str);
 		$str = preg_replace('~[ \t]+$~m', '', $str);
 
-		// Elements that should not be surrounded by p tags
-		$no_p = '(?:p|div|h[1-6r]|ul|ol|li|blockquote|d[dlt]|pre|t[dhr]|t(?:able|body|foot|head)|c(?:aption|olgroup)|form|s(?:elect|tyle)|a(?:ddress|rea)|ma(?:p|th))';
+		// The following regexes only need to be executed if the string contains html
+		if (($html_found = strpos($str, '<')) !== FALSE)
+		{
+			// Elements that should not be surrounded by p tags
+			$no_p = '(?:p|div|h[1-6r]|ul|ol|li|blockquote|d[dlt]|pre|t[dhr]|t(?:able|body|foot|head)|c(?:aption|olgroup)|form|s(?:elect|tyle)|a(?:ddress|rea)|ma(?:p|th))';
 
-		// Put at least two linebreaks before and after $no_p elements
-		$str = preg_replace('~^<'.$no_p.'[^>]*+>~im', "\n$0", $str);
-		$str = preg_replace('~</'.$no_p.'\s*+>$~im', "$0\n", $str);
+			// Put at least two linebreaks before and after $no_p elements
+			$str = preg_replace('~^<'.$no_p.'[^>]*+>~im', "\n$0", $str);
+			$str = preg_replace('~</'.$no_p.'\s*+>$~im', "$0\n", $str);
+		}
 
 		// Do the <p> magic!
 		$str = '<p>'.trim($str).'</p>';
 		$str = preg_replace('~\n{2,}~', "</p>\n\n<p>", $str);
 
-		// Remove p tags around $no_p elements
-		$str = preg_replace('~<p>(?=</?'.$no_p.'[^>]*+>)~i', '', $str);
-		$str = preg_replace('~(</?'.$no_p.'[^>]*+>)</p>~i', '$1', $str);
+		// The following regexes only need to be executed if the string contains html
+		if ($html_found !== FALSE)
+		{
+			// Remove p tags around $no_p elements
+			$str = preg_replace('~<p>(?=</?'.$no_p.'[^>]*+>)~i', '', $str);
+			$str = preg_replace('~(</?'.$no_p.'[^>]*+>)</p>~i', '$1', $str);
+		}
 
 		// Convert single linebreaks to <br />
 		$str = preg_replace('~(?<!\n)\n(?!\n)~', "<br />\n", $str);
