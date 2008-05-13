dde9e189353ecab9cd3b8c8075fd66ac2b83640c
alexsancho <alexsancho@6c8b873b-1030-0410-a378-995d7067e127>
2008-05-13 07:58:30 +0000

    es_ES update (Follow-up to r2628.)

--- system/i18n/es_ES/image.php
+++ system/i18n/es_ES/image.php
@@ -3,7 +3,6 @@
 $lang = array
 (
 	'getimagesize_missing'    => 'La librería &8220;Image&8221; requiere la función PHP <tt>getimagesize</tt>, que no parece estar disponible en tu instalación.',
-	'driver_not_supported'    => 'No se encuentra el driver, %s, requerido por la librería Image.',
 	'unsupported_method'      => 'El driver que has elegido en la configuración no soporta el tipo de transformación %s.',
 	'file_not_found'          => 'La imagen especificada, %s no se ha encontrado. Por favor, verifica que existe utilizando <tt>file_exists</tt> antes de manipularla.',
 	'type_not_allowed'        => 'El tipo de imagen especificado, %s, no es un tipo de imagen permitido.', 
--- system/i18n/es_ES/session.php
+++ system/i18n/es_ES/session.php
@@ -2,7 +2,5 @@
 
 $lang = array
 (
-	'driver_not_supported' => 'No se encuentra el driver, %s, requerido por la librería Session.',
-	'driver_implements'    => 'Los driver de sesiones deben implementar la interface Session_Driver.',
 	'invalid_session_name' => 'El parametro session_name, %s, no es valido. Solo debe contener caracteres alfanumericos y al menos uno debe de ser una letra.',
 );
