a55ebc74ec32db45986a76c0cd354b9de28c3bde
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-05-28 21:45:52 +0000

    Merged r2699 into 2.1.2

--- modules/forge/i18n/en_US/forge.php
+++ modules/forge/i18n/en_US/forge.php
@@ -2,5 +2,6 @@
 
 $lang = array
 (
-	'invalid_input' => 'Invalid input type requested: %s',
+	'invalid_input' => 'Error loading %s: All inputs must be constructed with a name',
+	'unknown_input' => 'Unable to find a Forge input class for: %s',
 );
