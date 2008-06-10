f5ee2f63416ab8e4fd99798e054aa937fe50bd43
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-06-10 21:58:09 +0000

    Updated en_GB for 2.1.2

--- system/i18n/en_GB/database.php
+++ system/i18n/en_GB/database.php
@@ -5,10 +5,11 @@ $lang = array
 	'undefined_group'       => 'The %s group is not defined in your configuration.',
 	'error'                 => 'There was an SQL error: %s',
 	'connection'            => 'There was an error connecting to the database: %s',
-	'driver_not_supported'  => 'The requested Database driver, %s, was not found.',
 	'invalid_dsn'           => 'The DSN you supplied is not valid: %s',
 	'must_use_set'          => 'You must set a SET clause for your query.',
 	'must_use_where'        => 'You must set a WHERE clause for your query.',
 	'must_use_table'        => 'You must set a database table for your query.',
-	'not_implemented'       => 'The method you called, %s, is not supported by this driver.'
+	'table_not_found'       => 'Table %s does not exist in your database.',
+	'not_implemented'       => 'The method you called, %s, is not supported by this driver.',
+	'result_read_only'      => 'Query results are read only.'
 );
