3bf2bc712ef58363df4ede95c01a3ce5a0417151
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-06-11 13:31:06 +0000

    Updated paypal config for website

--- application/config/payment.php
+++ application/config/payment.php
@@ -17,19 +17,18 @@ $config['default'] = array
 
 /*
  * PayPal Options:
- *  API_UserName - the username to use
- *  API_Password - the password to use
- *  API_Signature - the api signature to use
- *  ReturnUrl - the URL to send the user to after they login with paypal
+ *  USER - the username to use
+ *  PWD - the password to use
+ *  SIGNATURE - the api signature to use
+ *  RETURNURL - the URL to send the user to after they login with paypal
  *  CANCELURL - the URL to send the user to if they cancel the paypal transaction
  *  CURRENCYCODE - the Currency Code to to the transactions in (What do you want to get paid in?)
  */
 $config['Paypal'] = array
 (
-	'API_UserName' => '',
-	'API_Password' => '',
-	'API_Signature' => '',
-	'ReturnUrl' => 'http://kohanaphp.com/donate/paypal.html',
+	'USER' => '',
+	'PWD' => '',
+	'SIGNATURE' => '',
+	'RETURNURL' => 'http://kohanaphp.com/donate/paypal.html',
 	'CANCELURL' => 'http://kohanaphp.com/donate/index.html',
-	'CURRENCYCODE' => 'USD',
 );
