a7122c4932fdb63f13d9790c160c821fbcaa941a
kamilko <kamilko@6c8b873b-1030-0410-a378-995d7067e127>
2008-06-11 17:41:44 +0000

    ticket 633

--- system/i18n/pl_PL/database.php
+++ system/i18n/pl_PL/database.php
@@ -10,6 +10,6 @@ $lang = array
 	'must_use_set'          => 'Proszę ustawić sekcję SET dla zapytania.',
 	'must_use_where'        => 'Proszę ustawić sekcję WHERE dla zapytania.',
 	'must_use_table'        => 'Proszę wybrać tabelę dla zapytania.',
-	'not_implemented'       => 'Wywołana metoda, %s, nie jest obsługiwana przez sterownik.'
+	'not_implemented'       => 'Wywołana metoda, %s, nie jest obsługiwana przez sterownik.',
 	'result_read_only'      => 'Wynik zapytania jest tylko do odczytu.'
 );
