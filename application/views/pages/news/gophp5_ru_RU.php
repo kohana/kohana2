bb43563eb16825233ecdd8806b5ed4ca697598dc
littfed <littfed@6c8b873b-1030-0410-a378-995d7067e127>
2008-06-14 15:43:05 +0000

    add ru_RU news page translation

---
+++ application/views/pages/news/gophp5_ru_RU.php
@@ -0,0 +1,5 @@
+<p class="intro">Последние <strong>новости</strong> и <strong>обявления</strong> от команды разработчиков Kohana.</p>
+<h2>Kohana присоединяется к GoPHP5!</h2>
+<p>The Kohana Team is proud to announce joining the <?php echo html::anchor('http://gophp5.org', 'GoPHP5 initiative') ?>.</p>
+<p>The transition to PHP 5 has not been a smooth ride for many people, and early versions of PHP 5 were slow, buggy, and overall not "production ready". With PHP 5.2, these issues are almost completely gone. PHP 5.2 offers high performance, a full feature set, and a stable platform on which to build web applications.</p>
+<p>Kohana's adoption of PHP 5.2 ensures a level of stability and predictability for both end-users and developers, and we expect to take full advantage of these features in the next official release of Kohana.</p>
