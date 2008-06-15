e2dc0e16fc7a04835b401749918c4708db9f40c2
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-06-15 15:59:27 +0000

    Follow up to r2844, minor optimization/fix

--- application/views/layout.php
+++ application/views/layout.php
@@ -6,6 +6,9 @@ $lang = substr(Config::item('locale.language'), 0, 2);
 // Base URL
 $base_url = url::base();
 
+// Page suffix
+$suffix = Config::item('core.url_suffix');
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//<?php echo strtoupper($lang) ?>"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -56,7 +59,8 @@ echo html::script(array
 <!-- Start Language Picker -->
 <ul id="languages">
 <?php foreach (Config::item('locale.tlds') as $tld => $i18n): ?>
-<li><a class="<?php if ($tld === TLD) echo 'active' ?>" href="<?php echo ($url = preg_replace('/(?<=kohanaphp\.)[a-z.]+/', $tld, $base_url, 1)).Router::$complete_uri ?>"><img alt="<?php echo $i18n ?>" src="<?php echo $base_url ?>media/img/flags/<?php echo $i18n ?>.png" width="16" height="11" /></a></li>
+<li><a class="<?php if ($tld === TLD) echo 'active' ?>" href="<?php echo preg_replace('/(?<=kohanaphp\.)[a-z.]+/', $tld, $base_url, 1).$suffix.Router::$complete_uri ?>"><img alt="<?php echo $i18n ?>" src="<?php echo $base_url, 
+'media/img/flags/', $i18n, '.png' ?>" width="16" height="11" /></a></li>
 <?php endforeach ?>
 </ul>
 <!-- End Language Picker -->
