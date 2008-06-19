0dbbdb07fa2957655fd62ca97945ecaf622e5e07
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-06-19 22:22:57 +0000

    Fixed a missing comma in pt_BR, thanks radekk!

--- system/i18n/pt_BR/database.php
+++ system/i18n/pt_BR/database.php
@@ -8,8 +8,8 @@ $lang = array
     'invalid_dsn'           => 'O DSN informado não é válido: %s',
 	'must_use_set'          => 'Você deve fornecer uma cláusula SET para a sua consulta.',
 	'must_use_where'        => 'Você deve fornecer uma cláusula WHERE para a sua consulta.',
-	'must_use_table'        => 'Você deve fornecer um nome de tabela para a sua consulta.'
+	'must_use_table'        => 'Você deve fornecer um nome de tabela para a sua consulta.',
     'table_not_found'       => 'A tabela %s não existe em seu banco de dados.',
     'not_implemented'       => 'O método que você chamou, %s, não é suportado pelo driver.',
-    'result_read_only'      => 'Resultados de consultas são apenas para leitura.'
+    'result_read_only'      => 'Resultados de consultas são apenas para leitura.',
 );
