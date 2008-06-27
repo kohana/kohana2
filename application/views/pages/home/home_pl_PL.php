80ea8517e13ea6d362c1002f62804420c44f6c42
cmike <cmike@6c8b873b-1030-0410-a378-995d7067e127>
2008-06-27 06:10:51 +0000

    Changes in displayed HTML source

--- application/views/pages/home/home_pl_PL.php
+++ application/views/pages/home/home_pl_PL.php
@@ -1,44 +1,37 @@
-<p class="intro">Kohana jest <strong>framework'iem napisanym w PHP 5</strong>, który używa architektury <strong>Model View Controller</strong>.	Jest tworzony, aby być <strong>bezpiecznym</strong>, <strong>lekkim</strong>, i <strong>łatwym</strong> w użyciu.</p>
+<p class="intro">Kohana jest <strong>framework'iem napisanym w PHP 5</strong>, który używa architektury <strong>Model View Controller</strong>. Jest tworzony, aby być <strong>bezpiecznym</strong>, <strong>lekkim</strong>, i <strong>łatwym</strong> w użyciu.</p>
 <div style="float:left; padding-right: 4em;">
-<h2>Cechy</h2>
-<ul>
-	<li>Wysoce bezpieczny</li>
-	<li>Ekstremalnie lekki</li>
-	<li>Łatwy w nauce</li>
-	<li>Korzysta z architektury <abbr title="Model View Controller">MVC</abbr></li>
-	<li>100% kompatybilny z UTF-8</li>
-	<li>Luźno sprzężona architektura</li>
-	<li>Ekstremalnie łatwy do rozwijania</li>
-</ul>
+	<h2>Cechy</h2>
+	<ul>
+		<li>Wysoce bezpieczny</li>
+		<li>Ekstremalnie lekki</li>
+		<li>Łatwy w nauce</li>
+		<li>Korzysta z architektury <abbr title="Model View Controller">MVC</abbr></li>
+		<li>100% kompatybilny z UTF-8</li>
+		<li>Luźno sprzężona architektura</li>
+		<li>Ekstremalnie łatwy do rozwijania</li>
+	</ul>
 </div>
-
 <div style="float:left;">
-<h2>Technologia</h2>
-<ul>
-	<li>Czyste <abbr title="Programowanie obiektowe">OOP</abbr> w PHP 5</li>
-	<li>Prosta obsługa baz SQL przy wykorzystaniu sterowników SQL</li>
-	<li>Sesje wielokrotne (natywna, bazy danych, i cookie)</li>
-	<li>Rozbudowany system obsługi zdarzeń pozwalający na modyfikacje</li>
-	<li>Oparty na <a href="http://www.codeigniter.com">CodeIgniterze</a></li>
-</ul>
+	<h2>Technologia</h2>
+	<ul>
+		<li>Czyste <abbr title="Programowanie obiektowe">OOP</abbr> w PHP 5</li>
+		<li>Prosta obsługa baz SQL przy wykorzystaniu sterowników SQL</li>
+		<li>Sesje wielokrotne (natywna, bazy danych, i cookie)</li>
+		<li>Rozbudowany system obsługi zdarzeń pozwalający na modyfikacje</li>
+		<li>Oparty na <a href="http://www.codeigniter.com">CodeIgniterze</a></li>
+	</ul>
 </div>
-
 <h3 style="clear:both;padding-top:1em;">Czym wyróżnia się Kohana?</h3>
-
 <p>Pomimo faktu, iż Kohana używa wielu znanych rozwiązań, istnieje kilka kwestii, które powodują, że warto zwrócić na nią uwagę:</p>
-
 <ol>
-<li><strong>Rozwijany przez społeczność, nie firmę.</strong> Rozwój Kohany jest kierowany przez grupę oddanych ludzi, którzy potrzebują frameworka do szybkich, dużych rozwiązań.</li>
-<li><strong>Czyste <abbr title="projektowanie obiektowe">OOP</abbr> w  PHP 5.</strong> Oferuje wiele korzyści: automatyczne ładowanie klas, interfejsów, abstraktów, itp.</li>
-
-<li><strong>Bardzo lekki.</strong> Kohana nie ma zależności wobec bibliotek typu PECL lub PEAR. Duże, monolityczne biblioteki są unikane w celu optymalizacji rozwiązań.</li>
-<li><strong>Tablice GET, POST, COOKIE, i SESSION obsługiwane standardowo.</strong> Kohana nie ogranicza dostępu do danych globalnych, ale oferuje filtrowanie i ochronę przed atakami <abbr title="Cross Site Scripting">XSS</abbr>.</li>
-<li><strong>Autoładowanie klas.</strong> Prawdziwe ładowanie klas "na życzenie", jak tylko są wywoływane w aplikacji.</li>
-
-<li><strong>Brak konfliktów w nazwach.</strong> Wszystkie klasy są przedrostkowane, aby pozwolić na podobne nazwy pomiędzy komponentami, dla bardziej logicznego API.</li>
-<li><strong>Kaskadowanie zasobów pozwala na niezrównaną rozszerzalność.</strong> Prawie każda część Kohany może być rozszerzona bez edycji źródłowych plików systemu. Moduły pozwalają na dodawanie pluginów do aplikacji.</li>
-<li><strong>Zwięzłość sterowników bibliotek i API.</strong> Biblioteki mogą używać różnych "sterowników" do obsługi wielu zewnętrznych <abbr title="Application Programming Interface">API</abbr>. Na przykład, dostępne są opcje wielokrotnych sesji (baza danych, cookie, i natywna), ale ten sam interfejs jest używany do nich wszystkich. To pozwala na rozwój nowych sterowników dla istniejących bibliotek, które utrzymują logiczność i przejrzystość API.</li>
-<li><strong>Rozbudowany system obsługi zdarzeń.</strong> Silnik obsługi zdarzeń pozwala na dowolne dostosowywanie.</li>
-
-<li><strong>Częste aktualizacje.</strong> Wynikami szybkiego rozwoju są szybkie odpowiedzi na zgłaszane błędy i sugestie.</li>
-</ol></div></div>
+	<li><strong>Rozwijany przez społeczność, nie firmę.</strong> Rozwój Kohany jest kierowany przez grupę oddanych ludzi, którzy potrzebują frameworka do szybkich, dużych rozwiązań.</li>
+	<li><strong>Czyste <abbr title="projektowanie obiektowe">OOP</abbr> w  PHP 5.</strong> Oferuje wiele korzyści: automatyczne ładowanie klas, interfejsów, abstraktów, itp.</li>
+	<li><strong>Bardzo lekki.</strong> Kohana nie ma zależności wobec bibliotek typu PECL lub PEAR. Duże, monolityczne biblioteki są unikane w celu optymalizacji rozwiązań.</li>
+	<li><strong>Tablice GET, POST, COOKIE, i SESSION obsługiwane standardowo.</strong> Kohana nie ogranicza dostępu do danych globalnych, ale oferuje filtrowanie i ochronę przed atakami <abbr title="Cross Site Scripting">XSS</abbr>.</li>
+	<li><strong>Autoładowanie klas.</strong> Prawdziwe ładowanie klas "na życzenie", jak tylko są wywoływane w aplikacji.</li>
+	<li><strong>Brak konfliktów w nazwach.</strong> Wszystkie klasy są przedrostkowane, aby pozwolić na podobne nazwy pomiędzy komponentami, dla bardziej logicznego API.</li>
+	<li><strong>Kaskadowanie zasobów pozwala na niezrównaną rozszerzalność.</strong> Prawie każda część Kohany może być rozszerzona bez edycji źródłowych plików systemu. Moduły pozwalają na dodawanie pluginów do aplikacji.</li>
+	<li><strong>Zwięzłość sterowników bibliotek i API.</strong> Biblioteki mogą używać różnych "sterowników" do obsługi wielu zewnętrznych <abbr title="Application Programming Interface">API</abbr>. Na przykład, dostępne są opcje wielokrotnych sesji (baza danych, cookie, i natywna), ale ten sam interfejs jest używany do nich wszystkich. To pozwala na rozwój nowych sterowników dla istniejących bibliotek, które utrzymują logiczność i przejrzystość API.</li>
+	<li><strong>Rozbudowany system obsługi zdarzeń.</strong> Silnik obsługi zdarzeń pozwala na dowolne dostosowywanie.</li>
+	<li><strong>Częste aktualizacje.</strong> Wynikami szybkiego rozwoju są szybkie odpowiedzi na zgłaszane błędy i sugestie.</li>
+</ol>
