e042b3424922818433f912554588c998e98bda7e
msaraujo <msaraujo@6c8b873b-1030-0410-a378-995d7067e127>
2008-07-01 12:49:14 +0000

    still translating the pt_BR home

--- application/views/pages/home/home_pt_BR.php
+++ application/views/pages/home/home_pt_BR.php
@@ -8,14 +8,14 @@
 		<li>Utiliza o modelo <abbr title="Model View Controller">MVC</abbr></li>
 		<li>100% compativel UTF-8</li>
 		<li>Arquitetura facilmente acoplável</li>
-		<li>Extremamente facil para estender</li>
+		<li>Extremamente fácil para estender</li>
 	</ul>
 </div>
 <div style="float:left;">
 	<h2>Tecnologia</h2>
 	<ul>
 		<li>Estritamente escrito em PHP 5 <abbr title="Object Oriented Programming">OOP</abbr></li>
-		<li>Abstracão de acesso ao banco de dados simples utilizando SQL helpers</li>
+		<li>Simples Abstracão de Acesso a Bancos de Dados</li>
 		<li>Múltiplos Drivers de Sessão (nativo, banco de dados e cookie)</li>
 		<!-- <li>Advanced cache system with drivers (file, database, memcache, shmop)</li> -->
 		<li>Poderoso manipulador de eventos que permite pequenas modificacões dinamicamente</li>
@@ -25,14 +25,14 @@
 <h3 style="clear:both;padding-top:1em;">Por quê o Kohana é diferente?</h3>
 <p>Apesar do Kohana reutilizar vários padrões de design bastante comuns, existem algumas coisas que faz o Kohana se destacar:</p>
 <ol>
-	<li><strong>Community, not company, driven.</strong> O desenvolvimento do Kohana é dirigido por um time de pessoas dedicadas que necessitam de um framework para solucões rápidas e poderosas.</li>
+	<li><strong>Projetado pela comunidade e não por uma empresa.</strong> O desenvolvimento do Kohana é dirigido por um time de pessoas dedicadas que necessitam de um framework para solucões rápidas e poderosas.</li>
 	<li><strong>Strict PHP 5 <abbr title="Object Oriented Programming">OOP</abbr>.</strong> Oferece muitos benefícios: visibility protection, carregamento automático de classes, sobrecarregamento, interfaces, classes abstratas e singletons.</li>
-	<li><strong>Extremamente leve.</strong> Kohana não possui dependências de extensões PECL ou bibliotecas da PEAR. Large, monolithic libraries are avoided in favor of optimized solutions.</li>
-	<li><strong>GET, POST, COOKIE, <em>and</em> SESSION arrays all work as expected.</strong> Kohana does not limit your access to global data, but offers	filtering and <abbr title="Cross Site Scripting">XSS</abbr> protection.</li>
-	<li><strong>True auto-loading of classes.</strong> True on-demand loading of classes, as they are requested in your application.</li>
-	<li><strong>No namespace conflicts.</strong> All classes are suffixed to allow similar names between components, for a more coherent API.</li>
+	<li><strong>Extremamente leve.</strong> Kohana não possui dependências de extensões PECL ou bibliotecas da PEAR. Bibliotecas gigantescas e monolíticas são evitadas a favor de otimizar as solucões desenvolvidas.</li>
+	<li><strong>GET, POST, COOKIE <em>e</em> SESSION funcionam como esperado.</strong> Kohana não limita seu acesso aos dados globais, mas oferece filtros e protecão contra <abbr title="Cross Site Scripting">XSS (Cross Site Scripting)</abbr>.</li>
+	<li><strong>Carregamento automático de classes.</strong>Estas são carregadas sob demanda, de acordo com o requerido.</li>
+	<li><strong>Não existe conflitos de namespace.</strong> Todas as classes estão adequadas para permitir componentes com nomes similares, a favor de uma API mais coerente.</li>
 	<li><strong>Cascading resources offer unparalleled extensibility.</strong> Almost every part of Kohana can be overloaded or extended without editing core system files. Modules allow multi-file plugins to be added to your application, transparently.</li>
-	<li><strong>Library drivers and API consistency.</strong> Libraries can use different "drivers" to handle different external <abbr title="Application Programming Interface">API</abbr>s transparently. For example, multiple session storage options are available (database, cookie, and native), but the same interface is used for all of them. This allows new drivers to be developed for existing libraries, which keeps the API consistent and transparent.</li>
+	<li><strong>Drivers e API consistente.</strong> Bibliotecas podem ter diferentes "drivers" para manipular  to handle different external <abbr title="Application Programming Interface">API</abbr>s transparently. For example, multiple session storage options are available (database, cookie, and native), but the same interface is used for all of them. This allows new drivers to be developed for existing libraries, which keeps the API consistent and transparent.</li>
 	<li><strong>Powerful event handler.</strong> Observer-style event handlers allow for extreme levels of customization potential.</li>
-	<li><strong>Rapid development cycle.</strong> Rapid development results in faster response to user bugs and requests.</li>
+	<li><strong>Ciclo de Desenvolvimento Ágil.</strong> Rapid development results in faster response to user bugs and requests.</li>
 </ol>
