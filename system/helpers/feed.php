c0ce54e7695f4b0ff00be27b73acb08a64a28d11
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-07-01 18:38:34 +0000

    Added initial feed::create($info, $items, $format) method. Only supports RSS 2.0, for now.

--- system/helpers/feed.php
+++ system/helpers/feed.php
@@ -53,4 +53,61 @@ class feed_Core {
 		return $items;
 	}
 
-} // End rss
+	/**
+	 * Creates a feed from the given parameters.
+	 *
+	 * @param   array   feed information
+	 * @param   array   items to add to the feed
+	 * @return  string
+	 */
+	public static function create($info, $items, $format = 'rss2')
+	{
+		$info += array('title' => 'Generated Feed', 'link' => '', 'generator' => 'KohanaPHP');
+
+		$feed = '<?xml version="1.0"?><rss version="2.0"><channel></channel></rss>';
+		$feed = simplexml_load_string($feed);
+
+		foreach ($info as $name => $value)
+		{
+			if (($name === 'pubDate' OR $name === 'lastBuildDate') AND (is_int($value) OR ctype_digit($value)))
+			{
+				// Convert timestamps to RFC 822 formatted dates
+				$value = date(DATE_RFC822, $value);
+			}
+			elseif (($name === 'link' OR $name === 'docs') AND strpos($value, '://') === FALSE)
+			{
+				// Convert URIs to URLs
+				$value = url::site($value, 'http');
+			}
+
+			// Add the info to the channel
+			$feed->channel->addChild($name, $value);
+		}
+
+		foreach ($items as $item)
+		{
+			// Add the item to the channel
+			$row = $feed->channel->addChild('item');
+
+			foreach ($item as $name => $value)
+			{
+				if ($name === 'pubDate' AND (is_int($value) OR ctype_digit($value)))
+				{
+					// Convert timestamps to RFC 822 formatted dates
+					$value = date(DATE_RFC822, $value);
+				}
+				elseif (($name === 'link' OR $name === 'guid') AND strpos($value, '://') === FALSE)
+				{
+					// Convert URIs to URLs
+					$value = url::site($value, 'http');
+				}
+
+				// Add the info to the row
+				$row->addChild($name, $value);
+			}
+		}
+
+		return $feed->asXML();
+	}
+
+} // End feed
