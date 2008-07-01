e73ce817da8d837f8ca1e6bdf41eb68f58925985
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-07-01 22:54:18 +0000

    Added database config for object_db module

---
+++ modules/object_db/config/database.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$config['default'] = array
+(
+	'driver'   => 'mysql',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'anonymous',
+);
