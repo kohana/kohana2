2240a63039ff85d677050d35f34928242b9a07db
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-07-13 23:47:12 +0000

    Removed 13_37 i18n due to lack of maintainence. Sorry JB.

--- system/i18n/13_37/cache.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-$lang = array
-(
-	'driver_not_supported' => '7h3 %s c4ch3 dr1v3r d035 n07 3x157.',
-	'unwritable'           => '7h3 c0nf16ur3d 570r463 |0c4710n, <tt>%s</tt>, 15 n07 wr174b|3.',
-	'resources'            => 'C4ch1n6 0f r350urc35 15 1mp0551b|3, b3c4u53 r350urc35 c4nn07 b3 53r14|1z3d.',
-);
