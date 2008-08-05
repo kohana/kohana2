d1c6519df8a3e3d310058d557b761a76f47861ba
alexsancho <alexsancho@6c8b873b-1030-0410-a378-995d7067e127>
2008-08-05 22:13:18 +0000

    es_ES ready for 2.2

--- system/i18n/es_ES/encrypt.php
+++ system/i18n/es_ES/encrypt.php
@@ -2,6 +2,7 @@
 
 $lang = array
 (
+	'undefined_group'   => 'El grupo %s no esta definidp en la configuración.',
 	'requires_mcrypt'   => 'Para usar la librería de Encriptación, mcrypt debe estar habilitado.',
 	'no_encryption_key' => 'Para usar la librería de Encriptación, tienes que especificar una llave de encriptación en tu archivo de configuración.',
 );
