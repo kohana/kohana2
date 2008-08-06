4d2d96ce20067ea8599d3f54cb408edccdda55f1
alle <alle@6c8b873b-1030-0410-a378-995d7067e127>
2008-08-06 09:09:42 +0000

    it_IT ready for 2.2

--- system/i18n/it_IT/orm.php
+++ system/i18n/it_IT/orm.php
@@ -1,6 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-$lang = array
-(
-	'query_methods_not_allowed'   => 'Non è consentito l\'uso dei metodi di Query mediante ORM.',
-);
+$lang['query_methods_not_allowed'] = 'Non è consentito l\'uso dei metodi di Query mediante ORM.';
