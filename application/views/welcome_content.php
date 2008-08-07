16edf5eb0210cafa9f5ecfad335029d1e07251a2
Geert <Geert@6c8b873b-1030-0410-a378-995d7067e127>
2008-08-07 17:34:02 +0000

    Freshening up the welcome page design

--- application/views/welcome_content.php
+++ application/views/welcome_content.php
@@ -1,11 +1,14 @@
-<p class="intro">This is the default Kohana index page. You may also access this page as <code><?php echo html::anchor('welcome/index', 'welcome/index') ?></code>.</p>
+<div class="box">
+	<p>This is the default Kohana index page. You may also access this page as <code><?php echo html::anchor('welcome/index', 'welcome/index') ?></code>.</p>
 
-<p>To change what gets displayed for this page, edit <code>application/controllers/welcome.php</code>.</p>
-
-<p>To change this text, edit <code>application/views/welcome_content.php</code>.
+	<p>
+		To change what gets displayed for this page, edit <code>application/controllers/welcome.php</code>.<br />
+		To change this text, edit <code>application/views/welcome_content.php</code>.
+	</p>
+</div>
 
 <ul>
 <?php foreach ($links as $title => $url): ?>
-<li><?php echo ($title === 'License') ? html::file_anchor($url, $title) : html::anchor($url, $title) ?></li>
+	<li><?php echo ($title === 'License') ? html::file_anchor($url, html::specialchars($title)) : html::anchor($url, html::specialchars($title)) ?></li>
 <?php endforeach ?>
 </ul>
--- system/views/kohana/template.php
+++ system/views/kohana/template.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-<title><?php echo $title ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title><?php echo html::specialchars($title) ?></title>
 
-<style type="text/css">
-html { background: #83c018; }
-body { width: 700px; margin: 2em auto; background: transparent url('<?php echo url::base(FALSE).'kohana.png' ?>') center 0 no-repeat; font-size: 76%; font-family: Arial, Verdana, sans-serif; color: #111; line-height: 1.5; }
-div, h2, a, p, code, pre, ul { font-family: inherit; color: inherit; padding: 0; margin: 0; text-align: baseline; text-decoration: none; }
-h2 { padding: 200px 0 0; }
-p { padding: 0 0 0.6em; }
-a { text-decoration: underline; }
-ul { list-style: none; padding: 1em 0; }
-ul li { display: inline; padding-right: 1em; }
-ul li:before { content: '» '; }
-pre { margin: 1em 0; padding: 1em; background: #fff; border: solid 0.6em #ddd; white-space: pre-wrap; }
-pre,
-code { font-family: fixed; }
-.box { padding: 1em 1em 2em; background: #eee; }
-.intro { padding: 1em 0; font-size: 1.2em; }
-.copyright { margin-top: 1em; text-align: center; font-size: 0.8em; text-transform: uppercase; color: #44640b; }
-</style>
+	<style type="text/css">
+	html { background: #83c018 url(<?php echo url::base(FALSE) ?>kohana.png) 50% 0 no-repeat; }
+	body { width: 52em; margin: 200px auto 2em; font-size: 76%; font-family: Arial, sans-serif; color: #273907; line-height: 1.5; text-align: center; }
+	h1 { font-size: 3em; font-weight: normal; text-transform: uppercase; color: #fff; }
+	a { color: inherit; }
+	code { font-size: 1.3em; }
+	ul { list-style: none; padding: 2em 0; }
+	ul li { display: inline; padding-right: 1em; text-transform: uppercase; }
+	ul li a { padding: 0.5em 1em; background: #69ad0f; border: 1px solid #569f09; color: #fff; text-decoration: none; }
+	ul li a:hover { background: #569f09; }
+	.box { padding: 2em; background: #98cc2b; border: 1px solid #569f09; }
+	.copyright { font-size: 0.9em; text-transform: uppercase; color: #557d10; }
+	</style>
 
 </head>
 <body>
 
-<h2><?php echo $title ?></h2>
-<?php echo $content ?>
+	<h1><?php echo html::specialchars($title) ?></h1>
+	<?php echo $content ?>
 
-<p class="copyright">Rendered in {execution_time} seconds, using {memory_usage} of memory<br/>Copyright &copy;2007-2008 Kohana Team</p>
+	<p class="copyright">
+		Rendered in {execution_time} seconds, using {memory_usage} of memory<br />
+		Copyright ©2007–2008 Kohana Team
+	</p>
 
 </body>
 </html>
