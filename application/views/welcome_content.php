d08b288400ebae43292fc6de22fcf3de0a20893e
OscarB <OscarB@6c8b873b-1030-0410-a378-995d7067e127>
2008-08-07 09:59:58 +0000

    follow up for r3285, forgot the views

--- application/views/welcome_content.php
+++ application/views/welcome_content.php
@@ -3,3 +3,9 @@
 <p>To change what gets displayed for this page, edit <code>application/controllers/welcome.php</code>.</p>
 
 <p>To change this text, edit <code>application/views/welcome.php</code> and <code>application/views/welcome_content.php</code>.
+
+<ul>
+<?php foreach ($links as $title => $url): ?>
+<li><?php echo ($title === 'License') ? html::file_anchor($url, $title) : html::anchor($url, $title) ?></li>
+<?php endforeach ?>
+</ul>
