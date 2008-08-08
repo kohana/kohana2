243dc27e06f2e0a5455875ef59f186f81d66318b
Edy <Edy@6c8b873b-1030-0410-a378-995d7067e127>
2008-08-08 18:36:09 +0000

    de_DE update (typo)

--- system/i18n/de_DE/event.php
+++ system/i18n/de_DE/event.php
@@ -2,6 +2,6 @@
 
 $lang = array
 (
-	'invalid_subject'  => 'Der Versuch das ungültige Subjekt %s an %s anzuhängen ist fehlgeschlagen. Subjekte müssen die Klasse Event_Subject erweitern.',
-	'invalid_observer' => 'Der Versuch den ungültigen Beobachter %s an %s anzuhängen ist fehlgeschlagen. Beobachter müssen die Klasse Event_Observer erweitern.',
+	'invalid_subject'  => 'Der Versuch, das ungültige Subjekt %s an %s anzuhängen, ist fehlgeschlagen. Subjekte müssen die Klasse Event_Subject erweitern.',
+	'invalid_observer' => 'Der Versuch, den ungültigen Beobachter %s an %s anzuhängen, ist fehlgeschlagen. Beobachter müssen die Klasse Event_Observer erweitern.',
 );
