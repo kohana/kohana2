2f3997ba129ab0e1f20c3acae204c0eaeb713604
alle <alle@6c8b873b-1030-0410-a378-995d7067e127>
2008-08-25 01:04:32 +0000

    it_IT for some modules: gmaps, kobot, object_db, unit_test, user_guide. Part of the job was previously taken by alf. Thanks ;)

---
+++ modules/user_guide/i18n/it_IT/user_guide.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang = array
+(
+	'title'     => 'Kohana - Guida utente',
+	'copyright' => 'copyright (c) %s Kohana Team :: Tutti i diritti riservati :: Generato in {execution_time} secondi utilizzando {memory_usage}MB di memoria',
+);
