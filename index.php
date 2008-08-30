536c734aaa71fb7b175675f8f5bbef646e68b0c0
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-08-30 19:36:37 +0000

    Updated index.php for branches/website, fixes #811. Thanks bio!

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ ini_set('display_errors', TRUE);
 
 /**
  * If you rename all of your .php files to a different extension, set the new
- * extension here. This option can left to .php, even if this file is has a
+ * extension here. This option can left to .php, even if this file has a
  * different extension.
  */
 define('EXT', '.php');
@@ -63,12 +63,15 @@ define('EXT', '.php');
 //
 // DO NOT EDIT BELOW THIS LINE, UNLESS YOU FULLY UNDERSTAND THE IMPLICATIONS.
 // ----------------------------------------------------------------------------
-// $Id: index.php 2005 2008-02-09 06:07:53Z PugFish $
+// $Id: index.php 3366 2008-08-27 22:15:39Z Shadowhand $
 //
 
 // Define the front controller name and docroot
 define('DOCROOT', getcwd().DIRECTORY_SEPARATOR);
-define('KOHANA',  substr(__FILE__, strlen(DOCROOT)));
+define('KOHANA',  basename(__FILE__));
+
+// If the front controller is a symlink, change to the real docroot
+is_link(KOHANA) and chdir(dirname(realpath(__FILE__)));
 
 // Define application and system paths
 define('APPPATH', str_replace('\\', '/', realpath($kohana_application)).'/');
@@ -78,16 +81,34 @@ define('SYSPATH', str_replace('\\', '/', realpath($kohana_system)).'/');
 // Clean up
 unset($kohana_application, $kohana_modules, $kohana_system);
 
-(is_dir(APPPATH) AND is_dir(APPPATH.'/config')) or die
-(
-	'Your <code>$kohana_application</code> directory does not exist. '.
-	'Set a valid <code>$kohana_application</code> in <tt>'.KOHANA.'</tt> and refresh the page.'
-);
+if ( ! IN_PRODUCTION)
+{
+	// Check APPPATH
+	if ( ! (is_dir(APPPATH) AND is_file(APPPATH.'config/config'.EXT)))
+	{
+		die
+		(
+			'<div style="width:80%;margin:50px auto;text-align:center;">'.
+				'<h3>Application Directory Not Found</h3>'.
+				'<p>The <code>$kohana_application</code> directory does not exist.</p>'.
+				'<p>Set <code>$kohana_application</code> in <tt>'.KOHANA.'</tt> to a valid directory and refresh the page.</p>'.
+			'</div>'
+		);
+	}
 
-(is_dir(SYSPATH) AND file_exists(SYSPATH.'/core/'.'Bootstrap'.EXT)) or die
-(
-	'Your <code>$kohana_system</code> directory does not exist. '.
-	'Set a valid <code>$kohana_system</code> in <tt>'.KOHANA.'</tt> and refresh the page.'
-);
+	// Check SYSPATH
+	if ( ! (is_dir(SYSPATH) AND is_file(SYSPATH.'bootstrap'.EXT)))
+	{
+		die
+		(
+			'<div style="width:80%;margin:50px auto;text-align:center;">'.
+				'<h3>System Directory Not Found</h3>'.
+				'<p>The <code>$kohana_system</code> directory does not exist.</p>'.
+				'<p>Set <code>$kohana_system</code> in <tt>'.KOHANA.'</tt> to a valid directory and refresh the page.</p>'.
+			'</div>'
+		);
+	}
+}
 
-require SYSPATH.'core/Bootstrap'.EXT;
+// Initialize Kohana
+require SYSPATH.'bootstrap'.EXT;
