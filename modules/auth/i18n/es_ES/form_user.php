a6adb5d3678a5d44e425212be1b36a816bb0402d
alexsancho <alexsancho@6c8b873b-1030-0410-a378-995d7067e127>
2008-08-31 23:17:42 +0000

    es_ES translation for auth module (see r3403)

---
+++ modules/auth/i18n/es_ES/form_user.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['email']['default'] = 'Por favor, introduce una dirección de correo valida.';
+
+$lang['username']['default'] = 'Por favor, introduce un nombre de usuario valido, entre 4-32 caracteres.';
+$lang['username']['chars'] = 'El nombre de usuario solo puede contener letras, numeros, guiones bajos y puntos.';
+
+$lang['password']['default'] = 'Por favor, introduce una contraseña de al menos 5 caracteres.';
+
+$lang['password_confirm']['default'] = 'Las contrseñas no se corresponden!';
