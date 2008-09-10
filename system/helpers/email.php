fa4e145f5650ff6da867a549b43ae96e12cbc139
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-09-10 22:03:55 +0000

    Merged r3425 into tags/2.2.1

--- system/helpers/email.php
+++ system/helpers/email.php
@@ -95,7 +95,7 @@ class email_Core {
 			break;
 			default:
 				// Use the native connection
-				$connection = new Swift_Connection_NativeMail;
+				$connection = new Swift_Connection_NativeMail($config['options']);
 			break;
 		}
 
