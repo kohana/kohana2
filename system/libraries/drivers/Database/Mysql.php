169a33b859282f5c543bf863b3d7b57e331e7a3a
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-09-23 18:46:25 +0000

    Merged r3475 into tags/2.2.1

--- system/libraries/drivers/Database/Mysql.php
+++ system/libraries/drivers/Database/Mysql.php
@@ -472,6 +472,9 @@ class Mysql_Result extends Database_Result {
 	{
 		if ($this->offsetExists($offset) AND mysql_data_seek($this->result, $offset))
 		{
+			// Set the current row to the offset
+			$this->current_row = $offset;
+
 			return TRUE;
 		}
 		else
