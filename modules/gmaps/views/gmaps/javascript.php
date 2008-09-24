a353c566eedd6653c59d917522f5f2c4ee19ee60
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-09-24 18:16:46 +0000

    Fixing a bug in Gmaps that could cause an error if custom icons are not used.

--- modules/gmaps/views/gmaps/javascript.php
+++ modules/gmaps/views/gmaps/javascript.php
@@ -1,21 +1,23 @@
 google.load("maps", "2.x", {"language" : "<?php echo substr(Kohana::config('locale.language.0'), 0, 2);?>"});
 function initialize() {
-    if (GBrowserIsCompatible()) {
+	if (GBrowserIsCompatible()) {
 		// Initialize the GMap
 		<?php echo $map, "\n" ?>
-        <?php echo $controls, "\n" ?>
-        <?php echo $center, "\n" ?>
-        <?php echo $options->render(1), "\n" ?>
-	
-	// Build custom marker icons
-	<?php foreach($icons as $icon): ?>
-	<?php echo $icon->render(1), "\n" ?>
-	<?php endforeach ?>
+		<?php echo $controls, "\n" ?>
+		<?php echo $center, "\n" ?>
+		<?php echo $options->render(1), "\n" ?>
+		<?php if ( ! empty($icons)): ?>
 
-        // Show map points
-        <?php foreach($markers as $marker): ?>
-        <?php echo $marker->render(1), "\n" ?>
-        <?php endforeach ?>
-    }
+		// Build custom marker icons
+		<?php foreach($icons as $icon): ?>
+		<?php echo $icon->render(1), "\n" ?>
+		<?php endforeach ?>
+		<?php endif ?>
+
+		// Show map points
+		<?php foreach($markers as $marker): ?>
+		<?php echo $marker->render(1), "\n" ?>
+		<?php endforeach ?>
+	}
 }
 google.setOnLoadCallback(initialize);
