4a0d1a17d470cbddfb01af8aaf0f2f40f7e31d49
kamilko <kamilko@6c8b873b-1030-0410-a378-995d7067e127>
2008-10-01 20:07:21 +0000

    Added pl_PL translation for auth module

---
+++ modules/auth/i18n/pl_PL/form_login.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['username']['default'] = 'Proszę wprowadzić prawidłową nazwę użytkownika.';
+$lang['username']['not_found'] = 'Nie można znaleźć takiego użytkownika.';
+
+$lang['password']['default'] = 'Proszę wprowadzić prawidłowe hasło.';
+$lang['password']['incorrect'] = 'Nieprawidłowy użytkownik i/lub hasło.';
+
---
+++ modules/auth/i18n/pl_PL/form_user.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['email']['default'] = 'Proszę wprowadzić poprawny adres email.';
+
+$lang['username']['default'] = 'Proszę wprowadzić prawidłową nazwę użytkownika o długości od 4 do 32 znaków.';
+$lang['username']['chars'] = 'Nazwa użytkownika może zawierać wyłącznie litery, cyfry oraz znaki podkreślenia i minusa.';
+
+$lang['password']['default'] = 'Proszę wprowadzić hasło o minimum pięciu znakach.';
+
+$lang['password_confirm']['default'] = 'Hasła nie pasują!';
+
