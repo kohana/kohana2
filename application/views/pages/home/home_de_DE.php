7fed1ae91f864742764b9ee972829e909281810e
Edy <Edy@6c8b873b-1030-0410-a378-995d7067e127>
2008-10-02 17:19:09 +0000

    de_DE website update

--- application/views/pages/home/home_de_DE.php
+++ application/views/pages/home/home_de_DE.php
@@ -1,4 +1,4 @@
-<p class="intro">Kohana ist ein <strong>PHP-5-Framework</strong>, bei dem die <strong>Model-View-Controller</strong>-Architektur zum Einsatz. Ziel ist es <strong>sicher</strong>, <strong>schlank</strong> und <strong>leicht bedienbar</strong> zu sein.</p>
+<p class="intro">Kohana ist ein <strong>PHP-5-Framework</strong>, bei dem die <strong>Model-View-Controller</strong>-Architektur zum Einsatz kommt. Ziel ist es <strong>sicher</strong>, <strong>schlank</strong> und <strong>leicht bedienbar</strong> zu sein.</p>
 <div style="float:left; padding-right: 4em;">
 	<h2>Eigenschaften</h2>
 	<ul>
@@ -31,7 +31,7 @@
 	<li><strong>GET-, POST-, COOKIE-, <em>und</em> SESSION-Arrays funktionieren, wie gewohnt.</strong> Kohana beschränkt nicht den Zugriff auf globale Daten, sondern bietet deren Filterung sowie <abbr title="Cross Site Scripting">XSS</abbr>-Schutz an.</li>
 	<li><strong>Echtes auto-loading von Klassen.</strong> Klassen werden geladen, sobald diese in ihrer Applikation gebraucht werden.</li>
 	<li><strong>Keine Namensraum-Konflikte.</strong> Allen Klassen sind Suffixe angehängt, um ähnliche Namen innerhalb von Komponenten zu erlauben, so dass eine einheitliche API entsteht.</li>
-	<li><strong>Kaskadierte Ressourcen erlauben einmalige Erweiterbarkeit.</strong> Nahezu jeder Bestandteil in Kohana kann überladen oder erweitert werden, ohne System-Dateien bearbeiten zu müssen. Module erlauben es Plugins mit mehreren Dateien zu Ihrer Applikation transparent hinzuzufügen.</li>
+	<li><strong><a href="http://upload.wikimedia.org/wikipedia/en/1/1c/Kohana-modules.png">Kaskadierte Ressourcen</a> erlauben einmalige Erweiterbarkeit.</strong> Nahezu jeder Bestandteil in Kohana kann überladen oder erweitert werden, ohne System-Dateien bearbeiten zu müssen. Module erlauben es Plugins mit mehreren Dateien zu Ihrer Applikation transparent hinzuzufügen.</li>
 	<li><strong>Treiber-Bibliotheken und konsistente API.</strong> Bibliotheken können verschiedene "Treiber" nutzen, um externe <abbr title="Application Programming Interface">API</abbr>s transparent zu verarbeiten. Es gibt beispielsweise verschiedene Möglichkeiten eine Session zu speichern (Nativ, Datenbank und Cookies), jedoch wird das selbe Interface für alle Treiber benutzt. Dies stellt sicher, dass neue Treiber für vorhandene Bibliotheken entwickelt werden können, was die Konsistenz und Transparenz des API erhält.</li>
 	<li><strong>Mächtige Ereignisbehandlung.</strong> Ereignisbehandlung im Observer-Stil ermöglicht ein sehr hohes Anpassungspotenzial.</li>
 	<li><strong>Rasanter Entwicklungszyklus.</strong> Rasante Entwicklung ermöglicht eine schnellere Reaktion auf Fehlermeldungen und Wünsche der Benutzer.</li>
