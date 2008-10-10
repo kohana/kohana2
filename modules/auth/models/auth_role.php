37fbf70cb2bc6bb54c39ab9e9b5aa86fb5ef3bbe
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-10-10 18:56:24 +0000

    Follow up to r3572, merged Auth models from trunk into tags/2.2.1

--- modules/auth/models/auth_role.php
+++ modules/auth/models/auth_role.php
@@ -5,6 +5,23 @@ class Auth_Role_Model extends ORM {
 	protected $has_and_belongs_to_many = array('users');
 
 	/**
+	 * Validates and optionally saves a role record from an array.
+	 *
+	 * @param  array    values to check
+	 * @param  boolean  save the record when validation succeeds
+	 * @return boolean
+	 */
+	public function validate(array & $array, $save = FALSE)
+	{
+		$array = Validation::factory($array)
+			->pre_filter('trim')
+			->add_rules('name', 'required', 'length[4,32]')
+			->add_rules('description', 'length[0,255]');
+
+		return parent::validate($array, $save);
+	}
+
+	/**
 	 * Allows finding roles by name.
 	 */
 	public function unique_key($id)
--- modules/auth/models/auth_user.php
+++ modules/auth/models/auth_user.php
@@ -6,6 +6,9 @@ class Auth_User_Model extends ORM {
 	protected $has_many = array('user_tokens');
 	protected $has_and_belongs_to_many = array('roles');
 
+	// Columns to ignore
+	protected $ignored_columns = array('password_confirm');
+
 	public function __set($key, $value)
 	{
 		if ($key === 'password')
@@ -18,14 +21,34 @@ class Auth_User_Model extends ORM {
 	}
 
 	/**
-	 * Tests if a username exists in the database.
+	 * Validates and optionally saves a new user record from an array.
+	 *
+	 * @param  array    values to check
+	 * @param  boolean  save the record when validation succeeds
+	 * @return boolean
+	 */
+	public function validate(array & $array, $save = FALSE)
+	{
+		$array = Validation::factory($array)
+			->pre_filter('trim')
+			->add_rules('email', 'required', 'length[4,127]', 'valid::email')
+			->add_rules('username', 'required', 'length[4,32]', 'chars[a-zA-Z0-9_.]', array($this, 'username_available'))
+			->add_rules('password', 'required', 'length[5,42]')
+			->add_rules('password_confirm', 'matches[password]');
+
+		return parent::validate($array, $save);
+	}
+
+	/**
+	 * Tests if a username exists in the database. This can be used as a
+	 * Valdidation rule.
 	 *
 	 * @param   mixed    id to check
 	 * @return  boolean
 	 */
-	public function username_exists($id)
+	public function username_available($id)
 	{
-		return (bool) $this->db
+		return ! $this->db
 			->where($this->unique_key($id), $id)
 			->count_records($this->table_name);
 	}
