efedd7b341aab4081bd5549926364a8e449af09d
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-10-10 01:17:54 +0000

    Follow up to r3549

--- system/classes/url.php
+++ system/classes/url.php
@@ -177,7 +177,7 @@ class url_Core {
 	}
 
 	/**
-	 * Sends a page redirect header.
+	 * Sends a page redirect header and runs the system.redirect Event.
 	 *
 	 * @param  mixed   string site URI or URL to redirect to, or array of strings if method is 300
 	 * @param  string  HTTP method of redirect
@@ -190,66 +190,53 @@ class url_Core {
 			return FALSE;
 		}
 
-		$uri = (array) $uri;
-
-		for ($i = 0, $count_uri = count($uri); $i < $count_uri; $i++)
+		if ($method === '300')
 		{
-			if (strpos($uri[$i], '://') === FALSE)
+			$uri = (array) $uri;
+
+			$output = '<ul>';
+			foreach ($uri as $link)
 			{
-				$uri[$i] = url::site($uri[$i]);
+				$output .= '<li>'.html::anchor($link).'</li>';
 			}
-		}
+			$output .= '</ul>';
 
-		if ($method == '300')
-		{
-			if ($count_uri > 0)
-			{
-				// Run the redirect event
-				Event::run('system.redirect', $uri);
-
-				header('HTTP/1.1 300 Multiple Choices');
-				header('Location: '.$uri[0]);
-
-				$choices = '';
-				foreach ($uri as $href)
-				{
-					$choices .= '<li><a href="'.$href.'">'.$href.'</a></li>';
-				}
-
-				exit('<h1>301 - Multiple Choices:</h1><ul>'.$choices.'</ul>');
-			}
+			// The first URI will be used for the Location header
+			$uri = $uri[0];
 		}
 		else
 		{
-			$uri = $uri[0];
-
-			// Run the redirect event
-			Event::run('system.redirect', $uri);
-
-			if ($method == 'refresh')
-			{
-				header('Refresh: 0; url='.$uri);
-			}
-			else
-			{
-				$codes = array
-				(
-					'301' => 'Moved Permanently',
-					'302' => 'Found',
-					'303' => 'See Other',
-					'304' => 'Not Modified',
-					'305' => 'Use Proxy',
-					'307' => 'Temporary Redirect'
-				);
-
-				$method = isset($codes[$method]) ? $method : '302';
-
-				header('HTTP/1.1 '.$method.' '.$codes[$method]);
-				header('Location: '.$uri);
-			}
-
-			exit('<h1>'.$method.' - '.$codes[$method].'</h1><p><a href="'.$uri.'">'.$uri.'</a></p>');
+			$output = '<p>'.html::anchor($uri).'</p>';
 		}
+
+		// Run the redirect event
+		Event::run('system.redirect', array('uri' => $uri);
+
+		if ($method == 'refresh')
+		{
+			header('Refresh: 0; url='.$uri);
+		}
+		else
+		{
+			$codes = array
+			(
+				'300' => 'Multiple Choices',
+				'301' => 'Moved Permanently',
+				'302' => 'Found',
+				'303' => 'See Other',
+				'304' => 'Not Modified',
+				'305' => 'Use Proxy',
+				'307' => 'Temporary Redirect'
+			);
+
+			$method = isset($codes[$method]) ? $method : '302';
+
+			header('HTTP/1.1 '.$method.' '.$codes[$method]);
+			header('Location: '.$uri);
+		}
+
+		exit('<h1>'.$method.' - '.$codes[$method].'</h1>'.$output);
+
 	}
 
 } // End url
