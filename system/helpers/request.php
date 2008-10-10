db8ea0f79da0be69da06833fe2c159c349bf7302
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-10-10 14:22:10 +0000

    Merged request::protocol from r3405 into tags/2.2.1

--- system/helpers/request.php
+++ system/helpers/request.php
@@ -41,6 +41,28 @@ class request_Core {
 	}
 
 	/**
+	 * Returns the current request protocol, based on $_SERVER['https']. In CLI
+	 * mode, NULL will be returned.
+	 *
+	 * @return  string
+	 */
+	public static function protocol()
+	{
+		if (PHP_SAPI === 'cli')
+		{
+			return NULL;
+		}
+		elseif ( ! empty($_SERVER['HTTPS']) AND $_SERVER['HTTPS'] === 'on')
+		{
+			return 'https';
+		}
+		else
+		{
+			return 'http';
+		}
+	}
+
+	/**
 	 * Tests if the current request is an AJAX request by checking the X-Requested-With HTTP
 	 * request header that most popular JS frameworks now set for AJAX calls.
 	 *
