64f6480ce9f1e68b68fe1578e1bdc61a1f68b013
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-10-12 22:57:39 +0000

    Reverted r3509 in tags/2.2.1

--- system/libraries/ORM.php
+++ system/libraries/ORM.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 /**
  * [Object Relational Mapping][ref-orm] (ORM) is a method of abstracting database
  * access to standard PHP calls. All table rows are represented as model objects,
@@ -531,7 +531,7 @@ class ORM_Core {
 	{
 		if ( ! $array->submitted())
 		{
-			$safe_array = $array->as_array();
+			$safe_array = $array->safe_array();
 
 			foreach ($safe_array as $key => $val)
 			{
@@ -543,7 +543,7 @@ class ORM_Core {
 		// Validate the array
 		if ($status = $array->validate())
 		{
-			$safe_array = $array->as_array();
+			$safe_array = $array->safe_array();
 
 			foreach ($safe_array as $key => $val)
 			{
