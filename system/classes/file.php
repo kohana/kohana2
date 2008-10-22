7b673beeefc832bfbd879882f0251b578a941627
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-10-22 01:58:29 +0000

    Fixing #777, thanks Inquisitus.

--- system/classes/file.php
+++ system/classes/file.php
@@ -62,6 +62,15 @@ class file_Core {
 			return mime_content_type($filename);
 		}
 
+		if ( ! KOHANA_IS_WIN)
+		{
+			// Attempt to locate use the file command, checking the return value
+			if ($command = trim(exec('which file', $output, $return)) AND $return === 0)
+			{
+				return trim(exec($command.' -bi '.escapeshellarg($filename)));
+			}
+		}
+
 		if ( ! empty($extension) AND is_array($mime = Kohana::config('mimes.'.$extension)))
 		{
 			// Return the mime-type guess, based on the extension
