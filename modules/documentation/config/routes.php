f6ad5caf1c5c1c2148c0192e36b2deed2fb7898f
Shadowhand <Shadowhand@6c8b873b-1030-0410-a378-995d7067e127>
2008-11-13 19:52:38 +0000

    Updated documentation routes for r3669

--- modules/documentation/config/routes.php
+++ modules/documentation/config/routes.php
@@ -1,62 +1,63 @@
 <?php
+
 $config['kohana_documentation'] = array
 (
-	'route' => array
-	(
-		'uri' => 'docs',
-	),
+	'uri' => 'docs',
 
-	'controller' => 'kohana_documentation',
-	'method'     => 'index'
+	'defaults' => array
+	(
+		'controller' => 'kohana_documentation',
+		'method'     => 'index'
+	),
 );
+
 
 $config['kohana_media'] = array
 (
-	'route' => array
+	'uri'   => 'docs/:method/:file',
+	'regex' => array('method' => 'js|css|img')
+
+	'defaults' => array
 	(
-		'uri'   => 'docs/:method/:file',
-		'regex' => array('method' => 'js|css|img')
+		'controller' => 'kohana_documentation',
+		'method'     => 'user_guide',
 	),
-
-	'controller' => 'kohana_documentation',
-	'method'     => 'user_guide',
-
 );
 
 $config['kohana_user_guide'] = array
 (
-	'route' => array
-	(
-		'uri'   => 'docs/user_guide/:lang/:page/:dummy',
-		'regex' => array('dummy' => '.*')
-	),
+	'uri'   => 'docs/user_guide/:lang/:page/:dummy',
+	'regex' => array('dummy' => '.*')
 
-	'controller' => 'kohana_documentation',
-	'method'     => 'user_guide',
-	'lang'       => 'en',
-	'page'       => 'contents'
+	'defaults' => array
+	(
+		'controller' => 'kohana_documentation',
+		'method'     => 'user_guide',
+		'lang'       => 'en',
+		'page'       => 'contents'
+	),
 );
 
 $config['kohana_api_browse'] = array
 (
-	'route' => array
-	(
-		'uri' => 'docs/api/browse/:sort',
-	),
+	'uri' => 'docs/api/browse/:sort',
 
-	'controller' => 'kohana_documentation',
-	'method'     => 'browse_api',
-	'sort'       => 'name'
+	'defaults' => array
+	(
+		'controller' => 'kohana_documentation',
+		'method'     => 'browse_api',
+		'sort'       => 'name'
+	),
 );
 
 $config['kohana_api'] = array
 (
-	'route' => array
-	(
-		'uri' => 'docs/api/class/:class',
-	),
+	'uri' => 'docs/api/class/:class',
 
-	'controller' => 'kohana_documentation',
-	'method'     => 'api',
-	'class'      => false
+	'defaults' => array
+	(
+		'controller' => 'kohana_documentation',
+		'method'     => 'api',
+		'class'      => false
+	),
 );
