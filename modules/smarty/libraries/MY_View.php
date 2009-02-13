4ed0ef3ad56ccd78667449ab0dfa4260d4fcb343
ixmatus <ixmatus@6c8b873b-1030-0410-a378-995d7067e127>
2009-02-13 21:37:51 +0000

    Fixing my indentation - for some reason it switched to spaces instead of tabs...

--- modules/smarty/libraries/MY_View.php
+++ modules/smarty/libraries/MY_View.php
@@ -4,7 +4,7 @@ class View extends View_Core {
 
 	public function __construct($name, $data = NULL, $type = NULL)
 	{
-                $smarty_ext = Kohana::config('smarty.templates_ext');
+		$smarty_ext = Kohana::config('smarty.templates_ext');
 
 		if (Kohana::config('smarty.integration') == TRUE AND Kohana::find_file('views', $name, FALSE, (empty($type) ? $smarty_ext : $type)))
 		{
