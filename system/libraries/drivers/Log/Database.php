d934e715a76930ebc6165b00e21c32443fee108c
zombor <zombor@6c8b873b-1030-0410-a378-995d7067e127>
2009-04-18 16:39:21 +0000

    Adding database driver for #1265.

---
+++ system/libraries/drivers/Log/Database.php
@@ -0,0 +1,24 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * Log API driver.
+ *
+ * @package    Kohana_Log
+ * @author     Kohana Team
+ * @copyright  (c) 2007-2009 Kohana Team
+ * @license    http://kohanaphp.com/license.html
+ */
+class Log_Database_Driver extends Log_Driver {
+
+	public function save(array $messages)
+	{
+		do
+		{
+			// Load the next message
+			$to_insert = array_combine(array('date', 'level', 'message'), array_shift($messages));
+
+			// Add a new message line
+			Database::instance($this->config['group'])->insert($this->config['table'], $to_insert)
+		}
+		while ( ! empty($messages));
+	}
+}
