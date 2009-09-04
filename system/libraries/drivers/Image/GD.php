ea60476a6159790d675ff2d9eecf2267a509fd01
isaiah <isaiah@6c8b873b-1030-0410-a378-995d7067e127>
2009-09-04 22:46:36 +0000

    Replaced imagecopymerge() to support the alpha channel. Fixes #1888 Thanks Dmitry!

--- system/libraries/drivers/Image/GD.php
+++ system/libraries/drivers/Image/GD.php
@@ -336,22 +336,51 @@ class Image_GD_Driver extends Image_Driver {
 		switch($properties['mime'])
 		{
 			case "image/jpeg":
-			$overlay_img = imagecreatefromjpeg($properties['overlay_file']);
+				$overlay_img = imagecreatefromjpeg($properties['overlay_file']);
 			break;
 
 			case "image/gif":
-			$overlay_img = imagecreatefromgif($properties['overlay_file']);
+				$overlay_img = imagecreatefromgif($properties['overlay_file']);
 			break;
 
 			case "image/png":
-			$overlay_img = imagecreatefrompng($properties['overlay_file']);
+				$overlay_img = imagecreatefrompng($properties['overlay_file']);
 			break;
 		}
 
-		imagecopymerge($this->tmp_image, $overlay_img, $properties['x'], $properties['y'], 0, 0, imagesx($overlay_img), imagesy($overlay_img), $properties['transparency']);
+		$this->imagecopymerge_alpha($this->tmp_image, $overlay_img, $properties['x'], $properties['y'], 0, 0, imagesx($overlay_img), imagesy($overlay_img), $properties['transparency']);
+
 		imagedestroy($overlay_img);
+
 		return TRUE;
 	}
+
+	/**
+	 * A replacement for php's imagecopymerge() function that supports the alpha channel
+	 * See php bug #23815:  http://bugs.php.net/bug.php?id=23815
+	 *
+	 * @param  resource	$dst_im		Destination image link resource
+	 * @param  resource	$src_im		Source image link resource
+	 * @param  integer	$dst_x		x-coordinate of destination point
+	 * @param  integer	$dst_y		y-coordinate of destination point
+	 * @param  integer	$src_x		x-coordinate of source point
+	 * @param  integer	$src_y		y-coordinate of source point
+	 * @param  integer	$src_w		Source width
+	 * @param  integer	$src_h		Source height
+	 * @param  integer	$pct		Transparency percent (0 to 100)
+	 */
+    protected function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct)
+	{
+        // Create a new blank image the site of our source image
+        $cut = imagecreatetruecolor($src_w, $src_h);
+
+		// Copy the blank image into the destination image where the source goes
+        imagecopy($cut, $dst_im, 0, 0, $dst_x, $dst_y, $src_w, $src_h);
+
+        // Place the source image in the destination image
+        imagecopy($cut, $src_im, 0, 0, $src_x, $src_y, $src_w, $src_h);
+        imagecopymerge($dst_im, $cut, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct);
+    }
 
 	protected function properties()
 	{
