527a102916bf1581b3d666143b6819dd679e85ab
isaiah <isaiah@6c8b873b-1030-0410-a378-995d7067e127>
2009-09-12 04:25:17 +0000

    Fixes #2105

--- system/helpers/html.php
+++ system/helpers/html.php
@@ -23,30 +23,8 @@ class html_Core {
 	 */
 	public static function specialchars($str, $double_encode = TRUE)
 	{
-		// Force the string to be a string
-		$str = (string) $str;
-
-		// Do encode existing HTML entities (default)
-		if ($double_encode === TRUE)
-		{
-			$str = htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-		}
-		else
-		{
-			// Do not encode existing HTML entities
-			// From PHP 5.2.3 this functionality is built-in, otherwise use a regex
-			if (version_compare(PHP_VERSION, '5.2.3', '>='))
-			{
-				$str = htmlspecialchars($str, ENT_QUOTES, 'UTF-8', FALSE);
-			}
-			else
-			{
-				$str = preg_replace('/&(?!(?:#\d++|[a-z]++);)/ui', '&amp;', $str);
-				$str = str_replace(array('<', '>', '\'', '"'), array('&lt;', '&gt;', '&#39;', '&quot;'), $str);
-			}
-		}
-
-		return $str;
+		// Return HTML entities using the Kohana charset
+		return htmlspecialchars($str, ENT_QUOTES, Kohana::CHARSET, $double_encode);
 	}
 
 	/**
