90a94ade6c50de96e385b2009179cabea2338049
ixmatus <ixmatus@6c8b873b-1030-0410-a378-995d7067e127>
2010-02-18 07:28:09 +0000

    Finishing up docs for the num helper

--- system/helpers/num.php
+++ system/helpers/num.php
@@ -2,9 +2,17 @@
 /**
  * Number helper class.
  *
+ * ###### Using the num helper:
+ *
+ *     // Using the num helper is simple:
+ *     echo Kohana::debug(url::round(24));
+ *
+ *     // Output:
+ *     (double) 25
+ *
  * @package    Kohana
  * @author     Kohana Team
- * @copyright  (c) 2007-2009 Kohana Team
+ * @copyright  (c) 2007-2010 Kohana Team
  * @license    http://kohanaphp.com/license
  */
 class num_Core {
@@ -12,8 +20,15 @@ class num_Core {
 	/**
 	 * Round a number to the nearest nth
 	 *
-	 * @param   integer  number to round
-	 * @param   integer  number to round to
+	 * ###### Example
+	 *
+	 *     echo Kohana::debug(url::round(24));
+	 *
+	 *     // Output:
+	 *     (double) 25
+	 *
+	 * @param   integer  $number	Number to round
+	 * @param   integer  $nearest	Number to round to
 	 * @return  integer
 	 */
 	public static function round($number, $nearest = 5)
