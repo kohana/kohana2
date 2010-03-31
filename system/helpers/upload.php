b8a1f98bcd3f6d69ff1b61d1582d86b68ef53093
ixmatus <ixmatus@6c8b873b-1030-0410-a378-995d7067e127>
2010-03-31 18:56:56 +0000

    Fixing #2599

--- system/helpers/upload.php
+++ system/helpers/upload.php
@@ -1,23 +1,86 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 /**
- * Upload helper class for working with the global $_FILES
- * array and Validation library.
+ * THe upload helper class provides convenience methods for working
+ * with and manipulating the global $_FILES array.
  *
+ * [!!] Vanilla server-side file upload procedures can only validate the size of a file *once it is uploaded on the server*
+ * 
+ * For file size validation **before** upload, research
+ * Javascript/AJAX methods of validation pre-upload.
+ *
+ * ##### Complete Example
+ *     
+ *     $files = Validation::factory($_FILES)->add_rules('picture', 'upload::valid', 'upload::required', 'upload::type[gif,jpg,png]', 'upload::size[1M]');
+ *
+ *     if ($files->validate())
+ *     {
+ *			// Temporary file name
+ *			$filename = upload::save('picture');
+ *
+ *			// Resize, sharpen, and save the image
+ *			Image::factory($filename)->resize(100, 100, Image::WIDTH)
+ *									 ->save(DOCROOT.'media/pictures/'.basename($filename));
+ *			// Remove the temporary file
+ *			unlink($filename);
+ *     }
+ *
+ * ##### Upload Multiple Files
+ *     
+ *     foreach( arr::rotate($_FILES['image']) as $file )
+ *     {
+ *			$filename = upload::save($file);
+ *     
+ *			Image::factory($filename)->resize(30, 30, Image::AUTO)
+ *									 ->save(DOCROOT.'upload/'.basename($filename));
+ *			unlink($filename);
+ *     }
+ *     
  * @package    Kohana
  * @author     Kohana Team
- * @copyright  (c) 2007-2009 Kohana Team
+ * @copyright  (c) 2007-2010 Kohana Team
  * @license    http://kohanaphp.com/license
  */
 class upload_Core {
 
 	/**
-	 * Save an uploaded file to a new location.
+	 * This method will write a given uploaded file to the filesystem.
+	 * 
+	 * Provided a new filename, target directory, and permissions
+	 * bitmask the file will be written appropriately.
 	 *
-	 * @param   mixed    name of $_FILE input or array of upload data
-	 * @param   string   new filename
-	 * @param   string   new directory
-	 * @param   integer  chmod mask
-	 * @return  string   full path to new file
+	 * The first function argument takes as its value a string or
+	 * array and specifies the identifier of the file to be written
+	 * contained within the `$_FILES` global array given by the
+	 * submitting form input field. If supplied an array, a *name*
+	 * key will be the default key => value pair from which the
+	 * uploaded filename is to be assumed.
+	 *
+	 * The second function argument takes as its value a string and is
+	 * used to replace the given filename upon writing.
+	 *
+	 * The third function argument takes as its value a string and
+	 * specifies the target directory of which the file is to be
+	 * written.
+	 *
+	 * The fourth function argument takes as its value an octal
+	 * integer (leading zero) and is a bitmask representing the
+	 * intended permissions for that file.
+	 *
+	 * ###### Example
+	 *     
+	 *     // Assuming the value of $_FILES['name'] is "picture"
+	 *     $filename = upload::save('picture', 'my_new_file01.jpg', '/some/new/location/');
+	 *     
+	 *     echo Kohana::debug($filename);
+	 *     
+	 *     // Output:
+	 *     (string) /some/new/location/my_new_file01.jpg
+	 *
+	 * @param   mixed    $file      Name of $_FILE input or array of upload data
+	 * @param   string   $filename  Replacement filename
+	 * @param   string   $directory Target directory
+	 * @param   integer  $chmod     Octal bitmask
+	 * @return  string   Full path to the writte file
 	 */
 	public static function save($file, $filename = NULL, $directory = NULL, $chmod = 0644)
 	{
@@ -72,9 +135,17 @@ class upload_Core {
 	/* Validation Rules */
 
 	/**
-	 * Tests if input data is valid file type, even if no upload is present.
+	 * This method tests the input data to validate if it is of a
+	 * valid file type, even if no upload is present.
 	 *
-	 * @param   array  $_FILES item
+	 * [!!] These methods *can* be used independently; but, they are best utilized in conjunction with [Validation:add_rules]
+	 *
+	 * ###### Example
+	 *     
+	 *     // Assuming the value of $_FILES['name'] is "picture"
+	 *     $files = Validation::factory($_FILES)->add_rules('picture', 'upload::valid');
+	 * 
+	 * @param   array  $file $_FILES item
 	 * @return  bool
 	 */
 	public static function valid($file)
@@ -88,9 +159,17 @@ class upload_Core {
 	}
 
 	/**
-	 * Tests if input data has valid upload data.
+	 * This method tests the input data to validate if it has
+	 * *non*-empty data.
 	 *
-	 * @param   array    $_FILES item
+	 * [!!] These methods *can* be used independently; but, they are best utilized in conjunction with [Validation:add_rules]
+	 *
+	 * ###### Example
+	 *     
+	 *     // Assuming the value of $_FILES['name'] is "picture"
+	 *     $files = Validation::factory($_FILES)->add_rules('picture', 'upload::required');
+	 *
+	 * @param   array    $file $_FILES item
 	 * @return  bool
 	 */
 	public static function required(array $file)
@@ -102,10 +181,18 @@ class upload_Core {
 	}
 
 	/**
-	 * Validation rule to test if an uploaded file is allowed by extension.
+	 * This method tests the input data to validate if it is permitted
+	 * by extension.
 	 *
-	 * @param   array    $_FILES item
-	 * @param   array    allowed file extensions
+	 * [!!] These methods *can* be used independently; but, they are best utilized in conjunction with [Validation:add_rules]
+	 *
+	 * ###### Example
+	 *     
+	 *     // Assuming the value of $_FILES['name'] is "picture"
+	 *     $files = Validation::factory($_FILES)->add_rules('picture', 'upload::type[gif,jpg,png]');
+	 * 
+	 * @param   array    $file          $_FILES item
+	 * @param   array    $allowed_types Allowed file extensions
 	 * @return  bool
 	 */
 	public static function type(array $file, array $allowed_types)
@@ -121,13 +208,25 @@ class upload_Core {
 	}
 
 	/**
-	 * Validation rule to test if an uploaded file is allowed by file size.
-	 * File sizes are defined as: SB, where S is the size (1, 15, 300, etc) and
-	 * B is the byte modifier: (B)ytes, (K)ilobytes, (M)egabytes, (G)igabytes.
-	 * Eg: to limit the size to 1MB or less, you would use "1M".
+	 * This method tests the input data to validate if it is allowed
+	 * by file size.
 	 *
-	 * @param   array    $_FILES item
-	 * @param   array    maximum file size
+	 * File sizes are defined as: SB, where S is the size (1, 15, 300,
+	 * etc) and B is the byte modifier: (B)ytes, (K)ilobytes,
+	 * (M)egabytes, (G)igabytes. Eg: to limit the size to 1MB or less,
+	 * you would use "1M".
+	 *
+	 * @link http://en.wikipedia.org/wiki/Binary_prefix
+	 *
+	 * [!!] These methods *can* be used independently; but, they are best utilized in conjunction with [Validation:add_rules]
+	 *
+	 * ###### Example
+	 *     	
+	 *     // Assuming the value of $_FILES['name'] is "picture"
+	 *     $files = Validation::factory($_FILES)->add_rules('picture', 'upload::size[1M]');
+	 *
+	 * @param   array    $file $_FILES item
+	 * @param   array    $size Maximum file size
 	 * @return  bool
 	 */
 	public static function size(array $file, array $size)
